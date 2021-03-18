6b5c593e1bdc7d56239f0c9582f277613b8a3bad
Bernardo Paulino <bernardo@blue4you.be>
2021-03-18 16:42:19 +0100

    Update THEME_PATH location in deploy script.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -35,7 +35,7 @@ COLOR_RED="\e[91m"
 BG_COLOR_DEFAULT="\e[49m"
 BG_COLOR_GREEN="\e[42m"
 DRUSH_CMD="../vendor/bin/drush"
-THEME_PATH="themes/custom/eic_community"
+THEME_PATH="web/themes/custom/eic_community"
 DEFAULT_CONTENT_MODULES="eic_default_content default_content hal serialization"
 
 # Define list of arguments expected in the input
