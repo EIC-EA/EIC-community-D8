c686f1dc0d772e32c9d46ccbfba7f7031047ed4e
Bernardo Paulino <bernardo@blue4you.be>
2021-03-24 15:48:31 +0100

    EICNET-598: Create custom block plugin "eic_group_header".

---
+++ lib/modules/eic_groups/src/Plugin/Block/EICGroupHeaderBlock.php
@@ -0,0 +1,98 @@
+<?php
+
+namespace Drupal\eic_groups\Plugin\Block;
+
+use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Cache\CacheableMetadata;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Provides an EICGroupHeaderBlock block.
+ *
+ * @Block(
+ *   id = "eic_group_header",
+ *   admin_label = @Translation("EIC Group header"),
+ *   category = @Translation("European Innovation Council"),
+ *   context_definitions = {
+ *     "group" = @ContextDefinition("entity:group", required = FALSE)
+ *   }
+ * )
+ */
+class EICGroupHeaderBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /**
+   * The current route match.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $routeMatch;
+
+  /**
+   * Constructs a new EICGroupHeaderBlock instance.
+   *
+   * @param array $configuration
+   *   The plugin configuration, i.e. an array with configuration values keyed
+   *   by configuration option name. The special key 'context' may be used to
+   *   initialize the defined contexts by setting it to an array of context
+   *   values keyed by context names.
+   * @param string $plugin_id
+   *   The plugin_id for the plugin instance.
+   * @param mixed $plugin_definition
+   *   The plugin implementation definition.
+   * @param \Drupal\Core\Routing\RouteMatchInterface $route_match
+   *   The current route match.
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, RouteMatchInterface $route_match) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->routeMatch = $route_match;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('current_route_match')
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function build() {
+    $build = [];
+
+    if (($group = $this->getContextValue('group')) && $group->id()) {
+      // The content of this is block is shown depending on the current user's
+      // permissions. It obviously also varies per group, but we cannot know for
+      // sure how we got that group as it is up to the context provider to
+      // implement that. This block will then inherit the appropriate cacheable
+      // metadata from the context, as set by the context provider.
+      $cacheable_metadata = new CacheableMetadata();
+      $cacheable_metadata->setCacheContexts([
+        'user.group_permissions',
+        'url.path',
+      ]);
+
+      $build['content'] = [
+        '#theme' => 'eic_group_header_block',
+        '#group' => $group,
+        '#group_values' => [
+          'id' => $group->id(),
+          'bundle' => $group->bundle(),
+          'title' => $group->label(),
+          'description' => $group->get('field_body')->value,
+        ],
+      ];
+
+      $cacheable_metadata->applyTo($build);
+    }
+    return $build;
+  }
+
+}
