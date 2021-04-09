eb60088dc9b0a8c0c9a83d1dd7176e9c7691bac5
Bernardo Paulino <bernardo@blue4you.be>
2021-04-09 15:26:03 +0200

    EICNET-613: Update group wiki context; Redirect user to first top level wiki page when group wiki overview page.

--- lib/modules/eic_groups/src/Controller/GroupWikiOverviewController.php
+++ lib/modules/eic_groups/src/Controller/GroupWikiOverviewController.php
@@ -3,9 +3,10 @@
 namespace Drupal\eic_groups\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Render\RendererInterface;
+use Drupal\Core\Database\Connection;
 use Drupal\group\Entity\GroupInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\RedirectResponse;
 
 /**
  * Returns responses for EIC Groups routes.
@@ -13,20 +14,20 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
 class GroupWikiOverviewController extends ControllerBase {
 
   /**
-   * The renderer.
+   * The database service.
    *
-   * @var \Drupal\Core\Render\RendererInterface
+   * @var \Drupal\Core\Database\Connection
    */
-  protected $renderer;
+  protected $database;
 
   /**
    * The controller constructor.
    *
-   * @param \Drupal\Core\Render\RendererInterface $renderer
-   *   The renderer.
+   * @param \Drupal\Core\Database\Connection $database
+   *   The database service.
    */
-  public function __construct(RendererInterface $renderer) {
-    $this->renderer = $renderer;
+  public function __construct(Connection $database) {
+    $this->database = $database;
   }
 
   /**
@@ -34,7 +35,7 @@ class GroupWikiOverviewController extends ControllerBase {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('renderer')
+      $container->get('database')
     );
   }
 
@@ -42,10 +43,40 @@ class GroupWikiOverviewController extends ControllerBase {
    * Builds the response.
    */
   public function build(GroupInterface $group) {
-    // @todo Check if there are wiki pages in the group and if not, show a
-    // message "No Wiki pages (yet)" and a button to add a new wiki page.
+    $group_top_level_wiki_pages = $this->getGroupTopLevelWikiPages($group);
     $build['content'] = [];
-    return $build;
+
+    if (empty($group_top_level_wiki_pages)) {
+      $build['content'] = [
+        '#type' => 'item',
+        '#markup' => $this->t('No Wiki pages (yet)'),
+      ];
+    }
+    else {
+      $wiki_page = $this->entityTypeManager()->getStorage('node')->load($group_top_level_wiki_pages[0]->nid);
+      $response = new RedirectResponse($wiki_page->toUrl()->toString());
+      return $response->send();
+    }
+  }
+
+  /**
+   * Get top level wiki pages of a group.
+   *
+   * @param \Drupal\group\Entity\GroupInterface $group
+   *   The Group entity.
+   *
+   * @return array
+   *   An array of top level wiki page books.
+   */
+  private function getGroupTopLevelWikiPages(GroupInterface $group) {
+    $query = $this->database->select('group_content_field_data', 'gp');
+    $query->condition('gp.type', 'group-group_node-wiki_page');
+    $query->condition('gp.gid', $group->id());
+    $query->join('book', 'b', 'gp.entity_id = b.nid');
+    $query->fields('b', ['bid', 'nid']);
+    $query->condition('b.pid', 0);
+    $query->orderBy('b.weight');
+    return $query->execute()->fetchAll(\PDO::FETCH_OBJ);
   }
 
 }
