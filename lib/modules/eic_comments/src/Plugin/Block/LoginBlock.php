699536941ab09813d018aab3bbf981c92c765d41
Carsten de la Fonteijne <carsten.delafonteijne@oneshoe.nl>
2021-05-19 17:35:22 +0200

    EICNET-637: Implement style for block.

--- lib/modules/eic_comments/src/Plugin/Block/LoginBlock.php
+++ lib/modules/eic_comments/src/Plugin/Block/LoginBlock.php
@@ -2,9 +2,11 @@
 
 namespace Drupal\eic_comments\Plugin\Block;
 
+use Drupal\Component\Serialization\Json;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Link;
+use Drupal\Core\Render\Markup;
 use Drupal\Core\Url;
 
 /**
@@ -37,6 +39,18 @@ class LoginBlock extends BlockBase {
       '#description' => $this->t('What would you like to have as headline?'),
       '#default_value' => isset($config['headline']) ? $config['headline'] : '',
     ];
+    $form['login_text'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Log in button text'),
+      '#description' => $this->t('Label for the Login button'),
+      '#default_value' => isset($config['login_text']) ? $config['login_text'] : '',
+    ];
+    $form['register_text'] = [
+      '#type' => 'textfield',
+      '#title' => $this->t('Registration button text'),
+      '#description' => $this->t('Label for the Registration button'),
+      '#default_value' => isset($config['register_text']) ? $config['register_text'] : '',
+    ];
     return $form;
   }
 
@@ -53,23 +67,33 @@ class LoginBlock extends BlockBase {
   public function build() {
     $config = $this->getConfiguration();
 
-    $headline = $this->t('Please log in to see comment and contribute');
+    $title = $this->t('Please log in to see comment and contribute');
     if (!empty($config['headline'])) {
-      $headline = $config['headline'];
+      $title = $config['headline'];
+    }
+
+    $login_text = $this->t('Log in');
+    if (!empty($config['$login_text'])) {
+      $login_text = $config['$login_text'];
+    }
+
+    $register_text = $this->t('Register');
+    if (!empty($config['$register_text'])) {
+      $register_text = $config['$register_text'];
     }
 
     $register_route = Url::fromRoute('user.register');
-    $registration_link = Link::fromTextAndUrl('Register', $register_route);
-
+    $registration_link = Link::fromTextAndUrl($register_text, $register_route);
 
     $login_route = Url::fromRoute('user.login');
-    $login_link = Link::fromTextAndUrl('Log in', $login_route);
+    $login_link = Link::fromTextAndUrl($login_text, $login_route);
 
-    return [
-      ['#markup' => $this->t($headline)],
-      [$login_link->toRenderable()],
-      [$registration_link->toRenderable()],
-    ];
+    $build['title'] = $this->t($title);
+
+    $build['login_link'] = $login_link;
+    $build['registration_link'] = $registration_link;
+
+    return $build;
   }
 
 }
