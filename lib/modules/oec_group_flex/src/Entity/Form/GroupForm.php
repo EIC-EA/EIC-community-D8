da4f4955cc0573e473ce3aa0a9513257d7b0e759
Jaap Jan <mail@jaapjan.nl>
2021-05-31 13:06:55 +0200

    EICNET-762: Make sure the group visibility plugin is not empty before trying to save or retrieve per-entity group access.

--- lib/modules/oec_group_flex/src/Entity/Form/GroupForm.php
+++ lib/modules/oec_group_flex/src/Entity/Form/GroupForm.php
@@ -203,10 +203,12 @@ class GroupForm extends GroupFormBase {
       'joining_methods' => $form_state->getValue('group_joining_methods'),
     ];
 
-    $groupVisibilityPlugin = $this->visibilityManager->createInstance($groupFlexSettings['visibility']['plugin_id']);
+    if ($groupFlexSettings['visibility']['plugin_id']) {
+      $groupVisibilityPlugin = $this->visibilityManager->createInstance($groupFlexSettings['visibility']['plugin_id']);
 
-    if ($groupVisibilityPlugin instanceof GroupVisibilityOptionsInterface) {
-      $groupFlexSettings['visibility']['visibility_options'] = $groupVisibilityPlugin->getFormStateValues($form_state);
+      if ($groupVisibilityPlugin instanceof GroupVisibilityOptionsInterface) {
+        $groupFlexSettings['visibility']['visibility_options'] = $groupVisibilityPlugin->getFormStateValues($form_state);
+      }
     }
 
     $this->groupFlexSettings = $groupFlexSettings;
