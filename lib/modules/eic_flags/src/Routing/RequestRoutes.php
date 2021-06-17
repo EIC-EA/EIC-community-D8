ad49f5cd21679ad950f2c764fcffd6858c643e6e
Korhan Kale <kkale@prophets.be>
2021-06-17 21:30:49 +0200

    [EICNET-883] chore: Change a bit the way we generate entity routes

---
+++ lib/modules/eic_flags/src/Routing/RequestRoutes.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace Drupal\eic_flags\Routing;
+
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\eic_flags\RequestTypes;
+use Drupal\eic_flags\Service\HandlerInterface;
+use Symfony\Component\Routing\Route;
+use Symfony\Component\Routing\RouteCollection;
+
+/**
+ * Class RequestRoutes
+ *
+ * @package Drupal\eic_flags\Routing
+ */
+class RequestRoutes {
+
+  use StringTranslationTrait;
+
+  /**
+   * @return \Symfony\Component\Routing\RouteCollection
+   */
+  public function routes() {
+    $collector = \Drupal::service('eic_flags.handler_collector');
+    $available_types = array_map(
+      function (HandlerInterface $handler) {
+        return $handler->getType();
+      },
+      $collector->getHandlers()
+    );
+    $flag_type_list = implode('|', $available_types);
+    $route_collection = new RouteCollection();
+
+    // Define the route displaying flagged entities
+    $route = (new Route('/admin/community/request/{request_type}'))
+      ->addDefaults(
+        [
+          '_controller' => 'Drupal\eic_flags\Controller\FlagRequestController::listing',
+          '_title' => $this->t('Pending deletion requests')->render(),
+        ]
+      )
+      ->setRequirement('_role', 'administrator+content_administrator')
+      ->setRequirement('request_type', $flag_type_list)
+      ->setOption('_admin_route', TRUE);
+
+    $route_collection->add('eic_flags.flagged_entities.list', $route);
+
+    // Define the route displaying flags for a content entity
+    $route = (new Route(
+      '/admin/community/{entity_type}/{entity_id}/{request_type}/detail'
+    ))
+      ->addDefaults(
+        [
+          '_controller' => 'Drupal\eic_flags\Controller\FlagRequestController::detail',
+        ]
+      )
+      ->setRequirement('_role', 'administrator+content_administrator')
+      ->setRequirement('request_type', $flag_type_list)
+      ->setOption('_admin_route', TRUE);
+
+    $route_collection->add('eic_flags.flagged_entity.detail', $route);
+
+    return $route_collection;
+  }
+
+}
