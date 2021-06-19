a5e09e0767bb1503a6e56dc5fcdd27a597e467b9
Korhan Kale <kkale@prophets.be>
2021-06-19 05:15:00 +0200

    [EICNET-885] fix: Added the archive group content plugin + labeling fixes

--- lib/modules/eic_flags/src/Controller/FlagRequestController.php
+++ lib/modules/eic_flags/src/Controller/FlagRequestController.php
@@ -6,6 +6,7 @@ use Drupal\Core\Controller\ControllerBase;
 use Drupal\eic_flags\FlaggedEntitiesListBuilder;
 use Drupal\eic_flags\FlaggingListBuilder;
 use Drupal\eic_flags\RequestTypes;
+use http\Env\Request;
 
 /**
  * Class FlagRequestController
@@ -36,6 +37,18 @@ class FlagRequestController extends ControllerBase {
     return $this->entityTypeManager()
       ->createHandlerInstance(FlaggingListBuilder::class, $definition)
       ->render();
+  }
+
+  /**
+   * Returns the title for the eic_flags.flagged_entities.list route
+   *
+   * @param $request_type
+   *
+   * @return \Drupal\Core\StringTranslation\TranslatableMarkup
+   */
+  public function getTitle($request_type) {
+    $operation = $request_type === RequestTypes::DELETE ? 'delete' : 'archival';
+    return $this->t('Pending @operation requests', ['@operation' => $operation]);
   }
 
 }
