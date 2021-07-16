a7811a7a3bf7ed1cceb81e18f775a9ce8b738137
Bernardo Paulino <bernardo.paulino4@gmail.com>
2021-07-16 13:30:11 +0200

    EICNET-1113: Create module eic_group_statistics; Create new custom schema for saving group statistics; Implement statistics counter for group members; Delete group statistics when group is deleted.

---
+++ lib/modules/eic_statistics/modules/eic_group_statistics/src/GroupStatisticsStorage.php
@@ -0,0 +1,136 @@
+<?php
+
+namespace Drupal\eic_group_statistics;
+
+use Drupal\Core\Database\Connection;
+use Drupal\group\Entity\GroupInterface;
+
+/**
+ * Provides the default storage backend for Group statistics.
+ */
+class GroupStatisticsStorage implements GroupStatisticsStorageInterface {
+
+  /**
+   * The current active database's master connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $connection;
+
+  /**
+   * Constructs a new GroupStatisticsStorage object.
+   *
+   * @return \Drupal\Core\Database\Connection
+   *   The current active database's master connection.
+   */
+  public function __construct(Connection $connection) {
+    $this->connection = $connection;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function increment(GroupInterface $group, $statistic_type, $count = 1) {
+    $query = $this->connection->merge('eic_group_statistics')
+      ->keys([
+        'gid' => $group->id(),
+      ])
+      ->fields([
+        'gtype' => $group->bundle(),
+      ]);
+
+    switch ($statistic_type) {
+      case GroupStatisticsStorageInterface::STAT_TYPE_MEMBERS:
+        $query->fields(['members' => 1])
+          ->expression('members', '[members] + 1')
+          ->execute();
+        break;
+
+      case GroupStatisticsStorageInterface::STAT_TYPE_COMMENTS:
+        $query->fields(['comments' => 1])
+          ->expression('comments', '[comments] + 1')
+          ->execute();
+        break;
+
+      case GroupStatisticsStorageInterface::STAT_TYPE_FILES:
+        $query->fields(['files' => 1])
+          ->expression('files', '[files] + 1')
+          ->execute();
+        break;
+
+      case GroupStatisticsStorageInterface::STAT_TYPE_EVENTS:
+        $query->fields(['events' => 1])
+          ->expression('events', '[events] + 1')
+          ->execute();
+        break;
+
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function decrement(GroupInterface $group, $statistic_type, $count = 1) {
+    $query = $this->connection->merge('eic_group_statistics')
+      ->keys([
+        'gid' => $group->id(),
+      ])
+      ->fields([
+        'gtype' => $group->bundle(),
+      ]);
+
+    switch ($statistic_type) {
+      case GroupStatisticsStorageInterface::STAT_TYPE_MEMBERS:
+        $query->fields(['members' => 1])
+          ->expression('members', '[members] - 1')
+          ->execute();
+        break;
+
+      case GroupStatisticsStorageInterface::STAT_TYPE_COMMENTS:
+        $query->fields(['comments' => 1])
+          ->expression('comments', '[comments] - 1')
+          ->execute();
+        break;
+
+      case GroupStatisticsStorageInterface::STAT_TYPE_FILES:
+        $query->fields(['files' => 1])
+          ->expression('files', '[files] - 1')
+          ->execute();
+        break;
+
+      case GroupStatisticsStorageInterface::STAT_TYPE_EVENTS:
+        $query->fields(['events' => 1])
+          ->expression('events', '[events] - 1')
+          ->execute();
+        break;
+
+    }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function deleteGroupStatistics(GroupInterface $group) {
+    $this->connection->delete('eic_group_statistics')
+      ->condition('gid', $group->id())
+      ->execute();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function load(GroupInterface $group) {
+    $result = $this->connection->select('eic_group_statistics')
+      ->fields('eic_group_statistics')
+      ->condition('gid', $group->id())
+      ->range(0, 1)
+      ->execute()->fetchAssoc();
+
+    if (empty($result)) {
+      return FALSE;
+    }
+
+    return $result;
+  }
+
+}
---
+++ lib/modules/eic_statistics/modules/eic_group_statistics/src/GroupStatisticsStorageInterface.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace Drupal\eic_group_statistics;
+
+use Drupal\group\Entity\GroupInterface;
+
+/**
+ * Provides an interface defining Group Statistics Storage.
+ */
+interface GroupStatisticsStorageInterface {
+
+  /**
+   * Database column name for group members counter.
+   */
+  const STAT_TYPE_MEMBERS = 'members';
+
+  /**
+   * Database column name for group comments counter.
+   */
+  const STAT_TYPE_COMMENTS = 'comments';
+
+  /**
+   * Database column name for group files counter.
+   */
+  const STAT_TYPE_FILES = 'files';
+
+  /**
+   * Database column name for group events counter.
+   */
+  const STAT_TYPE_EVENTS = 'events';
+
+  /**
+   * Increments counter for a certain statistic type.
+   *
+   * @param \Drupal\group\Entity\GroupInterface $group
+   *   The Group entity.
+   * @param string $statistic_type
+   *   The type of statistic to increment the counter.
+   * @param int $count
+   *   (optional) The increment number to add to the statistic. Defaults to 1.
+   */
+  public function increment(GroupInterface $group, $statistic_type, $count = 1);
+
+  /**
+   * Decrements counter for a certain statistic type.
+   *
+   * @param \Drupal\group\Entity\GroupInterface $group
+   *   The Group entity.
+   * @param string $statistic_type
+   *   The type of statistic to increment the counter.
+   * @param int $count
+   *   (optional) The increment number to add to the statistic. Defaults to 1.
+   */
+  public function decrement(GroupInterface $group, $statistic_type, $count = 1);
+
+  /**
+   * Delete all statistics for a given group.
+   *
+   * @param \Drupal\group\Entity\GroupInterface $group
+   *   The Group entity.
+   */
+  public function deleteGroupStatistics(GroupInterface $group);
+
+  /**
+   * Loads statistics of a given group.
+   *
+   * @param \Drupal\group\Entity\GroupInterface $group
+   *   The Group entity.
+   */
+  public function load(GroupInterface $group);
+
+}
