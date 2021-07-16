67dad68861c63e8c31d99b4236ccf690d75139e2
theodoros.suliotis <tsuliotis@prophets.be>
2021-07-16 11:24:34 +0200

    [EICNET-843]feature: Use dependency injection for formbuilder

--- lib/modules/eic_groups/src/Form/SearchMenuGroupForm.php
+++ lib/modules/eic_groups/src/Form/SearchMenuGroupForm.php
@@ -6,7 +6,7 @@ use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 
 /**
- * Class SearchMenuGroupForm
+ * Provide a small search form next to the group menu
  *
  * @package Drupal\eic_groups\Form
  */
--- lib/modules/eic_groups/src/Plugin/Block/SearchMenuGroupBlock.php
+++ lib/modules/eic_groups/src/Plugin/Block/SearchMenuGroupBlock.php
@@ -4,10 +4,13 @@ namespace Drupal\eic_groups\Plugin\Block;
 
 use Drupal\Core\Block\Annotation\Block;
 use Drupal\Core\Block\BlockBase;
+use Drupal\Core\Form\FormBuilderInterface;
+use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\eic_groups\Form\SearchMenuGroupForm;
+use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Provides an SearchMenuGroupBlock block.
+ * Provides a SearchMenuGroupBlock block.
  *
  * @Block(
  *   id = "eic_groups_search_menu_group",
@@ -18,13 +21,41 @@ use Drupal\eic_groups\Form\SearchMenuGroupForm;
  *   }
  * )
  */
-class SearchMenuGroupBlock extends BlockBase {
+class SearchMenuGroupBlock extends BlockBase implements ContainerFactoryPluginInterface {
+
+  /** @var \Drupal\Core\Form\FormBuilderInterface $formBuilder */
+  private $formBuilder;
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('form_builder')
+    );
+  }
+
+  /**
+   * SearchMenuGroupBlock constructor.
+   *
+   * @param array $configuration
+   * @param $plugin_id
+   * @param $plugin_definition
+   * @param \Drupal\Core\Form\FormBuilderInterface $form_builder
+   */
+  public function __construct(array $configuration, $plugin_id, $plugin_definition, FormBuilderInterface $form_builder) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+    $this->formBuilder = $form_builder;
+  }
 
   /**
    * {@inheritdoc}
    */
   public function build() {
-    $form = \Drupal::formBuilder()->getForm(SearchMenuGroupForm::class);
+    $form = $this->formBuilder->getForm(SearchMenuGroupForm::class);
 
     return $form;
   }
