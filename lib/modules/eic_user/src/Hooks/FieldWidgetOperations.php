f471f7d423053feae878cc8f7450cb52b219daa7
Bernardo Paulino <bernardo.paulino4@gmail.com>
2021-07-26 11:22:30 +0200

    EICNET-1130: Remove helper text from LinkedIn field and prepend string "in/" if missing from the values.

--- lib/modules/eic_user/src/Hooks/FieldWidgetOperations.php
+++ lib/modules/eic_user/src/Hooks/FieldWidgetOperations.php
@@ -3,7 +3,6 @@
 namespace Drupal\eic_user\Hooks;
 
 use Drupal\Core\Form\FormStateInterface;
-use Drupal\Core\StringTranslation\StringTranslationTrait;
 
 /**
  * Class FieldWidgetOperations.
@@ -12,8 +11,6 @@ use Drupal\Core\StringTranslation\StringTranslationTrait;
  */
 class FieldWidgetOperations {
 
-  use StringTranslationTrait;
-
   /**
    * Implements hook_field_widget_social_links_form_alter().
    */
@@ -21,14 +18,6 @@ class FieldWidgetOperations {
     $form_build_info = $form_state->getBuildInfo();
 
     if ($form_build_info['base_form_id'] === 'profile_form') {
-      $selected_network = $element['social']['#default_value'];
-
-      // Adds custom helper texts needed to describe how to fill in certain
-      // social networks.
-      if (isset($this->getSocialLinksFieldDescriptions()[$selected_network])) {
-        $element['link']['#description'] = $this->getSocialLinksFieldDescriptions()[$selected_network];
-      }
-
       // Adds custom element validation to fix the social network links when
       // the user inserts the full url from the social network platform.
       $element['#element_validate'] = [
@@ -56,23 +45,18 @@ class FieldWidgetOperations {
     foreach ($form_state_values as $key => $value) {
       if ($value['social'] === $social_network_name) {
         $form_state_values[$key]['link'] = str_replace($social_network_base_url, '', $value['link']);
+
+        // Exception for LinkedIn since we need to prepend "in/" if missing.
+        if ($social_network_name === 'linkedin') {
+          if (substr($social_network_name, 0, 3) !== 'in/') {
+            $form_state_values[$key]['link'] = 'in/' . $form_state_values[$key]['link'];
+          }
+        }
         break;
       }
     }
 
     $form_state->setValue($field_name, $form_state_values);
-  }
-
-  /**
-   * Gets array of helper texts to help users fill in the social links field.
-   *
-   * @return array
-   *   Array of helper texts keyed by social network type.
-   */
-  public function getSocialLinksFieldDescriptions() {
-    return [
-      'linkedin' => $this->t('Insert your LinkedIn username in the following format: in/example-user-name.'),
-    ];
   }
 
 }
