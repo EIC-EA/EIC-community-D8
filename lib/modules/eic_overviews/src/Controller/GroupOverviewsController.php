76541e39c408f44f5643852769f23d4df668429d
Ludovic Ramarojaona <ludovic.ramarojaona@blue4you.be>
2021-07-30 09:06:17 +0000

    EICNET-851: Remove unnecessary entity type manager from the controller.

--- lib/modules/eic_overviews/src/Controller/GroupOverviewsController.php
+++ lib/modules/eic_overviews/src/Controller/GroupOverviewsController.php
@@ -3,40 +3,12 @@
 namespace Drupal\eic_overviews\Controller;
 
 use Drupal\Core\Controller\ControllerBase;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\group\Entity\GroupInterface;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Provides route response for group overview pages.
  */
 class GroupOverviewsController extends ControllerBase {
-
-  /**
-   * The entity type manager.
-   *
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
-   */
-  protected $entityTypeManager;
-
-  /**
-   * Constructs a new AboutPageController object.
-   *
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager.
-   */
-  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
-    $this->entityTypeManager = $entity_type_manager;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  public static function create(ContainerInterface $container) {
-    return new static(
-      $container->get('entity_type.manager')
-    );
-  }
 
   /**
    * Returns the content for a generic group overview page.
@@ -45,10 +17,7 @@ class GroupOverviewsController extends ControllerBase {
    *   A simple renderable array.
    */
   public function buildGenericPage(GroupInterface $group) {
-    $build = [
-      '#markup' => '',
-    ];
-    return $build;
+    return ['#markup' => ''];
   }
 
 }
