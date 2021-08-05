59b87e21f0e48beccb3201a85792413c7ef23556
Bernardo Paulino <bernardo.paulino4@gmail.com>
2021-08-05 16:10:30 +0200

    EICNET-1167: Create helper service (eic_comments.helper) that provides helper functions for comments.

---
+++ lib/modules/eic_comments/src/CommentsHelper.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace Drupal\eic_comments;
+
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\node\NodeInterface;
+
+/**
+ * Service that provides helper functions for comments.
+ */
+class CommentsHelper {
+
+  /**
+   * The database connection.
+   *
+   * @var \Drupal\Core\Database\Connection
+   */
+  protected $connection;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a CommentsHelper object.
+   *
+   * @param \Drupal\Core\Database\Connection $connection
+   *   The database connection.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(Connection $connection, EntityTypeManagerInterface $entity_type_manager) {
+    $this->connection = $connection;
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
+   * Get the number of comments that belong to a node.
+   *
+   * @param \Drupal\node\NodeInterface $node
+   *   The node object.
+   *
+   * @return int
+   *   The number of comments that belong to the node.
+   */
+  public function countNodeComments(NodeInterface $node) {
+    $query = $this->entityTypeManager->getStorage('comment')
+      ->getQuery()
+      ->condition('entity_id', $node->id())
+      ->count();
+    $results = $query->execute();
+    return (int) $results;
+  }
+
+}
