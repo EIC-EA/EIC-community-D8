75327a01d7d5800ea3a66ef0392da116ef906856
Bernardo Paulino <bernardo.paulino4@gmail.com>
2021-08-16 14:02:51 +0200

    EICNET-1098: Create route for group library overview page.

--- lib/modules/eic_groups/src/Plugin/GroupFeature/GroupFiles.php
+++ lib/modules/eic_groups/src/Plugin/GroupFeature/GroupFiles.php
@@ -3,7 +3,6 @@
 namespace Drupal\eic_groups\Plugin\GroupFeature;
 
 use Drupal\Core\Url;
-use Drupal\group\Entity\GroupInterface;
 
 /**
  * Group feature plugin implementation for Files.
@@ -19,11 +18,9 @@ class GroupFiles extends EicGroupsGroupFeaturePluginBase {
   /**
    * Route of the files overview.
    *
-   * @todo Change the route name once we have the final files overview page.
-   *
    * @var string
    */
-  const PRIMARY_OVERVIEW_ROUTE = '<front>';
+  const PRIMARY_OVERVIEW_ROUTE = 'eic_overviews.groups.overview_page.files';
 
   /**
    * {@inheritdoc}
@@ -33,13 +30,6 @@ class GroupFiles extends EicGroupsGroupFeaturePluginBase {
     // Set a specific weight for the menu item.
     $menu_item->set('weight', 7);
     return $menu_item;
-  }
-
-  /**
-   * {@inheritdoc}
-   */
-  protected function getPrimaryOverviewRoute(GroupInterface $group) {
-    return Url::fromRoute(static::PRIMARY_OVERVIEW_ROUTE);
   }
 
 }
