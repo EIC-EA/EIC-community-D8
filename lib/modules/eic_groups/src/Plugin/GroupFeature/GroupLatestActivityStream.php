651b623f356c0e1b8a3d64d1f8be8b188f007871
Bernardo Paulino <bernardo.paulino4@gmail.com>
2021-08-16 14:07:49 +0200

    EICNET-1079: Create group feature plugin to show/hide group menu link for the latest activity stream page.

---
+++ lib/modules/eic_groups/src/Plugin/GroupFeature/GroupLatestActivityStream.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace Drupal\eic_groups\Plugin\GroupFeature;
+
+use Drupal\Core\Url;
+
+/**
+ * Group feature plugin implementation for Latest activity stream.
+ *
+ * @GroupFeature(
+ *   id = "eic_groups_latest_activity_stream",
+ *   label = @Translation("Latest activity stream"),
+ *   description = @Translation("Group latest activity stream feature.")
+ * )
+ */
+class GroupLatestActivityStream extends EicGroupsGroupFeaturePluginBase {
+
+  /**
+   * Route of the latest activity stream overview.
+   *
+   * @todo Change the route name once we have the final overview page.
+   *
+   * @var string
+   */
+  const PRIMARY_OVERVIEW_ROUTE = 'eic_overviews.groups.overview_page.latest_activity_stream';
+
+  /**
+   * {@inheritdoc}
+   */
+  protected function getMenuItem(Url $url, string $menu_name) {
+    $menu_item = parent::getMenuItem($url, $menu_name);
+    // Set a specific weight for the menu item.
+    $menu_item->set('weight', 7);
+    return $menu_item;
+  }
+
+}
