e4550f02d940e6bbb6e1b2e75609e5f0f4665aae
Ludovic Ramarojaona <ludovic.ramarojaona@blue4you.be>
2021-08-18 14:35:52 +0000

    EICNET-851: Add classes for overview routes and IDs.

---
+++ lib/modules/eic_overviews/src/GlobalOverviewPageIds.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Drupal\eic_overviews;
+
+/**
+ * Provides constants for global overview pages.
+ *
+ * @package Drupal\eic_overviews
+ */
+final class GlobalOverviewPageIds {
+
+  /**
+   * ID of the Groups overview page.
+   */
+  const GROUPS = 1;
+
+  /**
+   * ID of the Members overview page.
+   */
+  const MEMBERS = 2;
+
+  /**
+   * ID of the Global search overview page.
+   */
+  const GLOBAL_SEARCH = 3;
+
+}
