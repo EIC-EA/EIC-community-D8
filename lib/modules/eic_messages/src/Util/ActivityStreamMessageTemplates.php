0ddf27e90055090eacecb3e7f819b96997d23b54
Bernardo Paulino <bernardo.paulino4@gmail.com>
2021-08-19 17:14:01 +0200

    EICNET-1161: Create separated message type for activity stream of Gallery Ctype; Fix coding standards

--- lib/modules/eic_messages/src/Util/ActivityStreamMessageTemplates.php
+++ lib/modules/eic_messages/src/Util/ActivityStreamMessageTemplates.php
@@ -3,19 +3,23 @@
 namespace Drupal\eic_messages\Util;
 
 use Drupal\Core\Entity\ContentEntityInterface;
-use InvalidArgumentException;
 
 /**
- * Class ActivityStreamMessageTemplates
+ * Class ActivityStreamMessageTemplates.
  */
 final class ActivityStreamMessageTemplates {
 
+  /**
+   * Array of activity stream message templates per entity type.
+   *
+   * @var array
+   */
   private static $templates = [
     'node' => [
       'discussion' => 'stream_discussion_insert_update',
       'wiki_page' => 'stream_wiki_page_insert_update',
       'document' => 'stream_document_insert_update',
-      'gallery' => 'stream_photo_album_insert_update',
+      'gallery' => 'stream_gallery_insert_update',
     ],
     'comment' => [
       'node_comment' => 'stream_comment_insert_update',
@@ -23,7 +27,10 @@ final class ActivityStreamMessageTemplates {
   ];
 
   /**
+   * Gets list of defined templates.
+   *
    * @return array
+   *   Array of templates.
    */
   public static function getAllowedTemplates(): array {
     $flat_values = call_user_func_array('array_merge', self::$templates);
@@ -32,13 +39,17 @@ final class ActivityStreamMessageTemplates {
   }
 
   /**
+   * Returns an activity stream message template for a given entity.
+   *
    * @param \Drupal\Core\Entity\ContentEntityInterface $entity
+   *   The entity object.
    *
    * @return string
+   *   The template name.
    */
   public static function getTemplate(ContentEntityInterface $entity): string {
     if (!isset(self::$templates[$entity->getEntityTypeId()][$entity->bundle()])) {
-      throw new InvalidArgumentException('Invalid entity / bundle provided');
+      throw new \InvalidArgumentException('Invalid entity / bundle provided');
     }
 
     return self::$templates[$entity->getEntityTypeId()][$entity->bundle()];
