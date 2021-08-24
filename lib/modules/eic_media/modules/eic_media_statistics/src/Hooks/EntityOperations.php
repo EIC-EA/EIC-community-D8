3b13f94b2db5072205bc215abe25a50dcb818cc0
Ludovic Ramarojaona <ludovic.ramarojaona@blue4you.be>
2021-08-24 12:00:26 +0000

    EICNET-1225: Clean up EntityOperations class.

--- lib/modules/eic_media/modules/eic_media_statistics/src/Hooks/EntityOperations.php
+++ lib/modules/eic_media/modules/eic_media_statistics/src/Hooks/EntityOperations.php
@@ -5,10 +5,7 @@ namespace Drupal\eic_media_statistics\Hooks;
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Drupal\Core\Entity\Display\EntityViewDisplayInterface;
 use Drupal\Core\Entity\EntityInterface;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\eic_media_statistics\EntityFileDownloadCount;
-use Drupal\eic_media_statistics\FileStatisticsDatabaseStorage;
-use Drupal\entity_usage\EntityUsageInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
@@ -17,27 +14,6 @@ use Symfony\Component\DependencyInjection\ContainerInterface;
  * Implementations of entity hooks.
  */
 class EntityOperations implements ContainerInjectionInterface {
-
-  /**
-   * The Entity type manager.
-   *
-   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
-   */
-  protected $entityTypeManager;
-
-  /**
-   * The Entity Usage service.
-   *
-   * @var \Drupal\entity_usage\EntityUsageInterface
-   */
-  protected $entityUsage;
-
-  /**
-   * The File statistics database storage service.
-   *
-   * @var \Drupal\eic_media_statistics\FileStatisticsDatabaseStorage
-   */
-  protected $fileStatisticsDbStorage;
 
   /**
    * The Entity file download count service.
@@ -49,24 +25,10 @@ class EntityOperations implements ContainerInjectionInterface {
   /**
    * Constructs a EntityOperation object.
    *
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
-   *   The entity type manager.
-   * @param \Drupal\entity_usage\EntityUsageInterface $entity_usage
-   *   The Entity Usage service.
-   * @param \Drupal\eic_media_statistics\FileStatisticsDatabaseStorage $file_statistics_db_storage
-   *   The File statistics database storage service.
    * @param \Drupal\eic_media_statistics\EntityFileDownloadCount $entity_file_download_count
    *   The Entity file download count service.
    */
-  public function __construct(
-    EntityTypeManagerInterface $entity_type_manager,
-    EntityUsageInterface $entity_usage,
-    FileStatisticsDatabaseStorage $file_statistics_db_storage,
-    EntityFileDownloadCount $entity_file_download_count
-  ) {
-    $this->entityTypeManager = $entity_type_manager;
-    $this->entityUsage = $entity_usage;
-    $this->fileStatisticsDbStorage = $file_statistics_db_storage;
+  public function __construct(EntityFileDownloadCount $entity_file_download_count) {
     $this->entityFileDownloadCount = $entity_file_download_count;
   }
 
@@ -75,15 +37,12 @@ class EntityOperations implements ContainerInjectionInterface {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('entity_type.manager'),
-      $container->get('entity_usage.usage'),
-      $container->get('eic_media_statistics.storage.file'),
       $container->get('eic_media_statistics.entity_file_download_count')
     );
   }
 
   /**
-   * Acts on hook_node_view() for node entities that belong to a group.
+   * Acts on hook_node_view() for node entities.
    *
    * @param array $build
    *   The renderable array representing the entity content.
