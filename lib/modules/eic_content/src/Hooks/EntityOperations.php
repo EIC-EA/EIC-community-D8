f03478418df9857c152dc9d87634c7cf2b38f57c
Ludovic Ramarojaona <ludovic.ramarojaona@blue4you.be>
2021-08-25 12:50:25 +0000

    EICNET-1225: Add the number of page views for all nodes.

---
+++ lib/modules/eic_content/src/Hooks/EntityOperations.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace Drupal\eic_content\Hooks;
+
+use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Entity\Display\EntityViewDisplayInterface;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\statistics\NodeStatisticsDatabaseStorage;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Class EntityOperations.
+ *
+ * Implementations of entity hooks.
+ */
+class EntityOperations implements ContainerInjectionInterface {
+
+  /**
+   * The Entity file download count service.
+   *
+   * @var \Drupal\statistics\NodeStatisticsDatabaseStorage
+   */
+  protected $nodeStatisticsDatabaseStorage;
+
+  /**
+   * Constructs a EntityOperation object.
+   *
+   * @param \Drupal\statistics\NodeStatisticsDatabaseStorage $node_statistics_db_storage
+   *   The Entity file download count service.
+   */
+  public function __construct(NodeStatisticsDatabaseStorage $node_statistics_db_storage) {
+    $this->nodeStatisticsDatabaseStorage = $node_statistics_db_storage;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('statistics.storage.node')
+    );
+  }
+
+  /**
+   * Acts on hook_node_view() for node entities.
+   *
+   * @param array $build
+   *   The renderable array representing the entity content.
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The node entity object.
+   * @param \Drupal\Core\Entity\Display\EntityViewDisplayInterface $display
+   *   The entity view display holding the display options.
+   * @param string $view_mode
+   *   The view mode the entity is rendered in.
+   */
+  public function nodeView(array &$build, EntityInterface $entity, EntityViewDisplayInterface $display, $view_mode) {
+    $node_views = $this->nodeStatisticsDatabaseStorage->fetchView($entity->id());
+    $build['page_views'] = [
+      '#markup' => '',
+      '#value' => $node_views->getTotalCount(),
+    ];
+  }
+
+}
