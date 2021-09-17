fb7b1cba651a2c122decb3de3bc0d56fa04fcb38
Korhan Kale <kkale@prophets.be>
2021-09-17 07:35:19 +0200

    [EICNET-1396] Fix: Additional CS fixes

--- lib/modules/eic_groups/src/Plugin/Block/LastGroupActivitiesBlock.php
+++ lib/modules/eic_groups/src/Plugin/Block/LastGroupActivitiesBlock.php
@@ -125,52 +125,38 @@ class LastGroupActivitiesBlock extends BlockBase implements ContainerFactoryPlug
 
     $members = GroupContent::loadMultiple($members_id);
 
-    $members_data = array_map(
-      function (GroupContent $groupContent) {
-        $profiles = $this->entityTypeManager->getStorage('profile')->loadByProperties(
-          [
-            'uid' => $groupContent->getEntity()->id(),
-            'type' => 'member',
-          ]
-        );
+    $members_data = array_map(function (GroupContent $groupContent) {
+      $profiles = $this->entityTypeManager->getStorage('profile')->loadByProperties([
+        'uid' => $groupContent->getEntity()->id(),
+        'type' => 'member',
+      ]);
 
-        if (empty($profiles)) {
-          return [];
-        }
+      if (empty($profiles)) {
+        return [];
+      }
 
-        /** @var \Drupal\profile\Entity\ProfileInterface $profile */
-        $profile = reset($profiles);
-        $user = $profile->getOwner();
+      /** @var \Drupal\profile\Entity\ProfileInterface $profile */
+      $profile = reset($profiles);
+      $user = $profile->getOwner();
 
-        if (!$user) {
-          return [];
-        }
+      if (!$user) {
+        return [];
+      }
 
-        /** @var \Drupal\media\MediaInterface|null $media_picture */
-        $media_picture = $user->get('field_media')->referencedEntities();
-        /** @var File|NULL $file */
-        $file = $media_picture ? File::load(
-          $media_picture[0]->get('oe_media_image')->target_id
-        ) : null;
-        $file_url = $file ? file_url_transform_relative(
-          file_create_url($file->get('uri')->value)
-        ) : null;
+      /** @var \Drupal\media\MediaInterface|null $media_picture */
+      $media_picture = $user->get('field_media')->referencedEntities();
+      /** @var File|NULL $file */
+      $file = $media_picture ? File::load($media_picture[0]->get('oe_media_image')->target_id) : NULL;
+      $file_url = $file ? file_url_transform_relative(file_create_url($file->get('uri')->value)) : NULL;
 
-        return [
-          'joined_date' => $this->dateFormatter->format(
-            $groupContent->getCreatedTime(),
-            'eu_short_date'
-          ),
-          'full_name' => $user->get('field_first_name')->value.' '.$user->get(
-              'field_last_name'
-            )->value,
-          'email' => $user->getEmail(),
-          'picture' => $file_url,
-          'url' => $profile->toUrl()->toString(),
-        ];
-      },
-      $members
-    );
+      return [
+        'joined_date' => $this->dateFormatter->format($groupContent->getCreatedTime(), 'eu_short_date'),
+        'full_name' => $user->get('field_first_name')->value . ' ' . $user->get('field_last_name')->value,
+        'email' => $user->getEmail(),
+        'picture' => $file_url,
+        'url' => $profile->toUrl()->toString(),
+      ];
+    }, $members);
 
     $current_group_route = $this->groupsHelper->getGroupFromRoute();
     $user_group_roles = [];
@@ -195,16 +181,8 @@ class LastGroupActivitiesBlock extends BlockBase implements ContainerFactoryPlug
         'load_more' => $this->t('Load more', [], ['context' => 'eic_group']),
         'block_title' => $this->t('Latest member activity', [], ['context' => 'eic_group']),
         'commented_on' => $this->t('commented on', [], ['context' => 'eic_group']),
-        'delete_modal_title' => $this->t(
-          'Delete activity from activity stream',
-          [],
-          ['context' => 'eic_group']
-        ),
-        'delete_modal_desc' => $this->t(
-          'Are you sure you want to delete this activity from the activity stream? Important: this action cannot be undone.',
-          [],
-          ['context' => 'eic_group']
-        ),
+        'delete_modal_title' => $this->t('Delete activity from activity stream', [], ['context' => 'eic_group']),
+        'delete_modal_desc' => $this->t('Are you sure you want to delete this activity from the activity stream? Important: this action cannot be undone.', [], ['context' => 'eic_group']),
         'delete_modal_confirm' => $this->t('Yes, delete activity', [], ['context' => 'eic_group']),
         'delete_modal_cancel' => $this->t('Cancel', [], ['context' => 'eic_group']),
         'delete_modal_close' => $this->t('Close', [], ['context' => 'eic_group']),
