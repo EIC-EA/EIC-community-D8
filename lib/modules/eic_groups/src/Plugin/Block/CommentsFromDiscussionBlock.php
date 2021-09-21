9111b2f0ffca3180beff0512dfb4889e93c19e2e
theodoros.suliotis <tsuliotis@prophets.be>
2021-09-21 04:27:26 +0200

    [EICNET-1417]feat: Use correct permissions check for request delete/archive and cache context

--- lib/modules/eic_groups/src/Plugin/Block/CommentsFromDiscussionBlock.php
+++ lib/modules/eic_groups/src/Plugin/Block/CommentsFromDiscussionBlock.php
@@ -6,6 +6,7 @@ use Drupal\Core\Block\Annotation\Block;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\eic_groups\EICGroupsHelper;
+use Drupal\file\Entity\File;
 use Drupal\group\Entity\GroupContent;
 use Drupal\group\GroupMembership;
 use Drupal\node\NodeInterface;
@@ -110,14 +111,19 @@ class CommentsFromDiscussionBlock extends BlockBase implements ContainerFactoryP
       return $paragraph->get('field_user_ref')->referencedEntities()[0]->id();
     }, $contributors);
 
-    $users = User::loadMultiple($users);
+    $users = array_unique(array_values($users), SORT_NUMERIC);
 
     $contributors_data ['items'] = [];
 
-    if ($node->getOwner() instanceof UserInterface && (int) $node->getOwner()
-        ->id() !== 0) {
+    if (
+      $node->getOwner() instanceof UserInterface &&
+      (int) $node->getOwnerId() !== 0 &&
+      !in_array($node->getOwnerId(), $users)
+    ) {
       $contributors_data['items'][] = eic_community_get_teaser_user_display($node->getOwner());
     }
+
+    $users = User::loadMultiple($users);
 
     foreach ($users as $user) {
       $contributors_data['items'][] = eic_community_get_teaser_user_display($user);
@@ -125,8 +131,23 @@ class CommentsFromDiscussionBlock extends BlockBase implements ContainerFactoryP
 
     $group_contents = GroupContent::loadByEntity($node);
 
+    /** @var \Drupal\media\MediaInterface|null $media_picture */
+    $media_picture = $current_user->get('field_media')->referencedEntities();
+    /** @var File|NULL $file */
+    $file = $media_picture ? File::load($media_picture[0]->get('oe_media_image')->target_id) : NULL;
+    $file_url = $file ? file_url_transform_relative(file_create_url($file->get('uri')->value)) : NULL;
+
     $build['#attached']['drupalSettings']['overview'] = [
       'is_group_owner' => array_key_exists(EICGroupsHelper::GROUP_OWNER_ROLE, $user_group_roles),
+      'user' => [
+        'avatar' => $file_url,
+        'fullname' => $current_user instanceof UserInterface ?
+          $current_user->get('field_first_name')->value . ' ' . $current_user->get('field_last_name')->value :
+          '',
+        'url' => $current_user instanceof UserInterface ?
+          $current_user->toUrl()->toString() :
+          '#',
+      ],
       'group_roles' => $user_group_roles,
       'permissions' => [
         'post_comment' =>
@@ -158,7 +179,7 @@ class CommentsFromDiscussionBlock extends BlockBase implements ContainerFactoryP
         'edit' => $this->t('Edit', [], ['context' => 'eic_groups']),
         'options' => $this->t('Options', [], ['context' => 'eic_groups']),
         'reply_to' => $this->t('Reply to', [], ['context' => 'eic_groups']),
-        'in_reply_to' => $this->t('In reply to', [], ['context' => 'eic_groups']),
+        'in_reply_to' => $this->t('in reply to', [], ['context' => 'eic_groups']),
         'reply' => $this->t('Reply', [], ['context' => 'eic_groups']),
         'submit' => $this->t('Submit', [], ['context' => 'eic_groups']),
         'reason' => $this->t('Reason', [], ['context' => 'eic_groups']),
@@ -170,7 +191,17 @@ class CommentsFromDiscussionBlock extends BlockBase implements ContainerFactoryP
       ],
     ];
 
+    $group_id = $current_group_route ? $current_group_route->id() : 0;
+
     return $build + [
+        '#cache' => [
+          'contexts' => [
+            'url.path',
+            'url.query_args',
+            "user.is_group_member:$group_id",
+            'user.group_permissions',
+          ],
+        ],
         '#theme' => 'eic_group_comments_from_discussion',
         '#discussion_id' => $node->id(),
         '#contributors' => $contributors_data,
