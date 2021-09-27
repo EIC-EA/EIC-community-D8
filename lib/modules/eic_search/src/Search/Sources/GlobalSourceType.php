c8f398db6f6c6b7bd11542df47641ad6e2856d0c
theodoros.suliotis <tsuliotis@prophets.be>
2021-09-27 19:49:56 +0200

    [EICNET]fix: Add more filters and sorts on global search

--- lib/modules/eic_search/src/Search/Sources/GlobalSourceType.php
+++ lib/modules/eic_search/src/Search/Sources/GlobalSourceType.php
@@ -47,7 +47,8 @@ class GlobalSourceType extends SourceType {
       'ss_global_content_type' => $this->t('Content type', [], ['context' => 'eic_search']),
       'ss_group_user_fullname' => $this->t('Full name', [], ['context' => 'eic_search']),
       'sm_content_field_vocab_topics_string' => $this->t('Topics', [], ['context' => 'eic_search']),
-      'sm_content_field_vocab_geo_string' => $this->t('Region & country', [], ['context' => 'eic_search']),
+      'sm_content_field_vocab_geo_string' => $this->t('Regions & countries', [], ['context' => 'eic_search']),
+      'ss_content_language_string' => $this->t('Languages', [], ['context' => 'eic_search']),
     ];
   }
 
@@ -73,8 +74,19 @@ class GlobalSourceType extends SourceType {
       ],
       'its_document_download_total' => [
         'label' => $this->t('Download', [], ['context' => 'eic_search']),
-        'ASC' => $this->t('Min download', [], ['context' => 'eic_search']),
-        'DESC' => $this->t('Max download', [], ['context' => 'eic_search']),
+        'DESC' => $this->t('Most downloaded', [], ['context' => 'eic_search']),
+      ],
+      'its_statistics_view' => [
+        'label' => $this->t('View', [], ['context' => 'eic_search']),
+        'DESC' => $this->t('Most viewed', [], ['context' => 'eic_search']),
+      ],
+      'its_content_comment_count' => [
+        'label' => $this->t('Comment count', [], ['context' => 'eic_search']),
+        'DESC' => $this->t('Most commented', [], ['context' => 'eic_search']),
+      ],
+      'its_flag_like_content' => [
+        'label' => $this->t('Likes', [], ['context' => 'eic_search']),
+        'DESC' => $this->t('Most liked', [], ['context' => 'eic_search']),
       ],
       'its_' . SolrDocumentProcessor::LAST_FLAGGED_KEY . '_' . FlagType::LIKE_CONTENT => [
         'label' => $this->t('Last liked', [], ['context' => 'eic_search']),
