00452c2b66310ed1eae83b61f3fae21e00dfc4ff
Bernardo Paulino <bernardo.paulino4@gmail.com>
2021-10-11 13:58:05 +0200

    EICNET-1199: Fix gallery file download url in react gallery component.

--- lib/themes/eic_community/react/components/Block/Gallery/FileInfo.jsx
+++ lib/themes/eic_community/react/components/Block/Gallery/FileInfo.jsx
@@ -13,7 +13,7 @@ const FileInfo = ({file, position, length}) => {
             <li> <ImageIcon /> {file.mime_type} </li>
             <li> {file.stats[1].value} </li>
           </ul>
-          <a className={"ecl-link ecl-link--standalone ecl-link--button ecl-link--button-primary ecl-link--icon ecl-link--svg"} href={file.image.src} target="_blank" > <GetAppIcon /> Download </a>
+          <a className={"ecl-link ecl-link--standalone ecl-link--button ecl-link--button-primary ecl-link--icon ecl-link--svg"} href={file.path} target="_blank" > <GetAppIcon /> Download </a>
         </div>
       </div>
       <p className={"ecl-gallery__data__description"}>{file.description}</p>
