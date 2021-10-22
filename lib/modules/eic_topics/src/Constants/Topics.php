e5292564816648c96affbae8b9e5212bffb1a6c5
theodoros.suliotis <tsuliotis@prophets.be>
2021-10-22 15:28:22 +0200

    [EICNET]feat: Exclude first level taxonomy terms for topics

---
+++ lib/modules/eic_topics/src/Constants/Topics.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Drupal\eic_topics\Constants;
+
+/**
+ * Class Topics
+ *
+ * @package Drupal\eic_topics\Constants
+ */
+final class Topics {
+  const TERM_TOPICS_ID = 'topics';
+}
