1251ebb9ffb2c6810d31ca0ec01b6116143ba629
theodoros.suliotis <tsuliotis@prophets.be>
2021-11-01 05:20:02 +0100

    [EICNET-1609]feat: add docblock

--- lib/modules/eic_private_message/src/Constants/PrivateMessage.php
+++ lib/modules/eic_private_message/src/Constants/PrivateMessage.php
@@ -2,6 +2,11 @@
 
 namespace Drupal\eic_private_message\Constants;
 
+/**
+ * Class PrivateMessage
+ *
+ * @package Drupal\eic_private_message\Constants
+ */
 final class PrivateMessage {
 
   public const PRIVATE_MESSAGE_USER_MAIL_KEY = 'private_message_user';
