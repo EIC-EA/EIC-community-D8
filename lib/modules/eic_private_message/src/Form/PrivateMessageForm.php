2dbffb8d8f05b855f181a57d329baf6dd5234a5e
theodoros.suliotis <tsuliotis@prophets.be>
2021-11-04 04:04:47 +0100

    [EICNET-1608]feat: Create new route to contact multiple users in a group

--- lib/modules/eic_private_message/src/Form/PrivateMessageForm.php
+++ lib/modules/eic_private_message/src/Form/PrivateMessageForm.php
@@ -3,13 +3,13 @@
 namespace Drupal\eic_private_message\Form;
 
 use Drupal\Core\Config\ConfigFactory;
-use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Language\LanguageManager;
 use Drupal\Core\Mail\MailManager;
 use Drupal\eic_private_message\Constants\PrivateMessage;
 use Drupal\eic_user\UserHelper;
+use Drupal\group\Entity\GroupInterface;
 use Drupal\user\Entity\User;
 use Drupal\user\UserInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -33,9 +33,6 @@ class PrivateMessageForm extends FormBase {
   /** @var \Drupal\Core\Language\LanguageManager $languageManager */
   private $languageManager;
 
-  /** @var \Drupal\Core\Entity\EntityTypeManagerInterface $entityTypeManager */
-  private $entityTypeManager;
-
   /**
    * @param \Symfony\Component\DependencyInjection\ContainerInterface $container
    *
@@ -47,7 +44,6 @@ class PrivateMessageForm extends FormBase {
       $container->get('config.factory'),
       $container->get('plugin.manager.mail'),
       $container->get('language_manager'),
-      $container->get('entity_type.manager')
     );
   }
 
@@ -58,20 +54,17 @@ class PrivateMessageForm extends FormBase {
    * @param \Drupal\Core\Config\ConfigFactory $config_factory
    * @param \Drupal\Core\Mail\MailManager $mail_manager
    * @param \Drupal\Core\Language\LanguageManager $language_manager
-   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    */
   public function __construct(
     UserHelper $user_helper,
     ConfigFactory $config_factory,
     MailManager $mail_manager,
-    LanguageManager $language_manager,
-    EntityTypeManagerInterface $entity_type_manager
+    LanguageManager $language_manager
   ) {
     $this->userHelper = $user_helper;
     $this->systemSettings = $config_factory->get('system.site');
     $this->mailManager = $mail_manager;
     $this->languageManager = $language_manager;
-    $this->entityTypeManager = $entity_type_manager;
   }
 
   /**
@@ -91,6 +84,7 @@ class PrivateMessageForm extends FormBase {
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
     $user_id = $this->getRouteMatch()->getParameter('user');
+    $group = $this->getRouteMatch()->getParameter('group');
 
     if ($user_id) {
       $user = User::load($user_id);
@@ -116,14 +110,6 @@ class PrivateMessageForm extends FormBase {
       }
     }
 
-    $recipients = $this->getRequest()
-      ->query
-      ->get('recipients_id');
-
-    if (empty($recipients)) {
-      $recipients = [$user_id];
-    }
-
     $form['from'] = [
       '#type' => 'textfield',
       '#title' => $this->t('From', [], ['context' => 'eic_private_message']),
@@ -141,6 +127,22 @@ class PrivateMessageForm extends FormBase {
         'readonly' => 'readonly',
       ],
     ];
+
+    if ($group instanceof GroupInterface) {
+      $form['to_recipients'] = [
+        '#type' => 'entity_autocomplete',
+        '#title' => $this->t('Select users in group', [], ['context' => 'eic_private_message']),
+        '#description' => $this->t('Seperate users by ",".', [], ['context' => 'eic_private_message']),
+        '#tags' => TRUE,
+        '#target_type' => 'group_content',
+        '#selection_handler' => 'default:group_membership',
+        '#selection_settings' => [
+          'filter' => [
+            'gid' => $group->id(),
+          ],
+        ],
+      ];
+    }
 
     $form['subject'] = [
       '#type' => 'textfield',
@@ -162,7 +164,7 @@ class PrivateMessageForm extends FormBase {
 
     $form['recipients'] = [
       '#type' => 'hidden',
-      '#value' => $recipients,
+      '#value' => [$user_id],
     ];
 
     $form['actions']['submit'] = [
@@ -176,13 +178,21 @@ class PrivateMessageForm extends FormBase {
   /**
    * {@inheritdoc}
    *
-   * Send mails to targetted users. Also send a copy if needed.
+   * Send mails to selected users. Also send a copy if needed.
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
     $values = $form_state->getValues();
     $current_langauge = $this->languageManager->getCurrentLanguage()->getId();
 
     $recipients = $values['recipients'];
+
+    if (array_key_exists('to_recipients', $values)) {
+      $recipients = $values['to_recipients'];
+
+      $recipients = array_map(function ($item) {
+        return $item['target_id'];
+      }, $recipients);
+    }
 
     $recipients = array_map(function (int $id) {
       $user = User::load($id);
