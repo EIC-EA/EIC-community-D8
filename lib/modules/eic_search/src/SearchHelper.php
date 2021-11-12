b699bcfe59107018b468051bdb50e14cbf991dae
Ludovic Ramarojaona <ludovic.ramarojaona@blue4you.be>
2021-11-12 14:28:54 +0000

    EICNET-1661: Use routes for show more links instead of path.

--- lib/modules/eic_search/src/SearchHelper.php
+++ lib/modules/eic_search/src/SearchHelper.php
@@ -8,34 +8,24 @@ namespace Drupal\eic_search;
 class SearchHelper {
 
   /**
-   * Builds a string for Solr query params.
+   * Returns an array for Solr query params.
    *
    * @param array $params
    *   An array of key/value to use as Solr query params.
-   * @param bool $include_question_mark
-   *   Whether to include a leading question mark. Default to FALSE.
    *
-   * @return string
-   *   The query param string.
+   * @return array
+   *   The query params.
    */
-  public static function buildSolrQueryParams(array $params, $include_question_mark = FALSE) {
+  public static function buildSolrQueryParams(array $params) {
     $i = 0;
-    $query_string = '';
+    $query_params = [];
 
     foreach ($params as $key => $value) {
-      if ($i > 0) {
-        $query_string .= '&';
-      }
-
-      $query_string .= "f[$i]=$key:$value";
+      $query_params["f[$i]"] = "$key:$value";
       $i++;
     }
 
-    if ($include_question_mark && strlen($query_string) > 0) {
-      $query_string = '?' . $query_string;
-    }
-
-    return $query_string;
+    return $query_params;
   }
 
 }
