d53a9099c791f5ec6168997250d2e09242a93988
Bernardo Paulino <bernardo.paulino4@gmail.com>
2021-11-25 11:11:07 +0100

    EICNET-1510: Create new generic message type to use when an entity has changed status; Send notification message to group owner when SA/SCM unblocks a group.

--- lib/modules/eic_messages/src/Hooks/EntityOperations.php
+++ lib/modules/eic_messages/src/Hooks/EntityOperations.php
@@ -10,7 +10,9 @@ use Drupal\eic_messages\Service\MessageBusInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
- * Class EntityUpdate
+ * Class EntityUpdate.
+ *
+ * Implementations for entity hooks.
  *
  * @package Drupal\eic_messages\Hooks
  */
@@ -19,18 +21,51 @@ class EntityOperations implements ContainerInjectionInterface {
   use StringTranslationTrait;
 
   /**
+   * The content moderation archived state key.
+   *
+   * @var string
+   */
+  const MODERATION_STATE_ARCHIVED = 'archived';
+
+  /**
+   * The content moderation blocked state key.
+   *
+   * @var string
+   */
+  const MODERATION_STATE_BLOCKED = 'blocked';
+
+  /**
+   * The entity type labels to use in notification message.
+   *
+   * @var array
+   */
+  const ENTITY_TYPE_LABELS = [
+    'node' => 'content',
+    'group__group' => 'group',
+    'group__event' => 'event',
+  ];
+
+  /**
+   * The content moderation information service.
+   *
    * @var \Drupal\content_moderation\ModerationInformationInterface
    */
   private $moderationInformation;
 
   /**
+   * The message bus service.
+   *
    * @var \Drupal\eic_messages\Service\MessageBusInterface
    */
   private $messageBus;
 
   /**
+   * Constructs a new EntityOperations object.
+   *
    * @param \Drupal\content_moderation\ModerationInformationInterface $moderationInformation
+   *   The content moderation information service.
    * @param \Drupal\eic_messages\Service\MessageBusInterface $message_bus
+   *   The message bus service.
    */
   public function __construct(
     ModerationInformationInterface $moderationInformation,
@@ -55,26 +90,83 @@ class EntityOperations implements ContainerInjectionInterface {
    */
   public function entityUpdate(EntityInterface $entity) {
     // We do send notifications only for moderated contents.
-    if (!$this->moderationInformation->isModeratedEntity($entity)
-      || $entity->original->get('moderation_state')->value !== 'archived') {
+    if (!$this->moderationInformation->isModeratedEntity($entity)) {
+      return;
+    }
+
+    // Gets the previous content moderation state.
+    $previous_state = $entity->original->get('moderation_state')->value;
+    // Gets the current content moderation state.
+    $current_state = $entity->get('moderation_state')->value;
+    // If the content moderation state didn't change, we do nothing.
+    if ($previous_state === $current_state) {
+      return;
+    }
+
+    // Gets the entity type label to use in the message notification.
+    $entity_type_label = isset(self::ENTITY_TYPE_LABELS[$entity->getEntityTypeId()])
+      ? self::ENTITY_TYPE_LABELS[$entity->getEntityTypeId()]
+      : (isset(self::ENTITY_TYPE_LABELS["{$entity->getEntityTypeId()}__{$entity->bundle()}"])
+          ? self::ENTITY_TYPE_LABELS["{$entity->getEntityTypeId()}__{$entity->bundle()}"]
+          : $entity->getEntityTypeId());
+
+    // By default notification is not skipped.
+    $skip_notification = FALSE;
+    // Default message fields.
+    $message = [
+      'template' => 'notify_content_status_changed',
+      'uid' => $entity->getOwnerId(),
+      'field_message_subject' => NULL,
+      'field_referenced_entity_label' => $entity->label(),
+      'field_entity_prev_status_label' => $previous_state,
+      'field_entity_status_label' => $current_state,
+      'field_entity_url' => [
+        'uri' => $entity->toUrl()->toString(),
+        'title' => $entity->label(),
+      ],
+    ];
+
+    switch ($previous_state) {
+      case self::MODERATION_STATE_ARCHIVED:
+        $message['field_message_subject'] = $this->t(
+          'Your @entity_type "@entity_label" has been published again',
+          [
+            '@entity_type' => $entity_type_label,
+            '@entity_label' => $entity->label(),
+          ]
+        );
+        break;
+
+      case self::MODERATION_STATE_BLOCKED:
+        $message['field_message_subject'] = $this->t(
+          'Your @entity_type "@entity_label" has been unblocked',
+          [
+            '@entity_type' => $entity_type_label,
+            '@entity_label' => $entity->label(),
+          ]
+        );
+        break;
+
+      default:
+        $skip_notification = TRUE;
+        break;
+
+    }
+
+    if ($skip_notification) {
       return;
     }
 
     // Check if the new state is marked as "published".
     $workflow = $this->moderationInformation->getWorkflowForEntity($entity);
     $is_published_state = $workflow->getTypePlugin()
-      ->getState($entity->get('moderation_state')->value)
+      ->getState($current_state)
       ->isPublishedState();
     if (!$is_published_state) {
       return;
     }
 
-    $this->messageBus->dispatch([
-      'template' => 'notify_archived_republished',
-      'uid' => $entity->getOwnerId(),
-      'field_message_subject' => $this->t('Your content is published again'),
-      'field_referenced_entity_label' => $entity->label(),
-    ]);
+    $this->messageBus->dispatch($message);
   }
 
 }
