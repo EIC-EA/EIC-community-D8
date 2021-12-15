a186e4b56b47394795c5cdb8a5d9770398dee16c
Dromadoros <theodoros_40@hotmail.com>
2021-12-15 14:38:51 +0100

    [EICNET-1820]feat: create static method to generate the field widget

--- lib/modules/eic_content/src/Plugin/Field/FieldWidget/EntityTreeWidget.php
+++ lib/modules/eic_content/src/Plugin/Field/FieldWidget/EntityTreeWidget.php
@@ -87,6 +87,8 @@ class EntityTreeWidget extends WidgetBase {
         'disable_top_choices' => FALSE,
         'load_all' => FALSE,
         'ignore_current_user' => FALSE,
+        'target_bundles' => [],
+        'is_required' => FALSE,
       ] + parent::defaultSettings();
   }
 
@@ -95,78 +97,64 @@ class EntityTreeWidget extends WidgetBase {
    */
   public function formElement(FieldItemListInterface $items, $delta, array $element, array &$form, FormStateInterface $form_state) {
     $settings = $this->getFieldSetting('handler_settings');
-    $target_entity = $this->fieldDefinition->getFieldStorageDefinition()
-      ->getSetting('target_type');
-    $target_bundles = array_key_exists('target_bundles', $settings) && $settings['target_bundles'] ?
-      $settings['target_bundles'] :
-      [];
 
-    $target_bundle = reset($target_bundles);
+    $preselected_items = self::formatPreselection($items->referencedEntities());
 
-    $element +=
-      [
-        '#type' => 'entity_autocomplete',
-        '#default_value' => $items->referencedEntities(),
-        '#tags' => TRUE,
-        '#target_type' => $this->getFieldSetting('target_type'),
-        '#maxlength' => 5000,
-        '#element_validate' => [
-          [static::class, 'validate'],
-        ],
-        '#attributes' => [
-          'class' => ['hidden', 'entity-tree-reference-widget'],
-          'data-selected-terms' => json_encode(array_map(function (EntityInterface $entity) {
-            if ($entity instanceof TermInterface) {
-              $parents = $entity->get('parent')->getValue();
-              $parent = reset($parents)['target_id'];
-              $name = $entity->getName();
-            }
+    $options = [
+      'match_top_level_limit' => $this->getSetting('items_to_load'),
+      'items_to_load' => $this->getSetting('items_to_load'),
+      'auto_select_parents' => $this->getSetting('auto_select_parents'),
+      'disable_top_choices' => $this->getSetting('disable_top_choices'),
+      'load_all' => $this->getSetting('load_all'),
+      'ignore_current_user' => $this->getSetting('ignore_current_user'),
+      'target_bundles' => $settings['target_bundles'] ?? [],
+      'is_required' => $this->fieldDefinition->isRequired(),
+    ];
 
-            if ($entity instanceof UserInterface) {
-              $parent = 0;
-              $name = realname_load($entity) . ' ' . '('. $entity->getEmail() .')';
-            }
-
-            return [
-              'name' => $name,
-              'tid' => $entity->id(),
-              'parent' => $parent,
-            ];
-          }, $items->referencedEntities())),
-          'data-translations' => json_encode([
-            'select_value' => $this->t('Select a value', [], ['context' => 'eic_search']),
-            'match_limit' => $this->t(
-              'You can select only <b>@match_limit</b> top-level items.',
-              ['@match_limit' => $this->getSetting('match_top_level_limit')],
-              ['context' => 'eic_search']
-            ),
-            'search' => $this->t('Search', [], ['context' => 'eic_search']),
-            'your_values' => $this->t('Your selected values', [], ['context' => 'eic_search']),
-            'required_field' => $this->t('This field is required', [], ['context' => 'eic_content']),
-          ]),
-          'data-terms-url' => Url::fromRoute('eic_content.entity_tree')
-            ->toString(),
-          'data-terms-url-search' => Url::fromRoute('eic_content.entity_tree_search')
-            ->toString(),
-          'data-terms-url-children' => Url::fromRoute('eic_content.entity_tree_children')
-            ->toString(),
-          'data-match-limit' => $this->getSetting('match_top_level_limit'),
-          'data-items-to-load' => $this->getSetting('items_to_load'),
-          'data-disable-top' => $this->getSetting('disable_top_choices'),
-          'data-load-all' => $this->getSetting('load_all'),
-          'data-ignore-current-user' => $this->getSetting('ignore_current_user'),
-          'data-target-bundle' => $target_bundle,
-          'data-target-entity' => $target_entity,
-          'data-is-required' => (int) $element['#required'],
-          // This allow to search users depending specific conditions
-          // in link SourceType.
-          'data-search-specific-users' => 0,
-        ],
-      ];
+    $element += self::getEntityTreeFieldStructure(
+      $items->referencedEntities(),
+      $this->getFieldSetting('target_type'),
+      $preselected_items,
+      $this->getSetting('items_to_load'),
+      Url::fromRoute('eic_content.entity_tree')->toString(),
+      Url::fromRoute('eic_content.entity_tree_search')->toString(),
+      Url::fromRoute('eic_content.entity_tree_children')->toString(),
+      $options
+    );
 
     $element['#attached']['library'][] = 'eic_community/react-tree-field';
 
     return $element;
+  }
+
+  /**
+   * Formats the preselection for the widget.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface[] $entities
+   *   Array of entities.
+   *
+   * @return string
+   *   A JSON encoded string.
+   */
+  public static function formatPreselection(array $entities) {
+    return json_encode(array_map(function (EntityInterface $entity) {
+      if ($entity instanceof TermInterface) {
+        $parents = $entity->get('parent')->getValue();
+        $parent = reset($parents)['target_id'];
+        $name = $entity->getName();
+      }
+
+      if ($entity instanceof UserInterface) {
+        $parent = 0;
+        $name = realname_load($entity) . ' ' . '('. $entity->getEmail() .')';
+      }
+
+      return [
+        'name' => $name,
+        'tid' => $entity->id(),
+        'parent' => $parent,
+      ];
+    }, $entities));
   }
 
   /**
@@ -213,6 +201,87 @@ class EntityTreeWidget extends WidgetBase {
     }
 
     return $summary;
+  }
+
+  /**
+   * Returns an array for the entity_tree widget.
+   *
+   * This array can be used for widget form elements (Field API) or in custom
+   * forms.
+   *
+   * @param array $default_values
+   * @param string $target_type
+   * @param string $preselected_terms
+   * @param int $number_to_load
+   * @param array $options
+   *   A config array with following optional keys:
+   *   - match_top_level_limit: (int) Maximum number of top-level selection.
+   *   - items_to_load: (int) Number of items to load with load more.
+   *   - auto_select_parents: (bool) Whether to auto-select parents when
+   *     selecting a child.
+   *   - disable_top_choices: (bool) Whether to disable the top-level selection.
+   *   - load_all: (bool) Whether to load all items.
+   *   - ignore_current_user: (bool) Whether to ignore the current user in the
+   *     user tree.
+   *   - target_bundles: (array) The selectable bundles. Defaults to all.
+   *   - is_required: (bool) Indicates if field is required.
+   *
+   * @return array
+   */
+  public static function getEntityTreeFieldStructure (
+    array $default_values,
+    string $target_type,
+    string $preselected_terms,
+    int $number_to_load,
+    string $base_url,
+    string $base_url_search,
+    string $base_url_search_children,
+    array $options = []
+  ): array {
+    /** @var \Drupal\Core\StringTranslation\TranslationManager $translation_manager */
+    $translation_manager = \Drupal::service('string_translation');
+
+    $options += self::defaultSettings();
+
+    // @todo We should be able to work with multiple bundles.
+    $target_bundle = empty($options['target_bundles']) ? '' : reset($options['target_bundles']);
+
+    return [
+      '#type' => 'entity_autocomplete',
+      '#default_value' => $default_values,
+      '#tags' => TRUE,
+      '#target_type' => $target_type,
+      '#maxlength' => 5000,
+      '#element_validate' => [
+        [static::class, 'validate'],
+      ],
+      '#attributes' => [
+        'class' => ['hidden', 'entity-tree-reference-widget'],
+        'data-selected-terms' => $preselected_terms,
+        'data-translations' => json_encode([
+          'select_value' => $translation_manager->translate('Select a value', [], ['context' => 'eic_search']),
+          'match_limit' => $translation_manager->translate(
+            'You can select only <b>@match_limit</b> top-level items.',
+            ['@match_limit' => $options['match_top_level_limit']],
+            ['context' => 'eic_search']
+          ),
+          'search' => $translation_manager->translate('Search', [], ['context' => 'eic_search']),
+          'your_values' => $translation_manager->translate('Your selected values', [], ['context' => 'eic_search']),
+          'required_field' => $translation_manager->translate('This field is required', [], ['context' => 'eic_content']),
+        ]),
+        'data-terms-url' => Url::fromRoute('eic_content.entity_tree')->toString(),
+        'data-terms-url-search' => Url::fromRoute('eic_content.entity_tree_search')->toString(),
+        'data-terms-url-children' => Url::fromRoute('eic_content.entity_tree_children')->toString(),
+        'data-match-limit' => $options['match_top_level_limit'],
+        'data-items-to-load' => $number_to_load,
+        'data-disable-top' => (int) $options['disable_top_choices'],
+        'data-load-all' => (int) $options['load_all'],
+        'data-ignore-current-user' => (int) $options['ignore_current_user'],
+        'data-target-bundle' => $target_bundle,
+        'data-target-entity' => $target_type,
+        'data-is-required' => (int) $options['is_required'],
+      ],
+    ];
   }
 
 }
