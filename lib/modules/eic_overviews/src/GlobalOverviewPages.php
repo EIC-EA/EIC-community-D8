010280c9572ddade5736fc1e21b3a8fee778b5bf
Bernardo Paulino <bernardo.paulino4@gmail.com>
2022-01-17 09:21:03 +0100

    EICNET-852: Show CTA button in groups overview page banner.

--- lib/modules/eic_overviews/src/GlobalOverviewPages.php
+++ lib/modules/eic_overviews/src/GlobalOverviewPages.php
@@ -2,14 +2,19 @@
 
 namespace Drupal\eic_overviews;
 
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\Url;
 use Drupal\eic_overviews\Entity\OverviewPage;
 
 /**
- * Provides functionality for global overview pages.
+ * Service that provides functionality for global overview pages.
  *
  * @package Drupal\eic_overviews
  */
 class GlobalOverviewPages {
+
+  use StringTranslationTrait;
 
   /**
    * ID of the Global search overview page.
@@ -37,6 +42,23 @@ class GlobalOverviewPages {
   const EVENTS = 5;
 
   /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * Constructs a GlobalOverviewPages object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   */
+  public function __construct(EntityTypeManagerInterface $entity_type_manager) {
+    $this->entityTypeManager = $entity_type_manager;
+  }
+
+  /**
    * Returns the URL object for the given global overview page.
    *
    * @param int $page
@@ -44,6 +66,7 @@ class GlobalOverviewPages {
    *
    * @return \Drupal\Core\Url|null
    *   The URL object or NULL if does not apply.
+   *
    * @throws \Drupal\Core\Entity\EntityMalformedException
    */
   public static function getGlobalOverviewPageUrl(int $page) {
@@ -55,7 +78,7 @@ class GlobalOverviewPages {
       return NULL;
     }
 
-    /** @var OverviewPage $overview_entity */
+    /** @var \Drupal\eic_overviews\Entity\OverviewPage $overview_entity */
     $overview_entity = OverviewPage::load(reset($overview_entities));
 
     if (!$overview_entity instanceof OverviewPage) {
@@ -63,6 +86,42 @@ class GlobalOverviewPages {
     }
 
     return $overview_entity->toUrl();
+  }
+
+  /**
+   * Returns the operation links for the given global overview page.
+   *
+   * @param int $page
+   *   The page identifier for which we return the URL.
+   *
+   * @return array
+   *   The array of operation links composed by:
+   *   - title: the operation link title;
+   *   - url: the operation URL string.
+   */
+  public function getGlobalOverviewPageOperations(int $page) {
+    switch ($page) {
+      case GlobalOverviewPages::GROUPS:
+        $entity_id = 'group';
+        $bundle = 'group';
+        $add_route = "entity.$entity_id.add_form";
+        $access_handler = $this->entityTypeManager->getAccessControlHandler($entity_id);
+
+        if ($access_handler->createAccess($bundle)) {
+          $url = is_callable($add_route)
+            ? call_user_func($add_route, $entity_id, $bundle)
+            : Url::fromRoute($add_route, [$entity_id . '_type' => $bundle]);
+
+          $operations[] = [
+            'title' => $this->t("Create a new group"),
+            'url' => $url->toString(),
+          ];
+        }
+        break;
+
+    }
+
+    return $operations;
   }
 
 }
