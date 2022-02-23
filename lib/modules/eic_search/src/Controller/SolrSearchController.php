a2a1cf3f9518ac036096df2b7b1523dc3f30b80e
Dromadoros <theodoros_40@hotmail.com>
2022-02-23 16:04:40 +0100

    [EICNET]fix: add topic id field prefilter topics interests on solr for message

--- lib/modules/eic_search/src/Controller/SolrSearchController.php
+++ lib/modules/eic_search/src/Controller/SolrSearchController.php
@@ -349,7 +349,7 @@ class SolrSearchController extends ControllerBase {
     }
 
     $user_topics_string = implode(' OR ', $user_topics_id);
-    $fq .= " AND (itm_group_field_vocab_topics:($user_topics_string) OR itm_content_field_vocab_topics:($user_topics_string))";
+    $fq .= " AND (itm_group_field_vocab_topics:($user_topics_string) OR itm_content_field_vocab_topics:($user_topics_string) OR itm_message_node_ref_field_vocab_topics:($user_topics_string))";
   }
 
   /**
