c54be91d5c38d93906fb04580fbd74fd81708795
Ludovic Ramarojaona <ludovic.ramarojaona@blue4you.be>
2022-03-02 09:25:05 +0000

    EICNET-1319: Rename OgGroupRef plugin to OgMembership.

--- lib/modules/eic_migrate/src/Plugin/migrate/process/OgMembership.php
+++ lib/modules/eic_migrate/src/Plugin/migrate/process/OgMembership.php
@@ -8,42 +8,39 @@ use Drupal\migrate\ProcessPluginBase;
 use Drupal\migrate\Row;
 
 /**
- * Provides an eic_d7_og_group_ref plugin.
+ * Provides an eic_d7_og_membership plugin.
+ *
+ * This module helps to get the og_membership of an entity.
+ * It will return the group ID(s).
  *
  * Usage:
  *
  * @code
  * process:
  *   bar:
- *     plugin: eic_d7_og_group_ref
+ *     plugin: eic_d7_og_membership
  *     source: nid
  *     entity_type: node
  * @endcode
  *
  * @MigrateProcessPlugin(
- *   id = "eic_d7_og_group_ref"
+ *   id = "eic_d7_og_membership"
  * )
  */
-class OgGroupRef extends ProcessPluginBase {
+class OgMembership extends ProcessPluginBase {
 
   /**
    * {@inheritdoc}
    */
   public function transform($value, MigrateExecutableInterface $migrate_executable, Row $row, $destination_property) {
-    $gids = [];
     $migrate_connection = Database::getConnection('default', 'migrate');
-    // Get the userid, rid and gid from og_users_roles.
-    $results = $migrate_connection->select('og_membership', 'ogm')
+    // Get the group IDs for the given value and entity type.
+    return $migrate_connection->select('og_membership', 'ogm')
       ->condition('entity_type', $this->configuration['entity_type'])
       ->condition('etid', $value)
       ->fields('ogm', ['gid'])
       ->execute()
-      ->fetchAll();
-
-    if (is_array($results) && count($results) > 0) {
-      return reset($results)->gid;
-    }
-    return $gids;
+      ->fetchField();
   }
 
 }
