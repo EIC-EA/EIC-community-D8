ba65f48726f06fb09223789f9247391253827800
Ludovic Ramarojaona <ludovic.ramarojaona@blue4you.be>
2022-03-08 11:58:24 +0000

    EICNET-673: Avoid running custom validation on node forms for workflow.eic_moderated_workflow.

--- lib/modules/eic_moderation/src/Hooks/FormOperations.php
+++ lib/modules/eic_moderation/src/Hooks/FormOperations.php
@@ -5,6 +5,7 @@ namespace Drupal\eic_moderation\Hooks;
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
 use Drupal\Core\DependencyInjection\DependencySerializationTrait;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\StringTranslation\StringTranslationTrait;
 use Drupal\content_moderation\ModerationInformationInterface;
 use Drupal\eic_moderation\Constants\EICContentModeration;
@@ -21,6 +22,13 @@ class FormOperations implements ContainerInjectionInterface {
   use StringTranslationTrait;
 
   /**
+   * The current route match.
+   *
+   * @var \Drupal\Core\Routing\RouteMatchInterface
+   */
+  protected $currentRouteMatch;
+
+  /**
    * The Moderation information service.
    *
    * @var \Drupal\content_moderation\ModerationInformationInterface
@@ -30,10 +38,15 @@ class FormOperations implements ContainerInjectionInterface {
   /**
    * Constructs a new FormOperations object.
    *
+   * @param \Drupal\Core\Routing\RouteMatchInterface $current_route_match
+   *   The current route match.
    * @param \Drupal\content_moderation\ModerationInformationInterface $moderation_information
    *   The Moderation information service.
    */
-  public function __construct(ModerationInformationInterface $moderation_information) {
+  public function __construct(
+    RouteMatchInterface $current_route_match,
+    ModerationInformationInterface $moderation_information) {
+    $this->currentRouteMatch = $current_route_match;
     $this->moderationInformation = $moderation_information;
   }
 
@@ -42,6 +55,7 @@ class FormOperations implements ContainerInjectionInterface {
    */
   public static function create(ContainerInterface $container) {
     return new static(
+      $container->get('current_route_match'),
       $container->get('content_moderation.moderation_information')
     );
   }
@@ -65,8 +79,12 @@ class FormOperations implements ContainerInjectionInterface {
       $form['revision_information']['#weight'] = 100;
     }
 
-    // Add our custom validation handler.
-    $form['#validate'][] = [$this, 'eicModerationFormNodeFormValidate'];
+    // We add our custom validation handler, but not when adding a node inside a
+    // group, where we don't require a log message.
+    if (!in_array($this->currentRouteMatch->getRouteName(), $this->getGroupNodeFormRoutes())) {
+      // Add our custom validation handler.
+      $form['#validate'][] = [$this, 'eicModerationFormNodeFormValidate'];
+    }
   }
 
   /**
@@ -89,6 +107,19 @@ class FormOperations implements ContainerInjectionInterface {
         $form_state->setErrorByName('revision_log', $this->t('Please provide a log message.'));
       }
     }
+  }
+
+  /**
+   * Returns the list of possible routes for node form inside groups.
+   *
+   * @return string[]
+   *   A list of route names.
+   */
+  public function getGroupNodeFormRoutes() {
+    return [
+      'entity.group_content.create_form',
+      'entity.group_content.group_node_add_page',
+    ];
   }
 
 }
