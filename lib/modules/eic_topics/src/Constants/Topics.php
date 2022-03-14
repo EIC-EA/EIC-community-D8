6546161f2d8713167f12151cff523579c9b7e8ea
Dromadoros <theodoros_40@hotmail.com>
2022-03-14 14:59:36 +0100

    [EICNET-2179]fix: Use new SOLR field for group topic name

--- lib/modules/eic_topics/src/Constants/Topics.php
+++ lib/modules/eic_topics/src/Constants/Topics.php
@@ -15,7 +15,7 @@ final class Topics {
 
   const TERM_TOPICS_ID_FIELD_USER_SOLR = 'sm_user_profile_field_vocab_topic_interest_array';
 
-  const TERM_TOPICS_ID_FIELD_GROUP_SOLR = 'ss_group_topic_name';
+  const TERM_TOPICS_ID_FIELD_GROUP_SOLR = 'sm_group_topic_name';
 
   const CONTENT_TYPE_ID_FIELD_SOLR = 'ss_global_content_type';
 
