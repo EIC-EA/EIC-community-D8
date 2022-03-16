00dde7b7e53c9dee28c3a9bb41029b5ceb5b0faf
Dromadoros <theodoros_40@hotmail.com>
2022-03-16 22:17:23 +0100

    [EICNET]feat: Add field to select groups for update permissions

--- lib/modules/eic_admin/src/Form/UpdateAllGroupPermissionsForm.php
+++ lib/modules/eic_admin/src/Form/UpdateAllGroupPermissionsForm.php
@@ -6,6 +6,8 @@ use Drupal\Component\Datetime\TimeInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
+use Drupal\group\Entity\Group;
+use Drupal\group\Entity\GroupInterface;
 use Drupal\group\Entity\GroupType;
 use Drupal\group_permissions\GroupPermissionsManagerInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
@@ -75,6 +77,23 @@ class UpdateAllGroupPermissionsForm extends FormBase {
    * {@inheritdoc}
    */
   public function buildForm(array $form, FormStateInterface $form_state) {
+    $form['groups'] = [
+      '#title' => $this->t('Select multiple groups', [], ['context' => 'eic_admin']),
+      '#description' => $this->t(
+        'Add multiple groups <b>separating them with a comma</b>.
+      If empty, it will select all.'
+      ),
+      '#type' => 'entity_autocomplete',
+      '#target_type' => 'group',
+      '#tags' => TRUE,
+      '#selection_settings' => [
+        'sort' => [
+          'field' => 'label',
+          'direction' => 'ASC',
+        ],
+      ],
+    ];
+
     $form['submit'] = [
       '#type' => 'submit',
       '#value' => $this->t('Rebuild permissions', [], ['context' => 'eic_admin']),
@@ -94,40 +113,45 @@ class UpdateAllGroupPermissionsForm extends FormBase {
    * {@inheritdoc}
    */
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $group_types_update = [
-      'event',
-      'group',
-      'organisation',
-    ];
-
-    foreach ($group_types_update as $group_type) {
-      $this->updatePermissionGroupType($group_type);
-    }
+    $groups = $form_state->getValue('groups');
+    $this->updatePermissionGroupType($groups);
   }
 
   /**
-   * By given group type, update all groups depending permissions from group type.
+   * Update all groups depending permissions from their group type.
    *
-   * @param string $group_type
+   * @param \Drupal\group\Entity\GroupInterface[] $groups
    *   The group type id.
    *
    * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
    * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
    */
-  private function updatePermissionGroupType(string $group_type) {
-    $groups = $this->em->getStorage('group')
-      ->loadByProperties([
-        'type' => $group_type,
-      ]);
-    /** @var GroupType $group_type */
-    $group_type = GroupType::load($group_type);
-    $type_roles = $group_type->getRoles();
+  private function updatePermissionGroupType(?array $groups_id) {
+    // If empty groups field, update all groups.
+    if (empty($groups_id)) {
+      $groups = $this->em->getStorage('group')->loadMultiple();
+    }
+    else {
+      $groups = Group::loadMultiple(
+        array_map(function ($group_id) {
+          return $group_id['target_id'];
+        }, $groups_id)
+      );
+    }
 
     foreach ($groups as $group) {
+      if (!$group instanceof GroupInterface) {
+        continue;
+      }
+
+      /** @var GroupType $group_type */
+      $group_type = $group->getGroupType();
+      $type_roles = $group_type->getRoles();
       $groupPermissions = $this->permissionsManager->loadByGroup($group);
 
-      if (!$groupPermissions)
+      if (!$groupPermissions) {
         continue;
+      }
 
       $permissions = $groupPermissions->getPermissions();
       foreach ($type_roles as $type_role) {
