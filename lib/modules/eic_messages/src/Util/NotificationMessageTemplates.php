eefd90c170c9b4b024723b7460204bee104cf288
Bernardo Paulino <bernardo.paulino4@gmail.com>
2022-03-25 15:07:48 +0100

    EICNET-2034: Create message notification to send to GO/SA/SCM users when a group is deleted + Override group entity class since we need to create the message notifications before deleting all the group content + create custom hook to act before group predelete.

--- lib/modules/eic_messages/src/Util/NotificationMessageTemplates.php
+++ lib/modules/eic_messages/src/Util/NotificationMessageTemplates.php
@@ -27,6 +27,11 @@ final class NotificationMessageTemplates implements MessageIdentifierInterface {
   const GROUP_VISIBILITY_CHANGE = 'notify_group_access_change';
 
   /**
+   * Message template for notifying group visibility changes.
+   */
+  const GROUP_DELETE = 'notify_group_deleted';
+
+  /**
    * {@inheritdoc}
    */
   public static function getMessageTemplatePrimaryKeys(MessageTemplateInterface $message_template) {
