8f19b6dd70c4f809471402e70cf5fe4a09b70844
Ludovic Ramarojaona <ludovic.ramarojaona@iodigital.com>
2022-04-04 11:20:12 +0000

    EICNET-191: Adapt group migration.

--- lib/modules/eic_migrate/src/EventSubscriber/PostMigrationSubscriber.php
+++ lib/modules/eic_migrate/src/EventSubscriber/PostMigrationSubscriber.php
@@ -3,11 +3,11 @@
 namespace Drupal\eic_migrate\EventSubscriber;
 
 use Drupal\Core\Database\Database;
+use Drupal\Core\Entity\ContentEntityInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\migrate\Event\MigrateEvents;
 use Drupal\migrate\Event\MigrateImportEvent;
 use Drupal\migrate\MigrateLookup;
-use Drupal\node\NodeInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
@@ -70,10 +70,61 @@ class PostMigrationSubscriber implements EventSubscriberInterface {
    */
   public function onMigratePostImport(MigrateImportEvent $event) {
     switch ($event->getMigration()->getBaseId()) {
+      case 'upgrade_d7_node_complete_group':
+        $this->completeRelatedGroups($event);
+        break;
+
       case 'upgrade_d7_node_complete_article':
         $this->completeRelatedStories($event);
         break;
 
+    }
+  }
+
+  /**
+   * Sets the correct entity references for field_related_groups field.
+   *
+   * @param \Drupal\migrate\Event\MigrateImportEvent $event
+   *   The import event object.
+   */
+  protected function completeRelatedGroups(MigrateImportEvent $event) {
+    $migration = $event->getMigration();
+
+    // Get the original nodes for which we have related groups.
+    $d7_query = $this->connection->select('field_data_c4m_related_group', 'rg');
+    $d7_query->innerJoin('node', 'n', 'n.nid = rg.c4m_related_group_target_id');
+    $d7_query->fields('n', ['nid', 'vid', 'language']);
+    $d7_query->fields('rg', ['c4m_related_group_target_id', 'entity_id']);
+    $d7_query->addField('rg', 'revision_id', 'parent_vid');
+    $d7_query->addField('rg', 'language', 'parent_language');
+    $d7_query->condition('rg.entity_type', 'node')
+      ->condition('rg.bundle', 'group');
+
+    // Gather all items for each row.
+    // Since we are using the node_complete migration, we assume that source ID
+    // is always:
+    // - destid1: nid.
+    // - destid2: vid.
+    // - destid3: langcode.
+    $node_values = [];
+    foreach ($d7_query->execute()->fetchAll(\PDO::FETCH_ASSOC) as $result) {
+      $source_ids = [$result['nid'], $result['vid'], $result['language']];
+      $parent_source_ids = [
+        $result['entity_id'],
+        $result['parent_vid'],
+        $result['parent_language'],
+      ];
+      $migrated_row = $migration->getIdMap()->getRowBySource($source_ids);
+      $parent_migrated_row = $migration->getIdMap()->getRowBySource($parent_source_ids);
+      // Populate the node values.
+      $node_values[$parent_migrated_row['destid1']][] = $migrated_row['destid1'];
+    }
+
+    // Set the values for each row.
+    foreach ($node_values as $parent_id => $items) {
+      if ($group = $this->entityTypeManager->getStorage('group')->load($parent_id)) {
+        $this->updateEntityReferenceValue($group, 'field_related_groups', $items);
+      }
     }
   }
 
@@ -125,9 +176,9 @@ class PostMigrationSubscriber implements EventSubscriberInterface {
   }
 
   /**
-   * Updates a node's entity references field with passed values.
+   * Updates an entity's entity references field with passed values.
    *
-   * @param \Drupal\node\NodeInterface $node
+   * @param \Drupal\Core\Entity\ContentEntityInterface $entity
    *   The source id values of the migrated item.
    * @param string $field_name
    *   The field name to update.
@@ -138,19 +189,19 @@ class PostMigrationSubscriber implements EventSubscriberInterface {
    *
    * @throws \Drupal\Core\Entity\EntityStorageException
    */
-  protected function updateEntityReferenceValue(NodeInterface $node, string $field_name, array $values, bool $preserve_existing_values = TRUE) {
+  protected function updateEntityReferenceValue(ContentEntityInterface $entity, string $field_name, array $values, bool $preserve_existing_values = TRUE) {
     if (!$preserve_existing_values) {
-      $node->set($field_name, []);
+      $entity->set($field_name, []);
     }
 
-    $changed = $node->getChangedTime();
-    $node->set($field_name, array_unique(array_merge($node->get($field_name)->getValue(), $values)));
-    $node->setNewRevision(FALSE);
-    $node->save();
+    $changed = $entity->getChangedTime();
+    $entity->set($field_name, array_unique(array_merge($entity->get($field_name)->getValue(), $values)));
+    $entity->setNewRevision(FALSE);
+    $entity->save();
 
     // Preserve the changed timestamp.
-    $node->setChangedTime($changed);
-    $node->save();
+    $entity->setChangedTime($changed);
+    $entity->save();
   }
 
 }
