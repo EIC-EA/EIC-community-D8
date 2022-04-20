1402e5892b434ff0310fb4db51d561c2e335dbb4
Korhan Kale <kkale@prophets.be>
2022-04-20 15:50:02 +0200

    [EICNET-961]chore: Displaye visibility labels on news/story teasers

--- lib/modules/eic_search/src/Plugin/Block/SearchOverviewBlock.php
+++ lib/modules/eic_search/src/Plugin/Block/SearchOverviewBlock.php
@@ -382,6 +382,7 @@ class SearchOverviewBlock extends BlockBase implements ContainerFactoryPluginInt
           ),
           'draft' => $this->t('Draft', [], ['context' => 'eic_group']),
           'archived' => $this->t('Archived', [], ['context' => 'eic_group']),
+          'restricted' => $this->t('Restricted', [], ['context' => 'eic_group']),
           'pending' => $this->t('Pending', [], ['context' => 'eic_group']),
           'blocked' => $this->t('Blocked', [], ['context' => 'eic_group']),
           'load_more' => $this->t('Load more', [], ['context' => 'eic_group']),
