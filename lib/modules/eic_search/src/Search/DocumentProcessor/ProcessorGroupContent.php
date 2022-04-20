812c6a0c4f0cddeb6780d43c038e1aa92691ed5c
Dromadoros <theodoros_40@hotmail.com>
2022-04-20 17:58:27 +0200

    [EICNET-2211]feat: Hide news/stories from organisation as anonymous

--- lib/modules/eic_search/src/Search/DocumentProcessor/ProcessorGroupContent.php
+++ lib/modules/eic_search/src/Search/DocumentProcessor/ProcessorGroupContent.php
@@ -20,6 +20,7 @@ class ProcessorGroupContent extends DocumentProcessor {
     $group_parent_url = $fields['ss_global_group_parent_url'] ?? '';
     $group_parent_id = $fields['its_global_group_parent_id'] ?? -1;
     $group_is_published = TRUE;
+    $group_type = '';
 
     if (array_key_exists('its_content__group_content__entity_id_gid', $fields)) {
       if ($group_entity = Group::load($fields['its_content__group_content__entity_id_gid'])) {
@@ -27,11 +28,13 @@ class ProcessorGroupContent extends DocumentProcessor {
         $group_parent_url = $group_entity->toUrl()->toString();
         $group_parent_id = $group_entity->id();
         $group_is_published = $group_entity->isPublished();
+        $group_type = $group_entity->getGroupType()->id();
       }
     }
 
     $this->addOrUpdateDocumentField($document, 'ss_global_group_parent_label', $fields, $group_parent_label);
     $this->addOrUpdateDocumentField($document, 'ss_global_group_parent_url', $fields, $group_parent_url);
+    $this->addOrUpdateDocumentField($document, 'ss_global_group_parent_type', $fields, $group_type);
     $this->addOrUpdateDocumentField($document, 'its_global_group_parent_id', $fields, $group_parent_id);
     $this->addOrUpdateDocumentField($document, 'its_global_group_parent_published', $fields, (int) $group_is_published);
   }
