1a752a66af8417b9452f317efee764fe7bcf9f81
Jimmy Morrens <morrensj@gmail.com>
2022-05-04 09:53:09 +0200

    [CYPRESS] AU cannot post content

---
+++ e2e/cypress/integration/global/permissions-check.spec.js
@@ -0,0 +1,22 @@
+
+
+context('Permissions', () => {
+  // Post files - anonymous user
+  specify('Group files overview show post content button for anonymous user', () => {
+    cy.visit('/groups/cypress-do-not-edit/library')
+    cy.wait(500);
+    cy.get("#group-overview .ecl-collapsible-options__trigger .ecl-button__label").should('not.exist');
+  })
+
+  // Post files - Admin user
+  specify('Group files overview show post content button for admin', () => {
+    cy.login('admin', 'admin');
+    cy.preserveCookie();
+    cy.visit('/groups/cypress-do-not-edit/library')
+    cy.wait(500);
+    cy.scrollTo(0, 300);
+    cy.get("#group-overview .ecl-collapsible-options__trigger .ecl-button__label").should('exist');
+  })
+
+})
+
