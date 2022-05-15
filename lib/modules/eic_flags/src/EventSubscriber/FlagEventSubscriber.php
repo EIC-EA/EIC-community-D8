1ec5b719667f2a03ca7a277743e7dda04f3a44d4
Dromadoros <theodoros_40@hotmail.com>
2022-05-15 17:42:44 +0200

    [EICNET-2043]feat: On new content or like/dislike resync group

--- lib/modules/eic_flags/src/EventSubscriber/FlagEventSubscriber.php
+++ lib/modules/eic_flags/src/EventSubscriber/FlagEventSubscriber.php
@@ -4,11 +4,13 @@ namespace Drupal\eic_flags\EventSubscriber;
 
 use Drupal\Core\Cache\Cache;
 use Drupal\eic_flags\FlagType;
+use Drupal\eic_groups\EICGroupsHelperInterface;
 use Drupal\eic_search\Service\SolrDocumentProcessor;
 use Drupal\flag\Event\FlagEvents;
 use Drupal\flag\Event\FlaggingEvent;
 use Drupal\flag\Event\UnflaggingEvent;
 use Drupal\flag\FlaggingInterface;
+use Drupal\group\Entity\GroupInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
@@ -22,6 +24,20 @@ class FlagEventSubscriber implements EventSubscriberInterface {
    * @var \Drupal\eic_search\Service\SolrDocumentProcessor
    */
   private $solrDocumentProcessor;
+
+  /**
+   * The EIC groups helper service.
+   *
+   * @var \Drupal\eic_groups\EICGroupsHelper
+   */
+  private $groupHelper;
+
+  /**
+   * @param \Drupal\eic_groups\EICGroupsHelperInterface $group_helper
+   */
+  public function __construct(EICGroupsHelperInterface $group_helper) {
+    $this->groupHelper = $group_helper;
+  }
 
   /**
    * @param \Drupal\eic_search\Service\SolrDocumentProcessor|NULL $solr_document_processor
@@ -45,35 +61,68 @@ class FlagEventSubscriber implements EventSubscriberInterface {
    *
    * @param \Drupal\flag\Event\FlaggingEvent $event
    *   The flagging event.
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Drupal\search_api\SearchApiException
    */
   public function onFlag(FlaggingEvent $event) {
-    $this->invalidateDependencies($event->getFlagging());
+    $flagging = $event->getFlagging();
+
+    $entity = $event->getFlagging()->getFlaggable();
+    $group = $this->groupHelper->getGroupFromContent($entity);
+    $extra_resync = [];
+
+    if ($group instanceof GroupInterface && $flagging->getFlagId() === FlagType::RECOMMEND_GROUP) {
+      /** @var \Drupal\eic_search\Service\SolrDocumentProcessor $solr_helper */
+      // Reindex group parent to have correct most active score.
+      $extra_resync[] = $group;
+    }
+
+    $this->invalidateDependencies($flagging, $extra_resync);
   }
 
   /**
    * React to unflagging event.
    *
    * @param \Drupal\flag\Event\UnflaggingEvent $event
+   *
+   * @throws \Drupal\Component\Plugin\Exception\InvalidPluginDefinitionException
+   * @throws \Drupal\Component\Plugin\Exception\PluginNotFoundException
+   * @throws \Drupal\search_api\SearchApiException
    */
   public function flagUnFlag(UnflaggingEvent $event) {
     $flaggings = $event->getFlaggings();
-    $this->invalidateDependencies(reset($flaggings));
+    $flagging = reset($flaggings);
+    $group = $this->groupHelper->getGroupFromContent($flagging->getFlaggable());
+    $extra_resync = [];
+
+    if ($group instanceof GroupInterface && $flagging->getFlagId() === FlagType::RECOMMEND_GROUP) {
+      /** @var \Drupal\eic_search\Service\SolrDocumentProcessor $solr_helper */
+      // Reindex group parent to have correct most active score.
+      $extra_resync[] = $group;
+    }
+
+    $this->invalidateDependencies($flagging, $extra_resync);
   }
 
   /**
    * @param \Drupal\flag\FlaggingInterface $flagging
+   * @param \Drupal\Core\Entity\EntityInterface[] $extra_resync
    *
    * @throws \Drupal\search_api\SearchApiException
    */
-  private function invalidateDependencies(FlaggingInterface $flagging) {
+  private function invalidateDependencies(FlaggingInterface $flagging, array $extra_resync = []) {
     // Some custom variables need to be updated in Solr, so we trigger the
     // re-index of the parent entity.
     if ($this->isReindexTargetedFlag($flagging)) {
       // Get the flagged entity to be updated.
       $parent_entity = $flagging->getFlaggable();
       Cache::invalidateTags($parent_entity->getCacheTags());
-      $this->solrDocumentProcessor->reIndexEntities([$parent_entity]);
+      $extra_resync[] = $parent_entity;
     }
+
+    $this->solrDocumentProcessor->reIndexEntities($extra_resync);
   }
 
   /**
