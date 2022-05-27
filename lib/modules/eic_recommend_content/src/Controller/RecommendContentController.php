e16e20b0b70dd4d958516d13523bcc14bf6d11d0
Bernardo Paulino <bernardo.paulino4@gmail.com>
2022-05-27 19:18:07 +0200

    EICNET-900: Create custom module to implement logic for content recommendation + Implement routes, access checkers and controller to trigger the creation of the recommendation flag + show Recommend flag in group header and content sidebar.

---
+++ lib/modules/eic_recommend_content/src/Controller/RecommendContentController.php
@@ -0,0 +1,188 @@
+<?php
+
+namespace Drupal\eic_recommend_content\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\eic_recommend_content\Services\RecommendContentManager;
+use Drupal\group\Entity\GroupInterface;
+use Drupal\node\NodeInterface;
+use Drupal\oec_group_flex\OECGroupFlexHelper;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\RequestStack;
+
+/**
+ * Returns responses for EIC Recommend Content routes.
+ */
+class RecommendContentController extends ControllerBase {
+
+  /**
+   * The OEC Group flex helper service.
+   *
+   * @var \Drupal\oec_group_flex\OECGroupFlexHelper
+   */
+  protected $oecGroupFlexHelper;
+
+  /**
+   * The Recommend content manager service.
+   *
+   * @var \Drupal\eic_recommend_content\RecommendContentManager
+   */
+  protected $recommendContentManager;
+
+  /**
+   * The current request.
+   *
+   * @var \Symfony\Component\HttpFoundation\Request|null
+   */
+  private $currentRequest;
+
+  /**
+   * Constructs a new RecommendGroupAccessCheck object.
+   *
+   * @param \Drupal\oec_group_flex\OECGroupFlexHelper $oec_group_flex_helper
+   *   The oec_group_flex.helper service.
+   * @param \Drupal\eic_recommend_content\RecommendContentManager $recommend_content_manager
+   *   The Recommend content manager service.
+   * @param \Symfony\Component\HttpFoundation\RequestStack $request_stack
+   *   The request stack service.
+   */
+  public function __construct(
+    OECGroupFlexHelper $oec_group_flex_helper,
+    RecommendContentManager $recommend_content_manager,
+    RequestStack $request_stack
+  ) {
+    $this->oecGroupFlexHelper = $oec_group_flex_helper;
+    $this->recommendContentManager = $recommend_content_manager;
+    $this->currentRequest = $request_stack->getCurrentRequest();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('oec_group_flex.helper'),
+      $container->get('eic_recommend_content.manager'),
+      $container->get('request_stack')
+    );
+  }
+
+  /**
+   * Triggers recommendation on a given entity.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The entity object.
+   */
+  public function recommendEntity(EntityInterface $entity) {
+    $content = json_decode($this->currentRequest->getContent(), TRUE);
+
+    // Working example.
+    // @todo Remove after react implementation.
+    $content['users'] = [2, 3, 4, 5];
+    $content['external_emails'] = [
+      'user1@example.com',
+      'user2@example.com',
+      'user3@example.com',
+    ];
+    $content['message'] = 'Test recommendation for users.';
+
+    if (
+      !isset($content['users']) &&
+      !isset($content['external_emails'])
+    ) {
+      throw new \InvalidArgumentException();
+    }
+
+    if (!isset($content['message'])) {
+      throw new \InvalidArgumentException();
+    }
+
+    $users = [];
+    if (
+      isset($content['users']) &&
+      is_array($content['users'])
+    ) {
+      foreach ($content['users'] as $user) {
+        if (!is_int($user)) {
+          continue;
+        }
+
+        /** @var \Drupal\user\UserInterface $loaded_user */
+        $loaded_user = $this->entityTypeManager()->getStorage('user')
+          ->load($user);
+        if (!$loaded_user || !$loaded_user->getEmail()) {
+          continue;
+        }
+
+        $users[] = $loaded_user;
+      }
+    }
+
+    $external_emails = [];
+    if (
+      isset($content['users']) &&
+      is_string($content['external_emails'])
+    ) {
+      $external_emails = explode(',', trim(strip_tags($content['external_emails'])));
+    }
+
+    try {
+      $this->recommendContentManager->recommend(
+        $entity,
+        $users,
+        $external_emails,
+        $content['message']
+      );
+    }
+    catch (\Exception $exception) {
+      return new JsonResponse([
+        'status' => FALSE,
+        'message' => $exception->getMessage(),
+      ]);
+    }
+
+    return new JsonResponse([
+      'status' => TRUE,
+      'message' => $this->t('The content has been recommended.'),
+    ]);
+  }
+
+  /**
+   * Recommends a node.
+   *
+   * @param \Drupal\node\NodeInterface $node
+   *   The node entity.
+   */
+  public function recommendNode(NodeInterface $node) {
+    $response = $this->recommendEntity($node);
+
+    // @todo Return JSON response after react implementation.
+    $build['content'] = [
+      '#type' => 'item',
+      '#markup' => $this->t('It works!'),
+    ];
+
+    return $build;
+  }
+
+  /**
+   * Recommends a group.
+   *
+   * @param \Drupal\group\Entity\GroupInterface $group
+   *   The group entity.
+   */
+  public function recommendGroup(GroupInterface $group) {
+    $response = $this->recommendEntity($group);
+
+    // @todo Return JSON response after react implementation.
+    $build['content'] = [
+      '#type' => 'item',
+      '#markup' => $this->t('It works!'),
+    ];
+
+    return $build;
+  }
+
+}
---
+++ lib/modules/eic_recommend_content/src/Services/RecommendContentManager.php
@@ -0,0 +1,346 @@
+<?php
+
+namespace Drupal\eic_recommend_content\Services;
+
+use Drupal\Component\Utility\EmailValidator;
+use Drupal\Core\Entity\EntityInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\Session\AccountProxyInterface;
+use Drupal\Core\StringTranslation\StringTranslationTrait;
+use Drupal\Core\Url;
+use Drupal\flag\Entity\Flag;
+use Drupal\flag\FlagCountManagerInterface;
+use Drupal\flag\FlagServiceInterface;
+use Drupal\oec_group_flex\OECGroupFlexHelper;
+
+/**
+ * Provides a service to manage content recommendations.
+ */
+class RecommendContentManager {
+
+  use StringTranslationTrait;
+
+  /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountProxyInterface
+   */
+  protected $currentUser;
+
+  /**
+   * The OEC Group Flex helper service.
+   *
+   * @var \Drupal\oec_group_flex\OECGroupFlexHelper
+   */
+  protected $oecGroupFlexHelper;
+
+  /**
+   * The email validator service.
+   *
+   * @var \Drupal\Component\Utility\EmailValidator
+   */
+  protected $emailValidator;
+
+  /**
+   * The flag service.
+   *
+   * @var \Drupal\flag\FlagServiceInterface
+   */
+  protected $flagService;
+
+  /**
+   * The flag count manager service.
+   *
+   * @var \Drupal\flag\FlagCountManagerInterface
+   */
+  protected $flagCountManager;
+
+  /**
+   * Constructs a RecommendContentManager object.
+   *
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\Core\Session\AccountProxyInterface $current_user
+   *   The current user.
+   * @param \Drupal\oec_group_flex\OECGroupFlexHelper $oec_group_flex_helper
+   *   The OEC Group Flex helper service.
+   * @param \Drupal\Component\Utility\EmailValidator $email_validator
+   *   The email validator service.
+   * @param \Drupal\flag\FlagServiceInterface $flag_service
+   *   The flag service.
+   * @param \Drupal\flag\FlagCountManagerInterface $flag_count_manager
+   *   The flag count manager service.
+   */
+  public function __construct(
+    EntityTypeManagerInterface $entity_type_manager,
+    AccountProxyInterface $current_user,
+    OECGroupFlexHelper $oec_group_flex_helper,
+    EmailValidator $email_validator,
+    FlagServiceInterface $flag_service,
+    FlagCountManagerInterface $flag_count_manager
+  ) {
+    $this->entityTypeManager = $entity_type_manager;
+    $this->currentUser = $current_user;
+    $this->oecGroupFlexHelper = $oec_group_flex_helper;
+    $this->emailValidator = $email_validator;
+    $this->flagService = $flag_service;
+    $this->flagCountManager = $flag_count_manager;
+  }
+
+  /**
+   * Gets recommend content link as renderable array.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The entity object.
+   *
+   * @return array|null
+   *   The renderable array of recommend content link.
+   */
+  public function getRecommendContentLink(EntityInterface $entity) {
+    $support_entity_types = self::getSupportedEntityTypes();
+    if (!array_key_exists($entity->getEntityTypeId(), $support_entity_types)) {
+      return NULL;
+    }
+
+    $flag = $this->flagService->getFlagById(
+      $support_entity_types[$entity->getEntityTypeId()]
+    );
+
+    if (!$flag instanceof Flag) {
+      return NULL;
+    }
+
+    $get_users_url_parameters = [
+      'datasource' => json_encode(['user']),
+      'source_class' => UserTaggingCommentsSourceType::class,
+      'page' => 1,
+    ];
+    $endpoint_url = NULL;
+    $can_recommend_external_users = TRUE;
+
+    $flag_counter = $this->flagCountManager->getEntityFlagCounts($entity);
+    if (!isset($flag_counter[$flag->id()])) {
+      $flag_counter[$flag->id()] = 0;
+    }
+
+    $link_label = $this->t(
+      'Recommend @entity_type (@count)',
+      [
+        '@entity_type' => $entity->getEntityTypeId(),
+        '@count' => $flag_counter[$flag->id()],
+      ]
+    );
+
+    switch ($entity->getEntityTypeId()) {
+      case 'node':
+        $endpoint_url = Url::fromRoute(
+          'eic_recommend_content.recommend_node',
+          [
+            'node' => $entity->id(),
+          ],
+          [
+            'absolute' => TRUE,
+          ]
+        );
+        if (!$endpoint_url->access($this->currentUser)) {
+          return NULL;
+        }
+
+        /** @var \Drupal\group\Entity\GroupContentInterface[] $group_contents */
+        $group_contents = $this->entityTypeManager->getStorage('group_content')
+          ->loadByEntity($entity);
+        $group = NULL;
+        if (!empty($group_contents)) {
+          $group_content = reset($group_contents);
+          $group = $group_content->getGroup();
+          $get_users_url_parameters['current_group'] = $group->id();
+          $can_recommend_external_users = $this->canRecommendExternalUsers($group);
+        }
+        $link_label = $this->t('Recommend content (@count)', ['@count' => $flag_counter[$flag->id()]]);
+        break;
+
+      case 'group':
+        $endpoint_url = Url::fromRoute(
+          'eic_recommend_content.recommend_group',
+          [
+            'group' => $entity->id(),
+          ],
+          [
+            'absolute' => TRUE,
+          ]
+        );
+        if (!$endpoint_url->access($this->currentUser)) {
+          return NULL;
+        }
+
+        $can_recommend_external_users = $this->canRecommendExternalUsers($entity);
+        $get_users_url_parameters['current_group'] = $entity->id();
+        break;
+
+    }
+
+    return $endpoint_url ? [
+      '#theme' => 'eic_recommend_content_link',
+      '#entity_type' => $entity->getEntityTypeId(),
+      '#entity_id' => $entity->id(),
+      '#get_user_url' => Url::fromRoute('eic_search.solr_search', $get_users_url_parameters)->toString(),
+      '#endpoint' => $endpoint_url->toString(),
+      '#can_recommend_external_users' => $can_recommend_external_users,
+      '#translations' => [
+        'link_label' => $link_label,
+      ],
+    ] : NULL;
+  }
+
+  /**
+   * Recommends content to another user(s).
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The entity that will be recommended.
+   * @param \Drupal\user\UserInterface[] $users
+   *   The user accounts to recommend.
+   * @param array $external_emails
+   *   Array of external emails to recommend.
+   * @param string|null $message
+   *   The message that goes in the recommendation.
+   */
+  public function recommend(
+    EntityInterface $entity,
+    array $users = [],
+    array $external_emails = [],
+    ?string $message = ''
+  ) {
+    if (empty($users) && empty($external_emails)) {
+      throw new \InvalidArgumentException('You need to provide users to recommend this content to.');
+    }
+
+    $user_emails = [];
+    // Get platform user emails.
+    foreach ($users as $user) {
+      if (!$entity->access('view', $user)) {
+        continue;
+      }
+
+      $user_emails[] = $user->getEmail();
+    }
+
+    $support_entity_types = self::getSupportedEntityTypes();
+    if (!array_key_exists($entity->getEntityTypeId(), $support_entity_types)) {
+      throw new \InvalidArgumentException('Content recommendation is not enabled for this entity.');
+    }
+
+    $flag = $this->flagService->getFlagById(
+      $support_entity_types[$entity->getEntityTypeId()]
+    );
+
+    if (!$flag instanceof Flag) {
+      throw new \InvalidArgumentException('Content recommendation is not enabled for this entity.');
+    }
+
+    switch ($entity->getEntityTypeId()) {
+      case 'node':
+        // Grab external emails.
+        foreach ($external_emails as $external_email) {
+          if (!$this->emailValidator->isValid($external_email)) {
+            continue;
+          }
+          $user_emails[] = $external_email;
+        }
+        break;
+
+      case 'group':
+        $visibility_settings = $this->oecGroupFlexHelper->getGroupVisibilitySettings($entity);
+        $visibility_type = $visibility_settings['plugin_id'];
+
+        // If group visibility is not public, there is nothing else to do.
+        if ($visibility_type !== 'public') {
+          break;
+        }
+
+        // Grab external emails.
+        foreach ($external_emails as $external_email) {
+          if (!$this->emailValidator->isValid($external_email)) {
+            continue;
+          }
+          $user_emails[] = $external_email;
+        }
+        break;
+
+    }
+
+    $flagging = $this->entityTypeManager->getStorage('flagging')->create([
+      'uid' => $this->currentUser->id(),
+      'session_id' => NULL,
+      'flag_id' => $flag->id(),
+      'entity_id' => $entity->id(),
+      'entity_type' => $entity->getEntityTypeId(),
+      'global' => $flag->isGlobal(),
+      'field_recommend_emails' => implode(',', $user_emails),
+      'field_recommend_message' => $message,
+    ]);
+    $flagging->save();
+  }
+
+  /**
+   * Checks if an entity can be recommended to external users.
+   *
+   * @param \Drupal\Core\Entity\EntityInterface $entity
+   *   The entity object.
+   *
+   * @return bool
+   *   TRUE if entity can be recommended to external users.
+   */
+  public function canRecommendExternalUsers(EntityInterface $entity) {
+    $allowed_group_visibilities = [
+      'public',
+    ];
+    $can_recommend = TRUE;
+
+    switch ($entity->getEntityTypeId()) {
+      case 'node':
+        /** @var \Drupal\group\Entity\GroupContentInterface[] $group_contents */
+        $group_contents = $this->entityTypeManager->getStorage('group_content')
+          ->loadByEntity($entity);
+
+        if (empty($group_contents)) {
+          break;
+        }
+
+        $group_content = reset($group_contents);
+        $group = $group_content->getGroup();
+        $visibility_settings = $this->oecGroupFlexHelper->getGroupVisibilitySettings($group);
+        if (!in_array($visibility_settings['plugin_id'], $allowed_group_visibilities)) {
+          $can_recommend = FALSE;
+        }
+        break;
+
+      case 'group':
+        $visibility_settings = $this->oecGroupFlexHelper->getGroupVisibilitySettings($entity);
+        if (!in_array($visibility_settings['plugin_id'], $allowed_group_visibilities)) {
+          $can_recommend = FALSE;
+        }
+        break;
+
+    }
+
+    return $can_recommend;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function getSupportedEntityTypes() {
+    return [
+      'node' => 'recommend_content',
+      'group' => 'recommend_content_group',
+    ];
+  }
+
+}
