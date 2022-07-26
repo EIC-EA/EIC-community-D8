c8947d742e615166023cde65f2cb3cd70e071e54
Ludovic Ramarojaona <ludovic.ramarojaona@iodigital.com>
2022-07-26 16:02:57 +0000

    EICNET-2643: Make sure to redirect users to the member access page on log in or register CTAs.

--- lib/modules/eic_groups/src/Plugin/Block/EICGroupHeaderBlock.php
+++ lib/modules/eic_groups/src/Plugin/Block/EICGroupHeaderBlock.php
@@ -380,7 +380,7 @@ class EICGroupHeaderBlock extends BlockBase implements ContainerFactoryPluginInt
             'destination' => Url::fromRoute('<current>')->toString(),
           ],
         ];
-        $link['url'] = Url::fromRoute('user.login', [], $login_link_options);
+        $link['url'] = Url::fromRoute('eic_user_login.member_access', [], $login_link_options);
         switch ($joining_methods[0]['plugin_id']) {
           case 'tu_open_method':
             $link['title'] = $this->t('Log in to join group');
