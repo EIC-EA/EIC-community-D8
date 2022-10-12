496c7e420e228698492f0ea1078daf4fea818717
Ludovic Ramarojaona <ludovic.ramarojaona@iodigital.com>
2022-10-12 15:26:26 +0000

    EICNET-2722: Fix missing like flag for groups.

--- lib/modules/eic_groups/src/Plugin/Block/EICGroupHeaderBlock.php
+++ lib/modules/eic_groups/src/Plugin/Block/EICGroupHeaderBlock.php
@@ -13,6 +13,7 @@ use Drupal\Core\Routing\RouteMatchInterface;
 use Drupal\Core\Session\AccountProxyInterface;
 use Drupal\Core\Url;
 use Drupal\eic_content\Constants\DefaultContentModerationStates;
+use Drupal\eic_flags\FlagType;
 use Drupal\eic_group_statistics\GroupStatisticsHelperInterface;
 use Drupal\eic_groups\EICGroupsHelper;
 use Drupal\eic_groups\EICGroupsHelperInterface;
@@ -499,8 +500,8 @@ class EICGroupHeaderBlock extends BlockBase implements ContainerFactoryPluginInt
    */
   public static function getGroupHeaderFlagsIds() {
     return [
-      'follow_group',
-      'recommend_group',
+      FlagType::FOLLOW_GROUP,
+      FlagType::LIKE_GROUP,
     ];
   }
 
