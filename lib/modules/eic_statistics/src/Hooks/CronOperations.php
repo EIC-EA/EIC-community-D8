491ec1bc2ad51feb7576ce8f04081eda148c066a
Bernardo Paulino <bernardo.paulino@iodigital.com>
2022-10-20 17:05:14 +0200

    EICNET-2901: Save node ID in a state cache whenever there is an update on the view counter so that we can re-index the node at a later stage with cron.

--- lib/modules/eic_statistics/src/Hooks/CronOperations.php
+++ lib/modules/eic_statistics/src/Hooks/CronOperations.php
@@ -3,6 +3,10 @@
 namespace Drupal\eic_statistics\Hooks;
 
 use Drupal\Core\DependencyInjection\ContainerInjectionInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\Core\State\StateInterface;
+use Drupal\eic_search\Service\SolrDocumentProcessor;
+use Drupal\eic_statistics\StatisticsHelper;
 use Drupal\eic_statistics\StatisticsStorageInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
@@ -21,13 +25,48 @@ class CronOperations implements ContainerInjectionInterface {
   protected $statisticsStorage;
 
   /**
+   * The entity type manager.
+   *
+   * @var \Drupal\Core\Entity\EntityTypeManagerInterface
+   */
+  protected $entityTypeManager;
+
+  /**
+   * The state service.
+   *
+   * @var \Drupal\Core\State\StateInterface
+   */
+  protected $state;
+
+  /**
+   * The SOLR Document Processor service.
+   *
+   * @var \Drupal\eic_search\Service\SolrDocumentProcessor
+   */
+  protected $solrDocumentProcessor;
+
+  /**
    * Constructs a Cron object.
    *
    * @param \Drupal\eic_statistics\StatisticsStorageInterface $statistics_storage
    *   The EIC statistics storage service.
+   * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
+   *   The entity type manager.
+   * @param \Drupal\Core\State\StateInterface $state
+   *   The state service.
+   * @param \Drupal\eic_search\Service\SolrDocumentProcessor $solr_document_processor
+   *   The SOLR Document Processor service.
    */
-  public function __construct(StatisticsStorageInterface $statistics_storage) {
+  public function __construct(
+    StatisticsStorageInterface $statistics_storage,
+    EntityTypeManagerInterface $entity_type_manager,
+    StateInterface $state,
+    SolrDocumentProcessor $solr_document_processor
+  ) {
     $this->statisticsStorage = $statistics_storage;
+    $this->entityTypeManager = $entity_type_manager;
+    $this->state = $state;
+    $this->solrDocumentProcessor = $solr_document_processor;
   }
 
   /**
@@ -35,7 +74,10 @@ class CronOperations implements ContainerInjectionInterface {
    */
   public static function create(ContainerInterface $container) {
     return new static(
-      $container->get('eic_statistics.storage')
+      $container->get('eic_statistics.storage'),
+      $container->get('entity_type.manager'),
+      $container->get('state'),
+      $container->get('eic_search.solr_document_processor')
     );
   }
 
@@ -44,6 +86,7 @@ class CronOperations implements ContainerInjectionInterface {
    */
   public function cron() {
     $this->updateEntityCountersStatistic();
+    $this->reindexNodeViewCountersStatistic();
   }
 
   /**
@@ -65,6 +108,22 @@ class CronOperations implements ContainerInjectionInterface {
         $this->statisticsStorage->updateEntityCounter($count, $entity_type);
       }
     }
+  }
+
+  /**
+   * Re-indexes node view counter statistics.
+   */
+  public function reindexNodeViewCountersStatistic() {
+    $node_view_counter_state_cache = $this->state->get(StatisticsHelper::NODE_VIEW_COUNTER_REINDEX_STATE_CACHE, []);
+    $this->state->delete(StatisticsHelper::NODE_VIEW_COUNTER_REINDEX_STATE_CACHE);
+    if (empty($node_view_counter_state_cache)) {
+      return;
+    }
+
+    $nodes = $this->entityTypeManager->getStorage('node')
+      ->loadMultiple($node_view_counter_state_cache);
+
+    $this->solrDocumentProcessor->reIndexEntities($nodes);
   }
 
 }
