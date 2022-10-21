6d64dd8c10ef1ab4eaa1b78338780725f6638404
Siggy <siggy.loenders@sigtechnica.com>
2022-10-21 12:20:50 +0200

    EICNET-2922: Changed variable for better readability.

--- web/sites/default/default.settings.k8s.php
+++ web/sites/default/default.settings.k8s.php
@@ -36,7 +36,7 @@ $databases['default']['default'] = [
 ];
 
 // Configuration for Redis
-$redis_enabled = getenv('NO_REDIS') || TRUE;
+$redis_enabled = getenv('USE_REDIS') || TRUE;
 if ($redis_enabled && !\Drupal\Core\Installer\InstallerKernel::installationAttempted() && extension_loaded('redis')) {
   // Set Redis as the default backend for any cache bin not otherwise specified.
   $settings['cache']['default'] = 'cache.backend.redis';
