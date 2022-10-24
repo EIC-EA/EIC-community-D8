a0131031bdefb4aaa39c119e843141d1dad5566f
Ludovic Ramarojaona <ludovic.ramarojaona@iodigital.com>
2022-10-24 16:04:10 +0000

    EICNET-2916: Add program_type terms to content generator.

--- lib/modules/eic_default_content/modules/eic_dummy_content/src/Generator/StoryGenerator.php
+++ lib/modules/eic_default_content/modules/eic_dummy_content/src/Generator/StoryGenerator.php
@@ -34,6 +34,7 @@ class StoryGenerator extends NewsGenerator {
         'field_vocab_topics' => $this->getRandomEntities('taxonomy_term', ['vid' => 'topics'], 1),
         'field_vocab_geo' => $this->getRandomEntities('taxonomy_term', ['vid' => 'geo'], 1),
         'field_vocab_story_type' => $this->getRandomEntities('taxonomy_term', ['vid' => 'story_type'], 2),
+        'field_vocab_program_type' => $this->getRandomEntities('taxonomy_term', ['vid' => 'program_type'], 2),
         'field_disclaimer' => $disclaimer,
         'moderation_state' => EICContentModeration::STATE_PUBLISHED,
       ];
