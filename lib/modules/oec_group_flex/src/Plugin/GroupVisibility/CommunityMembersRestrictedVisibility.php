4912004421276bfa16d6be17da60a73fd1e56d2a
Ludovic Ramarojaona <ludovic.ramarojaona@iodigital.com>
2023-07-20 09:54:06 +0000

    EICNET-2969: Modify the group visibility labels and descriptions.

--- lib/modules/oec_group_flex/src/Plugin/GroupVisibility/CommunityMembersRestrictedVisibility.php
+++ lib/modules/oec_group_flex/src/Plugin/GroupVisibility/CommunityMembersRestrictedVisibility.php
@@ -20,14 +20,14 @@ class CommunityMembersRestrictedVisibility extends RestrictedGroupVisibilityBase
    * {@inheritdoc}
    */
   public function getGroupLabel(GroupTypeInterface $groupType): string {
-    return $this->t('Community members only (The group can be accessed only by logged in users)');
+    return $this->t('Community members only (The @group_type_name can be accessed only by logged in users)', ['@group_type_name' => strtolower($groupType->label())]);
   }
 
   /**
    * {@inheritdoc}
    */
   public function getValueDescription(GroupTypeInterface $groupType): string {
-    return $this->t('This means the restricted group will be visible to each trusted user on the platform.');
+    return $this->t('This means the restricted @group_type_name will be visible to each trusted user on the platform.', ['@group_type_name' => strtolower($groupType->label())]);
   }
 
 }
