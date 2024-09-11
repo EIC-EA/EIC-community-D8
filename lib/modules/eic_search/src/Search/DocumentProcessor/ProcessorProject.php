9199d3ece05e6f9b15c583fb6e29558d1759a28f
Dimitris Kritsotakis <dimkri97@gmail.com>
2024-09-11 16:30:51 +0300

    EICNET-3082: add budget-range facet to project overview

--- lib/modules/eic_search/src/Search/DocumentProcessor/ProcessorProject.php
+++ lib/modules/eic_search/src/Search/DocumentProcessor/ProcessorProject.php
@@ -45,6 +45,38 @@ class ProcessorProject extends DocumentProcessor {
       return $map;
     }, $fields_of_science);
 
+    $total_cost = (float) $group->get('field_project_total_cost')->value;
+
+    $ranges = [
+      '0 - 1.000.000' => [
+        'min' => 0,
+        'max' => 1000000.00,
+      ],
+      '1.000.000 - 2.000.000' => [
+        'min' => 1000000.00,
+        'max' => 2000000.00
+      ],
+      '2.000.000 - 3.000.000' => [
+        'min' => 2000000.00,
+        'max' => 3000000.00,
+      ],
+      '3.000.000+' => [
+        'min' => 3000000.00,
+        'max' => PHP_INT_MAX,
+      ]
+    ];
+
+    $total_cost_solr_field = 'none';
+
+    foreach ($ranges as $string_val => $range) {
+      if ($total_cost > $range['min'] && $total_cost <= $range['max']) {
+        $total_cost_solr_field = $string_val;
+        break;
+      }
+    }
+
+    $document->addField('ss_group_project_field_total_cost', $total_cost_solr_field);
+
     $document->addField('ss_project_cordis_url', EIC_TAXONOMY_CORDIS_BASE_URL . $fields['its_project_grant_agreement_id']);
 
     $this->addOrUpdateDocumentField(
