f91ac8f791c6a4429fcd2444f5607eadd334e8e8
Dimitris Kritsotakis <dimkri97@gmail.com>
2024-09-17 16:46:24 +0300

    EICNET-3089: add service to handle post-migration event

---
+++ lib/modules/eic_projects/src/PostMigrationCordisProject.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Drupal\eic_projects;
+
+use Drupal\Core\File\FileSystemInterface;
+use Drupal\migrate\Event\MigrateImportEvent;
+
+class PostMigrationCordisProject {
+
+  protected FileSystemInterface $fileSystem;
+
+  public function __construct(FileSystemInterface $fileSystem) {
+    $this->fileSystem = $fileSystem;
+  }
+
+  //todo add fn to take group, set it as extracted
+  //  go to directory, delete all extracted files
+  public function handlePostMigration() {
+    $private_dir_path = $this->fileSystem->realpath("private://");
+
+    /** @var \Drupal\eic_projects\Entity\ExtractionRequest[] $requests */
+    $requests = \Drupal::entityTypeManager()
+      ->getStorage('extraction_request')
+      ->loadByProperties(['extraction_status' => 'migrating']);
+
+    foreach ($requests as $request) {
+      /** @var \Drupal\file\FileInterface $zip_file */
+      $zip_file = $request->get('extraction_file')->entity;
+      $filepath = $this->fileSystem->realpath($zip_file->getFileUri());
+      $filename = pathinfo($filepath, PATHINFO_FILENAME);
+
+      $request->set('extraction_status', 'completed')->save();
+      $this->fileSystem->deleteRecursive("$private_dir_path/cordis-xml/export/$filename");
+
+    }
+
+  }
+}
