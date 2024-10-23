7a5bca8c145d557e025167fede953eca02cdcd8d
Dimitris Kritsotakis <dimkri97@gmail.com>
2024-10-23 16:00:40 +0300

    EICNET-3155: fix cases of missing PIC during migration

--- lib/modules/eic_projects/src/Plugin/migrate/source/ProjectsCordisSource.php
+++ lib/modules/eic_projects/src/Plugin/migrate/source/ProjectsCordisSource.php
@@ -87,7 +87,13 @@ class ProjectsCordisSource extends SourcePluginBase {
         $name = $value->getElementsByTagName('legalName')->item(0)->nodeValue;
         $country_code = $xpath->query("relations/regions/region/euCode", $value)->item(0)->nodeValue;
         $country_name = $xpath->query("relations/regions/region[@type='relatedRegion']/name", $value)->item(0)->nodeValue;
-        $pic = $value->getElementsByTagName('id')->item(0)->nodeValue;
+        $pic = $value->getElementsByTagName('id')->item(0);
+        if (!is_null($pic)) {
+          $pic = $pic->nodeValue;
+        }
+        else {
+          $pic = 0;
+        }
         $organisations[] = [
           'name' => $name,
           'country_code' => $country_code,
