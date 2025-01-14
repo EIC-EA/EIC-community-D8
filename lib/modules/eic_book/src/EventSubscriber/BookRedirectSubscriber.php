fdcf88f592dc199b438786ae6574631f6f856e8d
Dimitris Kritsotakis <dimkri97@gmail.com>
2025-01-14 16:37:02 +0200

    EICNET-3163: copy code from BookBreadcrumbBuilder to go through all parent books, overwrite functionality if currentUser is poweruser

--- lib/modules/eic_book/src/EventSubscriber/BookRedirectSubscriber.php
+++ lib/modules/eic_book/src/EventSubscriber/BookRedirectSubscriber.php
@@ -4,9 +4,11 @@ namespace Drupal\eic_book\EventSubscriber;
 
 use Drupal\book\BookManagerInterface;
 use Drupal\Core\Cache\CacheableMetadata;
-use Drupal\Core\Cache\CacheableRedirectResponse;
+use Drupal\Core\Entity\EntityRepositoryInterface;
 use Drupal\Core\Entity\EntityTypeManagerInterface;
 use Drupal\Core\Http\Exception\CacheableAccessDeniedHttpException;
+use Drupal\Core\Session\AccountInterface;
+use Drupal\eic_user\UserHelper;
 use Drupal\node\NodeInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
@@ -32,6 +34,20 @@ class BookRedirectSubscriber implements EventSubscriberInterface {
   protected $entityTypeManager;
 
   /**
+   * The entity repository service.
+   *
+   * @var \Drupal\Core\Entity\EntityRepositoryInterface
+   */
+  protected $entityRepository;
+
+  /**
+   * The current user.
+   *
+   * @var \Drupal\Core\Session\AccountInterface
+   */
+  protected $currentUser;
+
+  /**
    * Constructs a new BookRedirectSubscriber instance.
    *
    * @param \Drupal\book\BookManagerInterface $book_manager
@@ -39,9 +55,11 @@ class BookRedirectSubscriber implements EventSubscriberInterface {
    * @param \Drupal\Core\Entity\EntityTypeManagerInterface $entity_type_manager
    *   The entity type manager.
    */
-  public function __construct(BookManagerInterface $book_manager, EntityTypeManagerInterface $entity_type_manager) {
+  public function __construct(BookManagerInterface $book_manager, EntityTypeManagerInterface $entity_type_manager, EntityRepositoryInterface $entity_repository, AccountInterface $current_user) {
     $this->bookManager = $book_manager;
     $this->entityTypeManager = $entity_type_manager;
+    $this->entityRepository = $entity_repository;
+    $this->currentUser = $current_user;
   }
 
   /**
@@ -66,16 +84,38 @@ class BookRedirectSubscriber implements EventSubscriberInterface {
       return;
     }
 
-    $parent_book_nid = $node->book['pid'];
-    if (!empty($parent_book_nid)) {
-      // we have a parent book. check if published
-      $parent_book_node = $this->entityTypeManager->getStorage('node')->load($parent_book_nid);
-      if (!$parent_book_node->isPublished()) {
-        $cacheability = new CacheableMetadata();
-        $cacheability->addCacheableDependency($parent_book_node);
-        $cacheability->addCacheableDependency($node);
-        throw new CacheableAccessDeniedHttpException($cacheability, 'Parent book is not published.');
+    if (UserHelper::isPowerUser($this->currentUser)) {
+      return;
+    }
+
+    $book_nids = [];
+    $result = [];
+    $cacheability = new CacheableMetadata();
+    $cacheability->addCacheableDependency($node);
+
+    $book = $node->book;
+    $depth = 1;
+    // We skip the current node.
+    while (!empty($book['p' . ($depth + 1)])) {
+      $book_nids[] = $book['p' . $depth];
+      $depth++;
+    }
+    /** @var \Drupal\node\NodeInterface[] $parent_books */
+    $parent_books = $this->entityTypeManager->getStorage('node')->loadMultiple($book_nids);
+    $parent_books = array_map([$this->entityRepository, 'getTranslationFromContext'], $parent_books);
+    if (count($parent_books) > 0) {
+      $depth = 1;
+      while (!empty($book['p' . ($depth + 1)])) {
+        if (!empty($parent_books[$book['p' . $depth]]) && ($parent_book = $parent_books[$book['p' . $depth]])) {
+          $result[$parent_book->id()] = $parent_book->isPublished();
+          $cacheability->addCacheableDependency($parent_book);
+        }
+        $depth++;
       }
+    }
+
+    if (in_array(FALSE, array_values($result))) {
+      throw new CacheableAccessDeniedHttpException($cacheability, 'Parent book is not published.');
     }
   }
 
