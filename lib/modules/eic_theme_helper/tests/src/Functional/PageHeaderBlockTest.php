54a43abaf57212445874a2a439d1f06cffe58fc0
Dimitris Kritsotakis <dimkri97@gmail.com>
2025-02-19 17:57:39 +0200

    rector update eic_theme_helper tests

--- lib/modules/eic_theme_helper/tests/src/Functional/PageHeaderBlockTest.php
+++ lib/modules/eic_theme_helper/tests/src/Functional/PageHeaderBlockTest.php
@@ -17,6 +17,11 @@ class PageHeaderBlockTest extends BrowserTestBase {
   /**
    * {@inheritdoc}
    */
+  protected $defaultTheme = 'stark';
+
+  /**
+   * {@inheritdoc}
+   */
   protected static $modules = [
     'block',
     'entity_test',
@@ -27,7 +32,7 @@ class PageHeaderBlockTest extends BrowserTestBase {
   /**
    * {@inheritdoc}
    */
-  public function setUp() {
+  public function setUp(): void {
     parent::setUp();
 
     // Enable oe_theme and set it as default.
