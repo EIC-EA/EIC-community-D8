c31a8cdeee12401fde60cdc15a70c49872858074
Dimitris Kritsotakis <dimkri97@gmail.com>
2025-03-05 17:23:18 +0200

    EICNET-3162: add condition check to url access too

--- lib/modules/eic_groups/src/Form/OrganisationEditDescriptionForm.php
+++ lib/modules/eic_groups/src/Form/OrganisationEditDescriptionForm.php
@@ -80,8 +80,8 @@ class OrganisationEditDescriptionForm extends FormBase {
   }
 
   public function access(AccountInterface $account) {
-    if ($this->group->bundle() !== 'organisation') {
-      return AccessResult::forbidden('Only applicable for organisation groups.');
+    if ((!($this->group instanceof GroupInterface)) || $this->group->bundle() !== 'organisation') {
+      return AccessResult::forbidden('Only applicable for organisation group entities.');
     }
     $permission = 'edit organisation description';
     $membership = $this->group->getMember($account);
