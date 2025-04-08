7accd532a6a3237f1df57c7e8806a8b6ed2c47de
arismag <aris@pointblank.gr>
2025-04-08 12:37:19 +0300

    EICNET-3187: Add Members by topic of expertise graph.

--- lib/modules/eic_dashboards/src/Constants/DashboardFilters.php
+++ lib/modules/eic_dashboards/src/Constants/DashboardFilters.php
@@ -11,4 +11,6 @@ final class DashboardFilters {
   const DASHBOARD_MEMBERS_LIST_COUNTRY = 'field_location_address_country_code[]';
   const DASHBOARD_MEMBERS_LIST_USER_TYPE = 'field_vocab_user_type_target_id[]';
   const DASHBOARD_MEMBERS_LIST_TOPIC_OF_INTEREST = 'field_vocab_topic_interest_target_id[]';
+  const DASHBOARD_MEMBERS_LIST_TOPIC_OF_EXPERTISE = 'field_vocab_topic_expertise_target_id[]';
+
 }
