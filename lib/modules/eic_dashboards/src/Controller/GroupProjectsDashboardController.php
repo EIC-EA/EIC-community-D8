84df60528578a4853500f59f6c23489dc97dc0f5
George Kaffezas <georgek@pointblank.gr>
2025-04-29 11:27:13 +0300

    EICNET-3192: Create Controller for Projects, menu, routing and first data.

---
+++ lib/modules/eic_dashboards/src/Controller/GroupProjectsDashboardController.php
@@ -0,0 +1,100 @@
+<?php
+
+namespace Drupal\eic_dashboards\Controller;
+
+use Drupal\Core\Controller\ControllerBase;
+use Drupal\eic_dashboards\Services\DashboardBuilderInterface;
+use Drupal\eic_dashboards\Services\DashboardHelperInterface;
+use Drupal\eic_dashboards\Services\GroupStatisticsInterface;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Displays statistics for Group type "Project".
+ */
+class GroupProjectsDashboardController extends ControllerBase {
+
+  /**
+   * The dashboards builder service.
+   *
+   * @var \Drupal\eic_dashboards\Services\DashboardBuilderInterface
+   */
+  protected DashboardBuilderInterface $dashboardBuilder;
+
+  /**
+   * The dashboard helper service.
+   *
+   * @var \Drupal\eic_dashboards\Services\DashboardHelperInterface
+   */
+  protected DashboardHelperInterface $dashboardHelper;
+
+  /**
+   * The content statistics service.
+   *
+   * @var \Drupal\eic_dashboards\Services\GroupStatisticsInterface
+   */
+  protected GroupStatisticsInterface $groupStatistics;
+
+  /**
+   * {@inheritdoc}
+   */
+  public function __construct(
+    DashboardBuilderInterface $dashboardBuilder,
+    DashboardHelperInterface $dashboardHelper,
+    GroupStatisticsInterface $groupStatistics,
+  )
+  {
+    $this->dashboardBuilder = $dashboardBuilder;
+    $this->dashboardHelper = $dashboardHelper;
+    $this->groupStatistics = $groupStatistics;
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public static function create(ContainerInterface $container) {
+    return new static(
+      $container->get('eic_dashboards.builder'),
+      $container->get('eic_dashboards.helper'),
+      $container->get('eic_dashboards.group_statistics'),
+    );
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function page(): array {
+    // Specify current group type.
+    $groupType = 'project';
+
+    // Define days, range, membership and flags.
+    $lastDaysLimit = 90;
+    $topGroupsLimit = 10;
+    $membershipType = 'project-group_membership';
+
+    // Number of groups.
+    $numberOfGroupsData = $this->groupStatistics->getNumberOfGroups($groupType);
+    $numberOfGroups = $this->dashboardBuilder->numberAndLink($this->t('Total @groups', ['@group' => $groupType]),
+      $numberOfGroupsData, '');
+
+    // Number of groups created in past days.
+    $numberOfGroupsPastDaysData = $this->groupStatistics->getNumberOfGroupsPastDays($groupType, $lastDaysLimit);
+    $numberOfGroupsPastDays = $this->dashboardBuilder->numberAndLink($this->t('New @groups - last @days days', ['@group' => $groupType, '@days' =>
+      $lastDaysLimit]), $numberOfGroupsPastDaysData, '');
+
+    // Section 1.
+    $section1Build = [
+      $this->dashboardBuilder->columns([
+        $numberOfGroups,
+        $numberOfGroupsPastDays,
+      ], 3),
+    ];
+
+    $build = [
+      'content' => [
+        $section1Build,
+      ],
+    ];
+
+    return $build;
+  }
+}
