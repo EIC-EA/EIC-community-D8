1a37c1a5e2da4fc646379a807c21e360d67b91ef
Dimitris Kritsotakis <dimkri97@gmail.com>
2025-05-08 15:50:57 +0300

    EICNET-3341: make content field plugins extend from the new base class. get node id and type from $values->_entity

--- lib/modules/eic_dashboards/src/Plugin/views/field/ContentFlagFollow.php
+++ lib/modules/eic_dashboards/src/Plugin/views/field/ContentFlagFollow.php
@@ -3,63 +3,33 @@
 namespace Drupal\eic_dashboards\Plugin\views\field;
 
 
-use Drupal\Core\Database\Connection;
-use Drupal\Core\Url;
-use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\group\Entity\GroupContent;
 use Drupal\views\ResultRow;
-use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * Field handler to show number of content flag follows.
  *
  * @ingroup views_field_handlers
  *
- * @ViewsField("content_flag_follows")
+ * @ViewsField("flag_follow_content")
  */
-class ContentFlagFollow extends FieldPluginBase {
-
-  public function __construct($configuration, $plugin_id, $plugin_definition, private readonly Connection $connection) {
-    parent::__construct($configuration, $plugin_id, $plugin_definition);
-  }
-
-  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
-    return new static(
-      $configuration,
-      $plugin_id,
-      $plugin_definition,
-      $container->get('database')
-    );
-  }
-
-  /**
-   * @inheritDoc
-   */
-  public function query() {
-    // Leave empty to avoid a query on this field.
-  }
+class ContentFlagFollow extends FlagPluginBase {
 
   /**
    * @inheritdoc
    */
   public function render(ResultRow $values) {
-    $string = 'N/A';
-    $nid = $values->node_field_data_group_content_field_data_nid;
+    $entity = $values->_entity;
+    if ($entity instanceof GroupContent) {
+      $entity = $entity->getEntity();
+      /** @var \Drupal\node\NodeInterface $entity */
 
-    if ($nid) {
-      $query = $this->connection->select('flag_counts', 'fc');
-      $query->addField('fc', 'count');
-      $query->condition('fc.entity_type', 'node')
-        ->condition('fc.entity_id', $nid)
-        ->condition('fc.flag_id', 'follow_content');
-
-      $results = $query->execute()->fetchAssoc();
-      if (!empty($results)) {
-        $string = $results['count'];
-      }
+      return $this->getFlagResults(
+        $entity->id(),
+        $entity->getEntityType()->id()
+      );
     }
-
-    return $string;
-
+    return 'N/A';
   }
 
 }
