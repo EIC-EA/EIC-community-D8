d883615400b0b698d6bb6e1dfb4074c7fa69daf5
Dimitris Kritsotakis <dimkri97@gmail.com>
2025-05-08 14:15:28 +0300

    EICNET-3341: add new view field plugin that counts flags per node row

---
+++ lib/modules/eic_dashboards/src/Plugin/views/field/ContentFlagFollow.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace Drupal\eic_dashboards\Plugin\views\field;
+
+
+use Drupal\Core\Database\Connection;
+use Drupal\Core\Url;
+use Drupal\views\Plugin\views\field\FieldPluginBase;
+use Drupal\views\ResultRow;
+use Symfony\Component\DependencyInjection\ContainerInterface;
+
+/**
+ * Field handler to show number of content flag follows.
+ *
+ * @ingroup views_field_handlers
+ *
+ * @ViewsField("content_flag_follows")
+ */
+class ContentFlagFollow extends FieldPluginBase {
+
+  public function __construct($configuration, $plugin_id, $plugin_definition, private readonly Connection $connection) {
+    parent::__construct($configuration, $plugin_id, $plugin_definition);
+  }
+
+  public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition) {
+    return new static(
+      $configuration,
+      $plugin_id,
+      $plugin_definition,
+      $container->get('database')
+    );
+  }
+
+  /**
+   * @inheritDoc
+   */
+  public function query() {
+    // Leave empty to avoid a query on this field.
+  }
+
+  /**
+   * @inheritdoc
+   */
+  public function render(ResultRow $values) {
+    $string = 'N/A';
+    $nid = $values->node_field_data_group_content_field_data_nid;
+
+    if ($nid) {
+      $query = $this->connection->select('flag_counts', 'fc');
+      $query->addField('fc', 'count');
+      $query->condition('fc.entity_type', 'node')
+        ->condition('fc.entity_id', $nid)
+        ->condition('fc.flag_id', 'follow_content');
+
+      $results = $query->execute()->fetchAssoc();
+      if (!empty($results)) {
+        $string = $results['count'];
+      }
+    }
+
+    return $string;
+
+  }
+
+}
