81387ce09ad945c430982f8ad92ac1bc936f8419
Dimitris Kritsotakis <dimkri97@gmail.com>
2025-05-14 11:31:54 +0300

    EICNET-3343: Accumulative dashboards  (#2349)
    * EICNET-3343: add constants per dashboard type
    
    * EICNET-3343: add db schema for custom table
    
    * EICNET-3343: add a service for use on accumulative charts data calculation
    
    * EICNET-3343: write a hook_deploy for members accumulative stats data population
    
    * EICNET-3343: simplify hook deploy, create new function to do the batch processing
    
    * EICNET-3343: add hook deploy for groups dashboard
    
    * EICNET-3343: clone $entity_query for count()
    
    * EICNET-3343: add condition for group type group
    
    * EICNET-3343: add hook_deploy for group type event
    
    * EICNET-3343: add hook_deploy for group type organisation/project and content type document,story,discussion
    
    * EICNET-3343: php code
    
    * EICNET-3343: add comments
    
    * EICNET-3343: D10 - fix patch
    
    * EICNET-3343: fix dashboard_type on hook_deploy_0001()
    
    * EICNET-3343: add method to get the data from database per dashboard_type
    
    * EICNET-3343: inject dateFormatter
    
    * EICNET-3343: fix entityquery on discussions hook
    
    * EICNET-3343: tmp

---
+++ lib/modules/eic_dashboards/eic_dashboards.deploy.php
@@ -0,0 +1,208 @@
+<?php
+
+use Drupal\Core\Entity\Query\QueryInterface;
+use Drupal\eic_dashboards\Constants\DashboardsDatabase;
+
+/**
+ * Populate eic_dashboards for members dashboard.
+ */
+function eic_dashboards_deploy_0001_members_past_stats(array &$sandbox) {
+
+  $entity_type_id = 'user';
+  $entity_query = \Drupal::entityQuery($entity_type_id)
+    ->condition('status', '1')
+    ->accessCheck(FALSE);
+
+  _eic_dashboards_populate_database_batch_helper(
+    $sandbox,
+    $entity_query,
+    50,
+    $entity_type_id,
+    DashboardsDatabase::MEMBERS_DASHBOARD_TYPE
+  );
+
+}
+
+/**
+ * Populate eic_dashboards for groups dashboard.
+ */
+function eic_dashboards_deploy_0002_groups_past_stats(array &$sandbox) {
+
+  $entity_type_id = 'group';
+  $entity_query = \Drupal::entityQuery($entity_type_id)
+    ->condition('type', 'group')
+    ->accessCheck(FALSE);
+
+  _eic_dashboards_populate_database_batch_helper(
+    $sandbox,
+    $entity_query,
+    50,
+    $entity_type_id,
+    DashboardsDatabase::GROUPS_DASHBOARD_TYPE)
+  ;
+
+}
+
+/**
+ * Populate eic_dashboards for events dashboard.
+ */
+function eic_dashboards_deploy_0003_events_past_stats(array &$sandbox) {
+
+  $entity_type_id = 'group';
+  $entity_query = \Drupal::entityQuery($entity_type_id)
+    ->condition('type', 'event')
+    ->accessCheck(FALSE);
+
+  _eic_dashboards_populate_database_batch_helper(
+    $sandbox,
+    $entity_query,
+    50,
+    $entity_type_id,
+    DashboardsDatabase::EVENTS_DASHBOARD_TYPE
+  );
+
+}
+
+/**
+ * Populate eic_dashboards for organisations dashboard.
+ */
+function eic_dashboards_deploy_0004_organisations_past_stats(array &$sandbox) {
+
+  $entity_type_id = 'group';
+  $entity_query = \Drupal::entityQuery($entity_type_id)
+    ->condition('type', 'organisation')
+    ->accessCheck(FALSE);
+
+  _eic_dashboards_populate_database_batch_helper(
+    $sandbox,
+    $entity_query,
+    50,
+    $entity_type_id,
+    DashboardsDatabase::ORGANISATIONS_DASHBOARD_TYPE
+  );
+
+}
+
+/**
+ * Populate eic_dashboards for projects dashboard.
+ */
+function eic_dashboards_deploy_0005_projects_past_stats(array &$sandbox) {
+
+  $entity_type_id = 'group';
+  $entity_query = \Drupal::entityQuery($entity_type_id)
+    ->condition('type', 'project')
+    ->accessCheck(FALSE);
+
+  _eic_dashboards_populate_database_batch_helper(
+    $sandbox,
+    $entity_query,
+    50,
+    $entity_type_id,
+    DashboardsDatabase::PROJECTS_DASHBOARD_TYPE
+  );
+
+}
+
+/**
+ * Populate eic_dashboards for documents dashboard.
+ */
+function eic_dashboards_deploy_0006_documents_past_stats(array &$sandbox) {
+
+  $entity_type_id = 'node';
+  $entity_query = \Drupal::entityQuery($entity_type_id)
+    ->condition('type', 'document')
+    ->accessCheck(FALSE);
+
+  _eic_dashboards_populate_database_batch_helper(
+    $sandbox,
+    $entity_query,
+    50,
+    $entity_type_id,
+    DashboardsDatabase::DOCUMENTS_DASHBOARD_TYPE
+  );
+
+}
+
+/**
+ * Populate eic_dashboards for stories dashboard.
+ */
+function eic_dashboards_deploy_0007_stories_past_stats(array &$sandbox) {
+
+  $entity_type_id = 'node';
+  $entity_query = \Drupal::entityQuery($entity_type_id)
+    ->condition('type', 'story')
+    ->accessCheck(FALSE);
+
+  _eic_dashboards_populate_database_batch_helper(
+    $sandbox,
+    $entity_query,
+    50,
+    $entity_type_id,
+    DashboardsDatabase::STORIES_DASHBOARD_TYPE
+  );
+
+}
+
+/**
+ * Populate eic_dashboards for discussions dashboard.
+ */
+function eic_dashboards_deploy_0008_discussions_past_stats(array &$sandbox) {
+
+  $entity_type_id = 'node';
+  $entity_query = \Drupal::entityQuery($entity_type_id)
+    ->condition('type', 'discussion')
+    ->accessCheck(FALSE);
+
+  _eic_dashboards_populate_database_batch_helper(
+    $sandbox,
+    $entity_query,
+    50,
+    $entity_type_id,
+    DashboardsDatabase::DISCUSSIONS_DASHBOARD_TYPE
+  );
+
+}
+
+function _eic_dashboards_populate_database_batch_helper(array &$sandbox, QueryInterface $entity_query, $entities_per_batch, $entity_type_id, $dashboard_type) {
+
+  $count_entity_query = clone $entity_query;
+  if (!isset($sandbox['total'])) {
+    $sandbox['total'] = $count_entity_query->count()->execute();
+    $sandbox['current'] = 0;
+
+    if (empty($sandbox['total'])) {
+      $sandbox['#finished'] = 1;
+      return;
+    }
+  }
+
+  $ids = $entity_query
+    ->range($sandbox['current'], $entities_per_batch)
+    ->execute();
+  if (empty($ids)) {
+    $sandbox['#finished'] = 1;
+    return;
+  }
+
+  foreach ($ids as $id) {
+    $entity = \Drupal::entityTypeManager()->getStorage($entity_type_id)->load($id);
+    $monthKey = \Drupal::service('date.formatter')->format($entity->get('created')->value, 'custom', 'Y-m') . '-01';
+    \Drupal::service('eic_dashboards.cumulative')->insertOrUpdate($dashboard_type, $monthKey);
+    $sandbox['current']++;
+  }
+
+  \Drupal::messenger()
+    ->addMessage($sandbox['current'] . ' entities processed.');
+
+  if ($sandbox['current'] >= $sandbox['total']) {
+    $sandbox['#finished'] = 1;
+    // We are done with populating the database. We need to fix the records so
+    // each month we add the previous count plus the running's.
+    \Drupal::service('eic_dashboards.cumulative')->calculatePastStats($dashboard_type);
+  }
+  else {
+    $sandbox['#finished'] = ($sandbox['current'] / $sandbox['total']);
+  }
+
+
+}
