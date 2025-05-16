bc2ecdafe9c71ab5728d316f9f3d0036994c0fd2
Dimitris Kritsotakis <dimkri97@gmail.com>
2025-05-16 15:53:21 +0300

    EICNET-3199: add cache to individual group dashboard

--- lib/modules/eic_dashboards/src/Controller/IndividualGroupDashboardController.php
+++ lib/modules/eic_dashboards/src/Controller/IndividualGroupDashboardController.php
@@ -122,142 +122,158 @@ class IndividualGroupDashboardController extends ControllerBase {
    * Group dashboard page.
    */
   public function page(GroupInterface $group): array {
-    // Define past days limit.
-    $lastDaysLimit = 30;
+    if ($cache = $this->cache()->get('dashboard:group:' . $group->id())) {
+      $content = $cache->data;
+    }
+    else {
+      // Define past days limit.
+      $lastDaysLimit = 30;
 
-    // Group members.
-    $groupMembersData = $this->groupStatistics->getGroupMembers($group);
-    $groupMembers = $this->dashboardBuilder->numberAndLink($this->t('Total members'), $groupMembersData, '');
+      // Group members.
+      $groupMembersData = $this->groupStatistics->getGroupMembers($group);
+      $groupMembers = $this->dashboardBuilder->numberAndLink($this->t('Total members'), $groupMembersData, '');
 
-    // Group members that joined in past days.
-    $groupMembersJoinedInPastDaysData = $this->groupStatistics->getGroupMembersRegisteredPastDays($group,
-      $lastDaysLimit);
-    $groupMembersJoinedInPastDays = $this->dashboardBuilder->numberAndLink($this->t('Joined - past @limit days', ['@limit' => $lastDaysLimit]), $groupMembersJoinedInPastDaysData, '');
+      // Group members that joined in past days.
+      $groupMembersJoinedInPastDaysData = $this->groupStatistics->getGroupMembersRegisteredPastDays($group,
+        $lastDaysLimit);
+      $groupMembersJoinedInPastDays = $this->dashboardBuilder->numberAndLink($this->t('Joined - past @limit days', ['@limit' => $lastDaysLimit]), $groupMembersJoinedInPastDaysData, '');
 
-    // Group members that logged in past days.
-    $groupMembersLoggedInPastDaysData = $this->groupStatistics->getGroupMembersLoggedPastDays($group, $lastDaysLimit);
-    $groupMembersLoggedInPastDays = $this->dashboardBuilder->numberAndLink($this->t('Logged in - past @limit days', ['@limit' => $lastDaysLimit]), $groupMembersLoggedInPastDaysData, '');
+      // Group members that logged in past days.
+      $groupMembersLoggedInPastDaysData = $this->groupStatistics->getGroupMembersLoggedPastDays($group, $lastDaysLimit);
+      $groupMembersLoggedInPastDays = $this->dashboardBuilder->numberAndLink($this->t('Logged in - past @limit days', ['@limit' => $lastDaysLimit]), $groupMembersLoggedInPastDaysData, '');
 
-    $section1Content = [
-      $this->dashboardBuilder->columns([
-        $groupMembers,
-        $groupMembersJoinedInPastDays,
-        $groupMembersLoggedInPastDays,
-      ], 3),
-    ];
+      $section1Content = [
+        $this->dashboardBuilder->columns([
+          $groupMembers,
+          $groupMembersJoinedInPastDays,
+          $groupMembersLoggedInPastDays,
+        ], 3),
+      ];
 
-    // Section 1.
-    $section1Title = 'Members metrics';
-    $section1Border = FALSE;
-    $section1Build = [$this->dashboardBuilder->dashboardSection($section1Title, '', $section1Content, 'members', $section1Border)];
+      // Section 1.
+      $section1Title = 'Members metrics';
+      $section1Border = FALSE;
+      $section1Build = [$this->dashboardBuilder->dashboardSection($section1Title, '', $section1Content, 'members', $section1Border)];
 
-    // Group members by country.
-    $groupMembersByCountryData = $this->dashboardHelper->jsonEncodeCategoriesSeries
-    ($this->dashboardHelper->transformIdCountToCategoriesSeries
-    ($this->groupStatistics->getGroupMembersGroupedByCountry($group, 'id', DashboardFilters::DASHBOARD_MEMBERS_LIST_GROUP_ID)));
-    $groupMembersByCountryChart = $this->dashboardBuilder->chartColumn($this->t('Members by country'), $groupMembersByCountryData, false);
+      // Group members by country.
+      $groupMembersByCountryData = $this->dashboardHelper->jsonEncodeCategoriesSeries
+      ($this->dashboardHelper->transformIdCountToCategoriesSeries
+      ($this->groupStatistics->getGroupMembersGroupedByCountry($group, 'id', DashboardFilters::DASHBOARD_MEMBERS_LIST_GROUP_ID)));
+      $groupMembersByCountryChart = $this->dashboardBuilder->chartColumn($this->t('Members by country'), $groupMembersByCountryData, FALSE);
 
-    $section2Build = [
-      $this->dashboardBuilder->columns([
-        $groupMembersByCountryChart,
-      ], 1)
-    ];
+      $section2Build = [
+        $this->dashboardBuilder->columns([
+          $groupMembersByCountryChart,
+        ], 1)
+      ];
 
-    // Group members by topics of expertise.
-    $topicsVocabulary = 'topics';
-    $topicsOfExpertiseField = 'field_vocab_topic_expertise';
-    $groupMembersByExpertiseData = json_encode($this->dashboardHelper->transformTermTreeCountsForChart($this->groupStatistics->getGroupMembersPerTaxonomyTerm($group, $topicsOfExpertiseField, 'id', DashboardFilters::DASHBOARD_MEMBERS_LIST_GROUP_ID), $topicsVocabulary));
-    $groupMembersByExpertiseChart = $this->dashboardBuilder->chartPie($this->t('Members by topics of expertise'), $groupMembersByExpertiseData, '');
+      // Group members by topics of expertise.
+      $topicsVocabulary = 'topics';
+      $topicsOfExpertiseField = 'field_vocab_topic_expertise';
+      $groupMembersByExpertiseData = json_encode($this->dashboardHelper->transformTermTreeCountsForChart($this->groupStatistics->getGroupMembersPerTaxonomyTerm($group, $topicsOfExpertiseField, 'id', DashboardFilters::DASHBOARD_MEMBERS_LIST_GROUP_ID), $topicsVocabulary));
+      $groupMembersByExpertiseChart = $this->dashboardBuilder->chartPie($this->t('Members by topics of expertise'), $groupMembersByExpertiseData, '');
 
-    // Group members by topics of interest.
-    $topicsOfInterestField = 'field_vocab_topic_interest';
-    $groupMembersByInterestData = json_encode($this->dashboardHelper->transformTermTreeCountsForChart($this->groupStatistics->getGroupMembersPerTaxonomyTerm($group, $topicsOfInterestField, 'id', DashboardFilters::DASHBOARD_MEMBERS_LIST_GROUP_ID), $topicsVocabulary));
-    $groupMembersByInterestChart = $this->dashboardBuilder->chartPie($this->t('Members by topics of interest'), $groupMembersByInterestData, '');
+      // Group members by topics of interest.
+      $topicsOfInterestField = 'field_vocab_topic_interest';
+      $groupMembersByInterestData = json_encode($this->dashboardHelper->transformTermTreeCountsForChart($this->groupStatistics->getGroupMembersPerTaxonomyTerm($group, $topicsOfInterestField, 'id', DashboardFilters::DASHBOARD_MEMBERS_LIST_GROUP_ID), $topicsVocabulary));
+      $groupMembersByInterestChart = $this->dashboardBuilder->chartPie($this->t('Members by topics of interest'), $groupMembersByInterestData, '');
 
-    $section3Build = [
-      $this->dashboardBuilder->columns([
-        $groupMembersByExpertiseChart,
-        $groupMembersByInterestChart,
-      ], 2)
-    ];
+      $section3Build = [
+        $this->dashboardBuilder->columns([
+          $groupMembersByExpertiseChart,
+          $groupMembersByInterestChart,
+        ], 2)
+      ];
 
-    // Group events statistics.
-    $eventsTitle = 'Events';
-    $eventsType = 'group-group_node-event';
+      // Group events statistics.
+      $eventsTitle = 'Events';
+      $eventsType = 'group-group_node-event';
 
-    // Group events.
-    $groupTotalEventsData = $this->groupStatistics->getNumberOfContentInGivenPeriod($group, $eventsType);
-    $groupTotalEvents = $this->dashboardBuilder->numberAndLink($this->t('Total events'), $groupTotalEventsData, '');
+      // Group events.
+      $groupTotalEventsData = $this->groupStatistics->getNumberOfContentInGivenPeriod($group, $eventsType);
+      $groupTotalEvents = $this->dashboardBuilder->numberAndLink($this->t('Total events'), $groupTotalEventsData, '');
 
-    // Group events created in past days.
-    $groupEventsCreatedInPastDaysData = $this->groupStatistics->getNumberOfContentInGivenPeriod($group, $eventsType, $lastDaysLimit);
-    $groupEventsCreatedInPastDays = $this->dashboardBuilder->numberAndLink($this->t('New events - past @limit days', ['@limit' => $lastDaysLimit]), $groupEventsCreatedInPastDaysData, '');
+      // Group events created in past days.
+      $groupEventsCreatedInPastDaysData = $this->groupStatistics->getNumberOfContentInGivenPeriod($group, $eventsType, $lastDaysLimit);
+      $groupEventsCreatedInPastDays = $this->dashboardBuilder->numberAndLink($this->t('New events - past @limit days', ['@limit' => $lastDaysLimit]), $groupEventsCreatedInPastDaysData, '');
 
-    // Group events by type chart.
-    $typeField = 'field_vocab_event_type';
-    $groupEventsByTypeData = json_encode($this->dashboardHelper->transformLabelCountToNameAndY($this->groupStatistics->getGroupNodesOfGroupByTerm($group, $eventsType, $typeField)));
-    $groupEventsByType = $this->dashboardBuilder->chartPie($this->t('Events by type'), $groupEventsByTypeData, '');
+      // Group events by type chart.
+      $typeField = 'field_vocab_event_type';
+      $groupEventsByTypeData = json_encode($this->dashboardHelper->transformLabelCountToNameAndY($this->groupStatistics->getGroupNodesOfGroupByTerm($group, $eventsType, $typeField)));
+      $groupEventsByType = $this->dashboardBuilder->chartPie($this->t('Events by type'), $groupEventsByTypeData, '');
 
-    // Group events by topic chart.
-    $topicField = 'field_vocab_topics';
-    $groupEventsByTopicData = json_encode($this->dashboardHelper->transformTermTreeCountsForChart($this->groupStatistics->getGroupNodesOfGroupByTerm($group, $eventsType, $topicField), $topicsVocabulary));
-    $groupEventsByTopic = $this->dashboardBuilder->chartPie($this->t('Events by topic'), $groupEventsByTopicData, '');
+      // Group events by topic chart.
+      $topicField = 'field_vocab_topics';
+      $groupEventsByTopicData = json_encode($this->dashboardHelper->transformTermTreeCountsForChart($this->groupStatistics->getGroupNodesOfGroupByTerm($group, $eventsType, $topicField), $topicsVocabulary));
+      $groupEventsByTopic = $this->dashboardBuilder->chartPie($this->t('Events by topic'), $groupEventsByTopicData, '');
 
-    $eventsContent = [
-      $this->dashboardBuilder->columns([
-        $groupTotalEvents,
-        $groupEventsCreatedInPastDays,
-        $groupEventsByType,
-        $groupEventsByTopic,
-      ], 2)
-    ];
+      $eventsContent = [
+        $this->dashboardBuilder->columns([
+          $groupTotalEvents,
+          $groupEventsCreatedInPastDays,
+          $groupEventsByType,
+          $groupEventsByTopic,
+        ], 2)
+      ];
 
-    // Section 4.
-    $section4Build = [$this->dashboardBuilder->dashboardSection($eventsTitle, '', $eventsContent, 'events')];
+      // Section 4.
+      $section4Build = [$this->dashboardBuilder->dashboardSection($eventsTitle, '', $eventsContent, 'events')];
 
-    // Group discussions statistics.
-    $discussionsTitle = 'Forum discussions';
-    $discussionType = 'group-group_node-discussion';
+      // Group discussions statistics.
+      $discussionsTitle = 'Forum discussions';
+      $discussionType = 'group-group_node-discussion';
 
-    $discussionsButton = $this->dashboardBuilder->buttonToRoute($this->t('List all'), 'eic_overviews.groups.overview_page.discussions', 'group', $group->id());
+      $discussionsButton = $this->dashboardBuilder->buttonToRoute($this->t('List all'), 'eic_overviews.groups.overview_page.discussions', 'group', $group->id());
 
-    $groupTotalDiscussionsData = $this->groupStatistics->getNumberOfContentInGivenPeriod($group, $discussionType);
-    $groupTotalDiscussions = $this->dashboardBuilder->numberAndLink($this->t('Total discussions'), $groupTotalDiscussionsData, '');
+      $groupTotalDiscussionsData = $this->groupStatistics->getNumberOfContentInGivenPeriod($group, $discussionType);
+      $groupTotalDiscussions = $this->dashboardBuilder->numberAndLink($this->t('Total discussions'), $groupTotalDiscussionsData, '');
 
-    $groupDiscussionsPastDaysData = $this->groupStatistics->getNumberOfContentInGivenPeriod($group, $discussionType, $lastDaysLimit);
-    $groupDiscussionsPastDays = $this->dashboardBuilder->numberAndLink($this->t('New discussions in last @limit days', ['@limit' => $lastDaysLimit]), $groupDiscussionsPastDaysData, '');
+      $groupDiscussionsPastDaysData = $this->groupStatistics->getNumberOfContentInGivenPeriod($group, $discussionType, $lastDaysLimit);
+      $groupDiscussionsPastDays = $this->dashboardBuilder->numberAndLink($this->t('New discussions in last @limit days', ['@limit' => $lastDaysLimit]), $groupDiscussionsPastDaysData, '');
 
-    $discussionsContent = [
-      $this->dashboardBuilder->columns([
-        $groupTotalDiscussions,
-        $groupDiscussionsPastDays,
-      ], 2)
-    ];
+      $discussionsContent = [
+        $this->dashboardBuilder->columns([
+          $groupTotalDiscussions,
+          $groupDiscussionsPastDays,
+        ], 2)
+      ];
 
-    // Section 5.
-    $section5Build = [$this->dashboardBuilder->dashboardSection($discussionsTitle, $discussionsButton, $discussionsContent, 'discussions')];
+      // Section 5.
+      $section5Build = [$this->dashboardBuilder->dashboardSection($discussionsTitle, $discussionsButton, $discussionsContent, 'discussions')];
 
-    // Group files statistics.
-    $filesTitle = 'Files';
-    $fileType = 'group-group_node-document';
+      // Group files statistics.
+      $filesTitle = 'Files';
+      $fileType = 'group-group_node-document';
 
-    $filesButton = $this->dashboardBuilder->buttonToRoute($this->t('List all'), 'eic_overviews.groups.overview_page.files', 'group', $group->id());
+      $filesButton = $this->dashboardBuilder->buttonToRoute($this->t('List all'), 'eic_overviews.groups.overview_page.files', 'group', $group->id());
 
-    $groupTotalFilesData = $this->groupStatistics->getNumberOfContentInGivenPeriod($group, $fileType);
-    $groupTotalFiles = $this->dashboardBuilder->numberAndLink($this->t('Total files'), $groupTotalFilesData, '');
+      $groupTotalFilesData = $this->groupStatistics->getNumberOfContentInGivenPeriod($group, $fileType);
+      $groupTotalFiles = $this->dashboardBuilder->numberAndLink($this->t('Total files'), $groupTotalFilesData, '');
 
-    $groupFilesPastDaysData = $this->groupStatistics->getNumberOfContentInGivenPeriod($group, $fileType, $lastDaysLimit);
-    $groupFilesPastDays = $this->dashboardBuilder->numberAndLink($this->t('New files in last @limit days', ['@limit' => $lastDaysLimit]), $groupFilesPastDaysData, '');
+      $groupFilesPastDaysData = $this->groupStatistics->getNumberOfContentInGivenPeriod($group, $fileType, $lastDaysLimit);
+      $groupFilesPastDays = $this->dashboardBuilder->numberAndLink($this->t('New files in last @limit days', ['@limit' => $lastDaysLimit]), $groupFilesPastDaysData, '');
 
-    $filesContent = [
-      $this->dashboardBuilder->columns([
-        $groupTotalFiles,
-        $groupFilesPastDays,
-      ], 2)
-    ];
+      $filesContent = [
+        $this->dashboardBuilder->columns([
+          $groupTotalFiles,
+          $groupFilesPastDays,
+        ], 2)
+      ];
 
-    // Section 6
-    $section6Build = [$this->dashboardBuilder->dashboardSection($filesTitle, $filesButton, $filesContent, 'files')];
+      // Section 6
+      $section6Build = [$this->dashboardBuilder->dashboardSection($filesTitle, $filesButton, $filesContent, 'files')];
+
+      $content = [
+        $section1Build,
+        $section2Build,
+        $section3Build,
+        $section4Build,
+        $section5Build,
+        $section6Build,
+      ];
+      $this->cache()->set('dashboard:group:' . $group->id(), $content);
+
+    }
 
     $build = [
       '#type' => 'container',
@@ -267,14 +283,7 @@ class IndividualGroupDashboardController extends ControllerBase {
           'ecl-u-mv-xl',
         ],
       ],
-      'content' => [
-        $section1Build,
-        $section2Build,
-        $section3Build,
-        $section4Build,
-        $section5Build,
-        $section6Build,
-      ],
+      'content' => $content,
     ];
 
     return $build;
