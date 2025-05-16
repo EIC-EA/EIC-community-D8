847b5f79167a8c51847234451a4403facb7dd5bf
Dimitris Kritsotakis <dimkri97@gmail.com>
2025-05-16 15:04:30 +0300

    EICNET-3199: cache members dashboard sections

--- lib/modules/eic_dashboards/src/Controller/MembersDashboardController.php
+++ lib/modules/eic_dashboards/src/Controller/MembersDashboardController.php
@@ -91,115 +91,156 @@ class MembersDashboardController extends ControllerBase
   public function membersOverview(): array {
     // ===== Section 1.
     // Platform members.
-    $totalMembers = $this->membersStatistics->getTotalMembers();
-    $membersLink = $this->dashboardBuilder->buttonToView('view.dashboard_members_list.page', '', '', $this->t('Members list'));
-    $members = $this->dashboardBuilder->numberAndLink($this->t('Platform members'), $totalMembers, $membersLink);
+    if ($section1Cache = $this->cache()->get('dashboard:members:section:1')) {
+      $section1Build = $section1Cache->data;
+    }
+    else {
+      $totalMembers = $this->membersStatistics->getTotalMembers();
+      $membersLink = $this->dashboardBuilder->buttonToView('view.dashboard_members_list.page', '', '', $this->t('Members list'));
+      $members = $this->dashboardBuilder->numberAndLink($this->t('Platform members'), $totalMembers, $membersLink);
 
-    // Joined in the past 30 days.
-    $days = 30;
-    $membersCreatedPastDaysData = $this->membersStatistics->getMembersRegisteredPastDays($days);
-    $membersCreatedPastDaysLink = $this->dashboardBuilder->buttonToView('view.dashboard_members_list.page', 'created[min]', gmdate("Y-m-d", strtotime("-$days days")), $this->t('Members list'));
-    $membersCreatedPastDays = $this->dashboardBuilder->numberAndLink($this->t('Joined in the past @days days', ['@days' => $days]), $membersCreatedPastDaysData, $membersCreatedPastDaysLink);
+      // Joined in the past 30 days.
+      $days = 30;
+      $membersCreatedPastDaysData = $this->membersStatistics->getMembersRegisteredPastDays($days);
+      $membersCreatedPastDaysLink = $this->dashboardBuilder->buttonToView('view.dashboard_members_list.page', 'created[min]', gmdate("Y-m-d", strtotime("-$days days")), $this->t('Members list'));
+      $membersCreatedPastDays = $this->dashboardBuilder->numberAndLink($this->t('Joined in the past @days days', ['@days' => $days]), $membersCreatedPastDaysData, $membersCreatedPastDaysLink);
 
-    // Recently logged in.
-    $days = 30;
-    $membersLoggedPastDaysData = $this->membersStatistics->getPlatformMembersLoggedPastDays($days);
-    $membersLoggedPastDaysLink = $this->dashboardBuilder->buttonToView('view.dashboard_members_list.page', 'access[min]', gmdate("Y-m-d", strtotime("-$days days")), $this->t('Members list'));
-    $membersLoggedPastDays = $this->dashboardBuilder->numberAndLink($this->t('Recently logged in'), $membersLoggedPastDaysData, $membersLoggedPastDaysLink);
+      // Recently logged in.
+      $days = 30;
+      $membersLoggedPastDaysData = $this->membersStatistics->getPlatformMembersLoggedPastDays($days);
+      $membersLoggedPastDaysLink = $this->dashboardBuilder->buttonToView('view.dashboard_members_list.page', 'access[min]', gmdate("Y-m-d", strtotime("-$days days")), $this->t('Members list'));
+      $membersLoggedPastDays = $this->dashboardBuilder->numberAndLink($this->t('Recently logged in'), $membersLoggedPastDaysData, $membersLoggedPastDaysLink);
 
-    $membersStats = [
-      $this->dashboardBuilder->columns([
-        $members,
-        $membersCreatedPastDays,
-        $membersLoggedPastDays,
-      ], 3),
-    ];
+      $membersStats = [
+        $this->dashboardBuilder->columns([
+          $members,
+          $membersCreatedPastDays,
+          $membersLoggedPastDays,
+        ], 3),
+      ];
 
-    // Members evolution.
-    $membersCumulativeStats = $this->dashboardCumulativeService->getCumulativeStatsPerDashboardType(DashboardsDatabase::MEMBERS_DASHBOARD_TYPE);
-    $membersData = $this->dashboardHelper->jsonEncodeCategoriesSeries($this->dashboardHelper->transformIdCountToCategoriesSeries($membersCumulativeStats));
-    $membersEvolution = $this->dashboardBuilder->chartLine($this->t('Members - evolution over time'), $membersData);
+      // Members evolution.
+      $membersCumulativeStats = $this->dashboardCumulativeService->getCumulativeStatsPerDashboardType(DashboardsDatabase::MEMBERS_DASHBOARD_TYPE);
+      $membersData = $this->dashboardHelper->jsonEncodeCategoriesSeries($this->dashboardHelper->transformIdCountToCategoriesSeries($membersCumulativeStats));
+      $membersEvolution = $this->dashboardBuilder->chartLine($this->t('Members - evolution over time'), $membersData);
 
-    $section1Build = [
-      $this->dashboardBuilder->columns([
-        $membersStats,
-        $membersEvolution
-      ], 1),
-    ];
+      $section1Build = [
+        $this->dashboardBuilder->columns([
+          $membersStats,
+          $membersEvolution
+        ], 1),
+      ];
+      $this->cache()->set('dashboard:members:section:1', $section1Build);
+    }
 
     // ===== Section 2.
     // Members by country.
-    $membersByCountryData = $this->dashboardHelper->jsonEncodeCategoriesSeries($this->dashboardHelper->transformIdCountToCategoriesSeries($this->membersStatistics->getMembersGroupedByCountry('id')));
-    $membersByCountryChart = $this->dashboardBuilder->chartColumn($this->t('Members by country'), $membersByCountryData, false);
-    $membersByCountryMenuData = $this->dashboardHelper->prepareDataForJumpMenu($this->membersStatistics->getMembersGroupedByCountry(DashboardFilters::DASHBOARD_PROJECTS_LIST_COUNTRY), 'view.dashboard_members_list.page', [], [DashboardFilters::DASHBOARD_PROJECTS_LIST_COUNTRY]);
-    $membersByCountryMenu = $this->dashboardBuilder->jumpMenu($this->t('List members of'), $this->t('Choose a country'), $membersByCountryMenuData);
-    $membersByCountry = $this->dashboardBuilder->chartWithMenu($membersByCountryChart, $membersByCountryMenu);
+    if ($section2Cache = $this->cache()->get('dashboard:members:section:2')) {
+      $section2Build = $section2Cache->data;
+    }
+    else {
+      $membersByCountryData = $this->dashboardHelper->jsonEncodeCategoriesSeries($this->dashboardHelper->transformIdCountToCategoriesSeries($this->membersStatistics->getMembersGroupedByCountry('id')));
+      $membersByCountryChart = $this->dashboardBuilder->chartColumn($this->t('Members by country'), $membersByCountryData, FALSE);
+      $membersByCountryMenuData = $this->dashboardHelper->prepareDataForJumpMenu($this->membersStatistics->getMembersGroupedByCountry(DashboardFilters::DASHBOARD_PROJECTS_LIST_COUNTRY), 'view.dashboard_members_list.page', [], [DashboardFilters::DASHBOARD_PROJECTS_LIST_COUNTRY]);
+      $membersByCountryMenu = $this->dashboardBuilder->jumpMenu($this->t('List members of'), $this->t('Choose a country'), $membersByCountryMenuData);
+      $membersByCountry = $this->dashboardBuilder->chartWithMenu($membersByCountryChart, $membersByCountryMenu);
 
-    $section2Build = $this->dashboardBuilder->columns([$membersByCountry], 1);
+      $section2Build = $this->dashboardBuilder->columns([$membersByCountry], 1);
+      $this->cache()->set('dashboard:members:section:2', $section2Build);
+    }
 
     // ===== Section 3.
-    // Members by organisation type.
-    $membersByUserTypeData = json_encode($this->dashboardHelper->transformLabelCountToNameAndY($this->membersStatistics->getMembersPerTaxonomyTerm('field_vocab_user_type', 'id')));
-    $membersByUserTypeChart = $this->dashboardBuilder->chartPie($this->t('Members by type'), $membersByUserTypeData, '');
-    $membersByUserTypeMenuData = $this->dashboardHelper->prepareDataForJumpMenu($this->membersStatistics->getMembersPerTaxonomyTerm('field_vocab_user_type', DashboardFilters::DASHBOARD_MEMBERS_LIST_USER_TYPE), 'view.dashboard_members_list.page', [], [DashboardFilters::DASHBOARD_MEMBERS_LIST_USER_TYPE]);
-    $membersByUserTypeMenu = $this->dashboardBuilder->jumpMenu($this->t('List members by type'), $this->t('Choose expertise'), $membersByUserTypeMenuData);
-    $membersByOrganizationType = $this->dashboardBuilder->chartWithMenu($membersByUserTypeChart, $membersByUserTypeMenu);
+    if ($section3Cache = $this->cache()->get('dashboard:members:section:3')) {
+      $section3Build = $section3Cache->data;
+    }
+    else {
+      $totalMembers = $this->membersStatistics->getTotalMembers();
 
-    // Members by topic of Interest.
-    $membersByTopicOfInterestData = json_encode($this->dashboardHelper->transformTermTreeCountsForChart($this->membersStatistics->getMembersPerTaxonomyTerm('field_vocab_topic_interest', 'id'), 'topics'));
-    $membersByTopicOfInterestChart = $this->dashboardBuilder->chartPie($this->t('Members by topic of interest'), $membersByTopicOfInterestData, '');
-    $membersByTopicOfInterestMenuData = $this->dashboardHelper->prepareDataForJumpMenu($this->membersStatistics->getMembersPerTaxonomyTerm('field_vocab_topic_interest', DashboardFilters::DASHBOARD_MEMBERS_LIST_TOPIC_OF_INTEREST), 'view.dashboard_members_list.page', [], [DashboardFilters::DASHBOARD_MEMBERS_LIST_TOPIC_OF_INTEREST]);
-    $membersByTopicOfInterestMenu = $this->dashboardBuilder->jumpMenu($this->t('List members by topic of interest'), $this->t('Choose interest'), $membersByTopicOfInterestMenuData);
-    $membersByTopicOfInterest = $this->dashboardBuilder->chartWithMenu($membersByTopicOfInterestChart, $membersByTopicOfInterestMenu);
+      // Members by organisation type.
+      $membersByUserTypeData = json_encode($this->dashboardHelper->transformLabelCountToNameAndY($this->membersStatistics->getMembersPerTaxonomyTerm('field_vocab_user_type', 'id')));
+      $membersByUserTypeChart = $this->dashboardBuilder->chartPie($this->t('Members by type'), $membersByUserTypeData, '');
+      $membersByUserTypeMenuData = $this->dashboardHelper->prepareDataForJumpMenu($this->membersStatistics->getMembersPerTaxonomyTerm('field_vocab_user_type', DashboardFilters::DASHBOARD_MEMBERS_LIST_USER_TYPE), 'view.dashboard_members_list.page', [], [DashboardFilters::DASHBOARD_MEMBERS_LIST_USER_TYPE]);
+      $membersByUserTypeMenu = $this->dashboardBuilder->jumpMenu($this->t('List members by type'), $this->t('Choose expertise'), $membersByUserTypeMenuData);
+      $membersByOrganizationType = $this->dashboardBuilder->chartWithMenu($membersByUserTypeChart, $membersByUserTypeMenu);
 
-    // Members by topic of Expertise.
-    $membersByTopicOfExpertiseData = json_encode($this->dashboardHelper->transformTermTreeCountsForChart($this->membersStatistics->getMembersPerTaxonomyTerm('field_vocab_topic_expertise', 'id'),'topics'));
-    $membersByTopicOfExpertiseChart = $this->dashboardBuilder->chartPie($this->t('Members by topic of expertise'), $membersByTopicOfExpertiseData, '');
-    $membersByTopicOfExpertiseMenuData = $this->dashboardHelper->prepareDataForJumpMenu($this->membersStatistics->getMembersPerTaxonomyTerm('field_vocab_topic_expertise', DashboardFilters::DASHBOARD_MEMBERS_LIST_TOPIC_OF_EXPERTISE), 'view.dashboard_members_list.page', [], [DashboardFilters::DASHBOARD_MEMBERS_LIST_TOPIC_OF_INTEREST]);
-    $membersByTopicOfExpertiseMenu = $this->dashboardBuilder->jumpMenu($this->t('List members by topic of expertise'), $this->t('Choose expertise'), $membersByTopicOfExpertiseMenuData);
-    $membersByTopicOfExpertise = $this->dashboardBuilder->chartWithMenu($membersByTopicOfExpertiseChart, $membersByTopicOfExpertiseMenu);
+      // Members by topic of Interest.
+      $membersByTopicOfInterestData = json_encode($this->dashboardHelper->transformTermTreeCountsForChart($this->membersStatistics->getMembersPerTaxonomyTerm('field_vocab_topic_interest', 'id'), 'topics'));
+      $membersByTopicOfInterestChart = $this->dashboardBuilder->chartPie($this->t('Members by topic of interest'), $membersByTopicOfInterestData, '');
+      $membersByTopicOfInterestMenuData = $this->dashboardHelper->prepareDataForJumpMenu($this->membersStatistics->getMembersPerTaxonomyTerm('field_vocab_topic_interest', DashboardFilters::DASHBOARD_MEMBERS_LIST_TOPIC_OF_INTEREST), 'view.dashboard_members_list.page', [], [DashboardFilters::DASHBOARD_MEMBERS_LIST_TOPIC_OF_INTEREST]);
+      $membersByTopicOfInterestMenu = $this->dashboardBuilder->jumpMenu($this->t('List members by topic of interest'), $this->t('Choose interest'), $membersByTopicOfInterestMenuData);
+      $membersByTopicOfInterest = $this->dashboardBuilder->chartWithMenu($membersByTopicOfInterestChart, $membersByTopicOfInterestMenu);
 
-    $totalCompletedMembersProfiles = $this->eicUserHelper->getMemberProfileCompletionCount();
-    $totalCompletedMembersProfilesData = json_encode([
-      ['name' => 'Completed profile', 'y' => $totalCompletedMembersProfiles],
-      ['name' => 'Incomplete profile', 'y' => ($totalMembers - $totalCompletedMembersProfiles)],
-    ], JSON_NUMERIC_CHECK);
-    $totalCompletedMembersProfilesChart = $this->dashboardBuilder->chartPie($this->t('Profile completion status'), $totalCompletedMembersProfilesData, '');
+      // Members by topic of Expertise.
+      $membersByTopicOfExpertiseData = json_encode($this->dashboardHelper->transformTermTreeCountsForChart($this->membersStatistics->getMembersPerTaxonomyTerm('field_vocab_topic_expertise', 'id'), 'topics'));
+      $membersByTopicOfExpertiseChart = $this->dashboardBuilder->chartPie($this->t('Members by topic of expertise'), $membersByTopicOfExpertiseData, '');
+      $membersByTopicOfExpertiseMenuData = $this->dashboardHelper->prepareDataForJumpMenu($this->membersStatistics->getMembersPerTaxonomyTerm('field_vocab_topic_expertise', DashboardFilters::DASHBOARD_MEMBERS_LIST_TOPIC_OF_EXPERTISE), 'view.dashboard_members_list.page', [], [DashboardFilters::DASHBOARD_MEMBERS_LIST_TOPIC_OF_INTEREST]);
+      $membersByTopicOfExpertiseMenu = $this->dashboardBuilder->jumpMenu($this->t('List members by topic of expertise'), $this->t('Choose expertise'), $membersByTopicOfExpertiseMenuData);
+      $membersByTopicOfExpertise = $this->dashboardBuilder->chartWithMenu($membersByTopicOfExpertiseChart, $membersByTopicOfExpertiseMenu);
 
-    // Members linked to Organisations
-    $membersLinkedToOrganisations = $this->membersStatistics->getMembersLinkedByType('organisation-group_membership');
-    $membersLinkedToOrganisationsData = json_encode([
-      ['name' => 'Linked to at least one EIC Community organisation', 'y' => $membersLinkedToOrganisations],
-      ['name' => 'Not linked to any Organisation', 'y' => ($totalMembers - $membersLinkedToOrganisations)],
-    ], JSON_NUMERIC_CHECK);
-    $membersLinkedToOrganisationsChart = $this->dashboardBuilder->chartPie($this->t('Members linked to Organisations'), $membersLinkedToOrganisationsData, '');
+      $totalCompletedMembersProfiles = $this->eicUserHelper->getMemberProfileCompletionCount();
+      $totalCompletedMembersProfilesData = json_encode([
+        ['name' => 'Completed profile', 'y' => $totalCompletedMembersProfiles],
+        [
+          'name' => 'Incomplete profile',
+          'y' => ($totalMembers - $totalCompletedMembersProfiles)
+        ],
+      ], JSON_NUMERIC_CHECK);
+      $totalCompletedMembersProfilesChart = $this->dashboardBuilder->chartPie($this->t('Profile completion status'), $totalCompletedMembersProfilesData, '');
 
-    // Members linked to Projects
-    $membersLinkedToProjects = $this->membersStatistics->getMembersLinkedByType('project-group_membership');
-    $membersLinkedToProjectsData = json_encode([
-      ['name' => 'Linked to at least one EIC Community organisation', 'y' => $membersLinkedToProjects],
-      ['name' => 'Not linked to any Organisation', 'y' => ($totalMembers - $membersLinkedToProjects)],
-    ], JSON_NUMERIC_CHECK);
-    $membersLinkedToProjectsChart = $this->dashboardBuilder->chartPie($this->t('Members linked to Projects'), $membersLinkedToProjectsData, '');
+      // Members linked to Organisations
+      $membersLinkedToOrganisations = $this->membersStatistics->getMembersLinkedByType('organisation-group_membership');
+      $membersLinkedToOrganisationsData = json_encode([
+        [
+          'name' => 'Linked to at least one EIC Community organisation',
+          'y' => $membersLinkedToOrganisations
+        ],
+        [
+          'name' => 'Not linked to any Organisation',
+          'y' => ($totalMembers - $membersLinkedToOrganisations)
+        ],
+      ], JSON_NUMERIC_CHECK);
+      $membersLinkedToOrganisationsChart = $this->dashboardBuilder->chartPie($this->t('Members linked to Organisations'), $membersLinkedToOrganisationsData, '');
 
-    $section3Build = $this->dashboardBuilder->columns([
-      $membersByOrganizationType,
-      $membersByTopicOfInterest,
-      $membersByTopicOfExpertise,
-      $totalCompletedMembersProfilesChart,
-      $membersLinkedToOrganisationsChart,
-      $membersLinkedToProjectsChart
-    ], 2);
+      // Members linked to Projects
+      $membersLinkedToProjects = $this->membersStatistics->getMembersLinkedByType('project-group_membership');
+      $membersLinkedToProjectsData = json_encode([
+        [
+          'name' => 'Linked to at least one EIC Community organisation',
+          'y' => $membersLinkedToProjects
+        ],
+        [
+          'name' => 'Not linked to any Organisation',
+          'y' => ($totalMembers - $membersLinkedToProjects)
+        ],
+      ], JSON_NUMERIC_CHECK);
+      $membersLinkedToProjectsChart = $this->dashboardBuilder->chartPie($this->t('Members linked to Projects'), $membersLinkedToProjectsData, '');
+
+      $section3Build = $this->dashboardBuilder->columns([
+        $membersByOrganizationType,
+        $membersByTopicOfInterest,
+        $membersByTopicOfExpertise,
+        $totalCompletedMembersProfilesChart,
+        $membersLinkedToOrganisationsChart,
+        $membersLinkedToProjectsChart
+      ], 2);
+      $this->cache()->set('dashboard:members:section:3', $section3Build);
+    }
 
     // ===== Section 4.
-    $maxResults = 10;
-    $lastRegisteredMembersList = $this->dashboardBuilder->titleLinkList($this->t('Latest members'), '', $this->membersStatistics->getMembersListInGivenPeriod('', '', $maxResults));
+    if ($section4Cache = $this->cache()->get('dashboard:members:section:4')) {
+      $section4Build = $section4Cache->data;
+    }
+    else {
+      $maxResults = 10;
+      $lastRegisteredMembersList = $this->dashboardBuilder->titleLinkList($this->t('Latest members'), '', $this->membersStatistics->getMembersListInGivenPeriod('', '', $maxResults));
 
-    $section4Build = [
-      $this->dashboardBuilder->columns([
-        $lastRegisteredMembersList,
-      ], 2),
-    ];
+      $section4Build = [
+        $this->dashboardBuilder->columns([
+          $lastRegisteredMembersList,
+        ], 2),
+      ];
+      $this->cache()->set('dashboard:members:section:4', $section4Build);
+    }
 
     // Build sections
     $build = [
