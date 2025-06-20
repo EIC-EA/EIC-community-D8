38ac344b9539f761d588b2d1b7c140f1e06a33ed
arismag <aris@pointblank.gr>
2025-06-20 10:23:47 +0300

    EICNET-3339: Add story 'call' type as constants and share among php/react.

---
+++ lib/modules/eic_content/src/Constants/StoryTypes.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Drupal\eic_content\Constants;
+
+/**
+ * Story types 'call' related constants.
+ */
+final class StoryTypes {
+
+  /**
+   * The node ID of the top first-level page.
+   *
+   * @var int
+   */
+  const PARTNER_CALLS_TAXONOMY_TERM_ID = 1222;
+  const OPEN_CALLS_TAXONOMY_TERM_ID = 1221;
+
+}
