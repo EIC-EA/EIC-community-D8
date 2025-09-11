361a86f51dc076ae0ea207429b9539350e4bd2d5
Dimitris Kritsotakis <dimkri97@gmail.com>
2025-09-11 16:56:14 +0300

    EICNET-3386: add an action to dispatch message to group admins

--- lib/modules/eic_eca/src/Plugin/Action/GroupFlagDispatchAction.php
+++ lib/modules/eic_eca/src/Plugin/Action/GroupFlagDispatchAction.php
@@ -6,19 +6,19 @@ use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Access\AccessResultInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\eca\Plugin\Action\ConfigurableActionBase;
+use Drupal\eic_groups\EICGroupsHelper;
 
 /**
  * Describes the eic_eca group_inactivity_action action.
  *
  * @Action(
- *   id = "eic_eca_flag_dispatch_usre",
- *   label = @Translation("Flag dispatch action"),
- *   description = @Translation("Dispatch notification email for user."),
- *   eca_version_introduced = "1.0.0",
- *   type = "entity"
+ *   id = "eic_eca_flag_dispatch_group_admins",
+ *   label = @Translation("Group flag dispatch action"),
+ *   description = @Translation("Dispatch notification email for group
+ *   admins."), eca_version_introduced = "1.0.0", type = "entity"
  *   )
  */
-class FlagDispatchAction extends ConfigurableActionBase {
+class GroupFlagDispatchAction extends ConfigurableActionBase {
 
   /**
    * {@inheritdoc}
@@ -33,20 +33,22 @@ class FlagDispatchAction extends ConfigurableActionBase {
    */
   public function execute($entity = NULL): void {
     $flagging = $this->entityTypeManager->getStorage('flagging')->create([
-      'flag_id' => 'user_inactive_1_month',
-      'entity_type' => 'user',
+      'flag_id' => 'group_inactive',
+      'entity_type' => 'group',
       'entity_id' => $entity->id(),
+      'field_inactivity_duration' => 6,
       'uid' => 1,
     ]);
     $flagging->save();
-
     /** @var \Drupal\eic_messages\Service\MessageBusInterface $bus */
     $bus = \Drupal::service('eic_messages.message_bus');
-
-    $bus->dispatch([
-      'template' => 'notify_contact_inactive_user',
-      'uid' => $entity->id(),
-    ]);
+    foreach (EICGroupsHelper::getGroupAdmins($entity) as $group_admin) {
+      $admin = $group_admin->getUser();
+      $bus->dispatch([
+        'template' => 'notify_contact_inactive_group_6',
+        'uid' => $admin->id(),
+      ]);
+    }
   }
 
 }
