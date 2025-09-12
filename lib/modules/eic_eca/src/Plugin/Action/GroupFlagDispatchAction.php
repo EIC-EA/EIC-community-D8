3da0262f930db070aec64af7fda0440f1fe8fe2e
Dimitris Kritsotakis <dimkri97@gmail.com>
2025-09-12 18:04:28 +0300

    EICNET-3386: add config form to group dispatch action

--- lib/modules/eic_eca/src/Plugin/Action/GroupFlagDispatchAction.php
+++ lib/modules/eic_eca/src/Plugin/Action/GroupFlagDispatchAction.php
@@ -4,6 +4,7 @@ namespace Drupal\eic_eca\Plugin\Action;
 
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Access\AccessResultInterface;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\eca\Plugin\Action\ConfigurableActionBase;
 use Drupal\eic_groups\EICGroupsHelper;
@@ -14,8 +15,9 @@ use Drupal\eic_groups\EICGroupsHelper;
  * @Action(
  *   id = "eic_eca_flag_dispatch_group_admins",
  *   label = @Translation("Group flag dispatch action"),
- *   description = @Translation("Dispatch notification email for group
- *   admins."), eca_version_introduced = "1.0.0", type = "entity"
+ *   description = @Translation("Dispatch notification email for group admins."),
+ *   eca_version_introduced = "1.0.0",
+ *   type = "entity"
  *   )
  */
 class GroupFlagDispatchAction extends ConfigurableActionBase {
@@ -33,10 +35,10 @@ class GroupFlagDispatchAction extends ConfigurableActionBase {
    */
   public function execute($entity = NULL): void {
     $flagging = $this->entityTypeManager->getStorage('flagging')->create([
-      'flag_id' => 'group_inactive',
+      'flag_id' => $this->configuration['flag_id'],
       'entity_type' => 'group',
       'entity_id' => $entity->id(),
-      'field_inactivity_duration' => 6,
+      'field_inactivity_duration' => $this->configuration['inactivity_duration'],
       'uid' => 1,
     ]);
     $flagging->save();
@@ -45,10 +47,63 @@ class GroupFlagDispatchAction extends ConfigurableActionBase {
     foreach (EICGroupsHelper::getGroupAdmins($entity) as $group_admin) {
       $admin = $group_admin->getUser();
       $bus->dispatch([
-        'template' => 'notify_contact_inactive_group_6',
+        'template' => $this->configuration['message_template'],
         'uid' => $admin->id(),
       ]);
     }
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration(): array {
+    return [
+        'message_template' => 'notify_contact_inactive_group_6',
+        'flag_id' => 'group_inactive',
+        'inactivity_duration' => 6,
+      ] + parent::defaultConfiguration();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state): array {
+    $form['flag_id'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Flag ID to record the transaction.'),
+      '#default_value' => $this->configuration['flag_id'],
+      '#options' => [
+        'group_inactive' => $this->t('Group inactive'),
+      ]
+    ];
+
+    $form['message_template'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Message template to use'),
+      '#default_value' => $this->configuration['message_template'],
+      '#options' => [
+        'notify_contact_inactive_group_6' => $this->t('Notify contact inactive 6 months.'),
+      ]
+    ];
+
+    $form['inactivity_duration'] = [
+      '#type' => 'number',
+      '#title' => $this->t('Inactivity duration'),
+      '#description' => $this->t('Enter the same value as in the previous action!'),
+      '#default_value' => $this->configuration['inactivity_duration'],
+      '#field_suffix' => $this->t('month(s)'),
+    ];
+    return parent::buildConfigurationForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state): void {
+    $this->configuration['message_template'] = $form_state->getValue('message_template');
+    $this->configuration['flag_id'] = $form_state->getValue('flag_id');
+    $this->configuration['inactivity_duration'] = $form_state->getValue('inactivity_duration');
+    parent::submitConfigurationForm($form, $form_state);
   }
 
 }
