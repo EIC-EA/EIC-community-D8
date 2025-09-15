ec8d8eaae87baba7b8485e79d1f6a09f8e2660ca
Dimitris Kritsotakis <dimkri97@gmail.com>
2025-09-15 18:26:05 +0300

    EICNET-3386: add config form to user flag dispatch action

--- lib/modules/eic_eca/src/Plugin/Action/UserFlagDispatchAction.php
+++ lib/modules/eic_eca/src/Plugin/Action/UserFlagDispatchAction.php
@@ -4,6 +4,7 @@ namespace Drupal\eic_eca\Plugin\Action;
 
 use Drupal\Core\Access\AccessResult;
 use Drupal\Core\Access\AccessResultInterface;
+use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Session\AccountInterface;
 use Drupal\eca\Plugin\Action\ConfigurableActionBase;
 
@@ -33,7 +34,7 @@ class UserFlagDispatchAction extends ConfigurableActionBase {
    */
   public function execute($entity = NULL): void {
     $flagging = $this->entityTypeManager->getStorage('flagging')->create([
-      'flag_id' => 'user_inactive_1_month',
+      'flag_id' => $this->configuration['flag_id'],
       'entity_type' => 'user',
       'entity_id' => $entity->id(),
       'uid' => 1,
@@ -44,9 +45,53 @@ class UserFlagDispatchAction extends ConfigurableActionBase {
     $bus = \Drupal::service('eic_messages.message_bus');
 
     $bus->dispatch([
-      'template' => 'notify_contact_inactive_user',
+      'template' => $this->configuration['message_template'],
       'uid' => $entity->id(),
     ]);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function defaultConfiguration(): array {
+    return [
+        'message_template' => 'notify_contact_inactive_user',
+        'flag_id' => 'user_inactive_1_month',
+      ] + parent::defaultConfiguration();
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function buildConfigurationForm(array $form, FormStateInterface $form_state): array {
+    $form['flag_id'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Flag ID to record the transaction.'),
+      '#default_value' => $this->configuration['flag_id'],
+      '#options' => [
+        'user_inactive_1_month' => $this->t('User inactive 1 month'),
+      ]
+    ];
+
+    $form['message_template'] = [
+      '#type' => 'select',
+      '#title' => $this->t('Message template to use'),
+      '#default_value' => $this->configuration['message_template'],
+      '#options' => [
+        'notify_contact_inactive_user' => $this->t('Notify user inactive 1 month.'),
+      ]
+    ];
+
+    return parent::buildConfigurationForm($form, $form_state);
+  }
+
+  /**
+   * {@inheritdoc}
+   */
+  public function submitConfigurationForm(array &$form, FormStateInterface $form_state): void {
+    $this->configuration['message_template'] = $form_state->getValue('message_template');
+    $this->configuration['flag_id'] = $form_state->getValue('flag_id');
+    parent::submitConfigurationForm($form, $form_state);
   }
 
 }
