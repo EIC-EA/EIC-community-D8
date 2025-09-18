5345067e3bf7d4b1f04faea4af222a15c77e7d6a
Dimitris Kritsotakis <dimkri97@gmail.com>
2025-09-18 13:40:15 +0300

    EICNET-3386: add message template for 8 months group inactivity

--- lib/modules/eic_eca/src/Plugin/Action/GroupFlagDispatchAction.php
+++ lib/modules/eic_eca/src/Plugin/Action/GroupFlagDispatchAction.php
@@ -97,6 +97,7 @@ class GroupFlagDispatchAction extends ConfigurableActionBase {
       '#default_value' => $this->configuration['message_template'],
       '#options' => [
         'notify_contact_inactive_group_6' => $this->t('Notify contact inactive 6 months.'),
+        'notify_contact_inactive_group_8' => $this->t('Notify contact inactive 8 months.'),
       ]
     ];
 
